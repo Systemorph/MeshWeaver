8da7c161eb173939e3820b9fe570564962a101e1
Dmitry Kalabin <dkalabin@systemorph.com>
2024-01-12 08:53:15 +0100

    added IMessageDelivery interface

---
+++ src/Systemorph.Messaging.Contract/IMessageService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Systemorph.Messaging;
+
+public record PostOptions(object Sender)
+{
+    public const string RequestId = nameof(RequestId);
+    internal object Target { get; init; }
+    public ImmutableDictionary<string, object> Properties { get; init; } = ImmutableDictionary<string, object>.Empty;
+
+    public PostOptions WithProperty(string key, object value) => this with { Properties = Properties.SetItem(key, value)};
+    public PostOptions WithTarget(object targetAddress) => this with { Target = targetAddress };
+
+    public PostOptions ResponseFor(IMessageDelivery requestDelivery) =>
+        requestDelivery == null ? this : 
+        this with
+        {
+            Target = requestDelivery.Sender,
+            Properties = requestDelivery.Properties
+                                        .SetItem(RequestId, requestDelivery.Id)
+        };
+
+    public PostOptions WithRequestIdFrom(IMessageDelivery requestDelivery) => this with
+                                                                              {
+                                                                                  Properties = requestDelivery.Properties
+                                                                                                              .SetItem(RequestId, requestDelivery.Id)
+                                                                              };
+
+
+    public PostOptions WithProperties(IDictionary<string, object> properties)
+    {
+        return this with { Properties = Properties.AddRange(properties) };
+    }
+
+
+    //public PostOptions WithSubQueue(string name) => this with { SubQueueName = name };
+    internal string SubQueueName { get; init; }
+}
