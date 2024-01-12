1b0f5a3c0cb1c0549d5b2832029cdbf10dc45d45
Dmitry Kalabin <dkalabin@systemorph.com>
2024-01-12 11:35:27 +0100

    added IMessageHandler and IMessageService interfaces

---
+++ src/OpenSmc.Messaging.Contract/IMessageHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OpenSmc.Messaging;
+
+public interface IMessageHandler : IAsyncDisposable
+{
+    Task<IMessageDelivery> HandleMessageAsync(IMessageDelivery request);
+    void Connect(IMessageService messageService, object address);
+}
---
+++ src/OpenSmc.Messaging.Contract/IMessageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OpenSmc.Messaging;
+
+public interface IMessageService : IAsyncDisposable
+{
+    internal void AddHandler(IMessageHandler messageHandler);
+    internal void RemoveHandler(IMessageHandler messageHandler);
+    public IDisposable Defer(Predicate<IMessageDelivery> deferredFilter);
+    IMessageDelivery IncomingMessage(IMessageDelivery message);
+    IMessageDelivery Forward(IMessageDelivery delivery, object address) => IncomingMessage(delivery.ForwardTo(address));
+    IMessageDelivery Post<TMessage>(TMessage message, Func<PostOptions, PostOptions> configure = null);
+
+
+    void Schedule(Func<Task> action);
+
+    void Schedule(Action action) => Schedule(() =>
+    {
+        action();
+        return Task.CompletedTask;
+    });
+
+    Task<bool> FlushAsync();
+    internal void Start();
+}
