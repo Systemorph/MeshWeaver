dc16cd40617951877f1b3d3cba59024d6abf84f1
Dmitry Kalabin <dkalabin@systemorph.com>
2024-01-12 11:42:29 +0100

    added SyncDelivery and AsyncDelivery delegates

--- src/OpenSmc.Messaging.Contract/IMessageHandler.cs
+++ src/OpenSmc.Messaging.Contract/IMessageHandler.cs
@@ -8,3 +8,8 @@ public interface IMessageHandler : IAsyncDisposable
     Task<IMessageDelivery> HandleMessageAsync(IMessageDelivery request);
     void Connect(IMessageService messageService, object address);
 }
+
+public delegate Task<IMessageDelivery> AsyncDelivery<in TMessage>(IMessageDelivery<TMessage> request);
+public delegate IMessageDelivery SyncDelivery<in TMessage>(IMessageDelivery<TMessage> request);
+public delegate Task<IMessageDelivery> AsyncDelivery(IMessageDelivery request);
+public delegate IMessageDelivery SyncDelivery(IMessageDelivery request);
