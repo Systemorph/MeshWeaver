dcc833ef176ab539edcd566cf7a73478d1687912
Dmitry Kalabin <dkalabin@systemorph.com>
2024-01-15 09:43:41 +0100

    moved a part of ReflectionHelper

---
+++ src/OpenSmc.Reflection/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenSmc.Reflection;
+
+public static class TypeExtensions
+{
+
+    /// <summary>
+    /// Tests if the given <paramref name="type"/> is a static class
+    /// </summary>
+    /// <param name="type">The type to test</param>
+    /// <returns>True, if the given <paramref name="type"/> is a static class.</returns>
+    /// <exception cref="ArgumentNullException">Thrown, if the <paramref name="type"/> is <see langword="null"/></exception>
+    public static bool IsStatic(this Type type)
+    {
+        if (type == null)
+            throw new ArgumentNullException(nameof(type));
+
+        return type.IsAbstract && type.IsSealed;
+    }
+}
