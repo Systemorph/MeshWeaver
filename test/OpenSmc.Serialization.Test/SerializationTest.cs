56bb4e4cc9eaed8cfde6f74368094a523db79992
Alexander Yolokhov <ayolokhov@systemorph.com>
2024-01-16 16:36:50 +0300

    SerializationTest (wip)

--- test/OpenSmc.Serialization.Test/SerializationTest.cs
+++ test/OpenSmc.Serialization.Test/SerializationTest.cs
@@ -1,13 +1,40 @@
-﻿using Xunit;
+﻿using System.Reactive.Linq;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using OpenSmc.Fixture;
+using OpenSmc.Messaging;
+using OpenSmc.Messaging.Hub;
+using OpenSmc.ServiceProvider;
+using Xunit;
 
-namespace OpenSmc.Serialization.Test
+namespace OpenSmc.Serialization.Test;
+
+public class SerializationTest
 {
-    public class SerializationTest
-    {
-        [Fact]
-        public void SimpleTest()
-        {
+    private readonly IServiceCollection serviceCollection;
 
-        }
+    public SerializationTest()
+    {
+        serviceCollection = new ServiceCollection()
+            .AddLogging(l => l.AddXUnitLogger());
+    }
+
+    [Fact]
+    public async Task SimpleTest()
+    {
+        var serviceProvider = serviceCollection
+            .AddSingleton(sp => sp.CreateMessageHub(new HostAddress(),
+                hubConf => hubConf.AddSerialization(conf =>
+                    conf.ForType<MyEvent>(s => s.WithMutation((value, context) => context.SetProperty("NewProp", "New"))))))
+            .SetupModules(new ModulesBuilder());
+
+        var hub = serviceProvider.GetRequiredService<IMessageHub<HostAddress>>();
+        hub.Post(new MyEvent("Hello"));
+        var events = await hub.Out.Timeout(TimeSpan.FromMicroseconds(500)).ToArray();
+        events.Should().HaveCount(1);
     }
 }
+
+
+public record HostAddress();
+public record MyEvent(string Text);
