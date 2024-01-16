cfee35945408fbb5e4dfe80f4a9c8278715e5fd6
Roland Bürgi <rbuergi@systemorph.com>
2024-01-16 15:00:55 +0100

    adding test framework

---
+++ test/OpenSmc.Messaging.Hub.Test/MessageHubTest.cs
@@ -0,0 +1,18 @@
+﻿using OpenSmc.Fixture;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenSmc.Messaging.Hub.Test;
+
+public class MessageHubTest : TestBase
+{
+    public MessageHubTest(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public async Task HelloWorld()
+    {
+
+    }
+}
