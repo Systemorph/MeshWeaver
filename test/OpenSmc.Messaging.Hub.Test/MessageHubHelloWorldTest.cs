4cd8d7449967cca1c4a6aeeba5452f6c6e1d5bf8
Alexander Yolokhov <ayolokhov@systemorph.com>
2024-01-23 14:27:32 +0300

    shorter config of root hub

--- test/OpenSmc.Messaging.Hub.Test/MessageHubHelloWorldTest.cs
+++ test/OpenSmc.Messaging.Hub.Test/MessageHubHelloWorldTest.cs
@@ -1,6 +1,4 @@
-﻿using System.Reactive.Linq;
-using FluentAssertions;
-using Microsoft.Extensions.DependencyInjection;
+﻿using FluentAssertions;
 using OpenSmc.Fixture;
 using OpenSmc.ServiceProvider;
 using Xunit;
@@ -10,38 +8,26 @@ namespace OpenSmc.Messaging.Hub.Test;
 
 public class MessageHubHelloWorldTest : TestBase
 {
-    record RouterAddress;
+    record RouterAddress; // TODO V10: can we use implicitly some internal address and not specify it outside? (23.01.2024, Alexander Yolokhov)
     record HostAddress;
     record ClientAddress;
 
     record SayHelloRequest : IRequest<HelloEvent>;
     record HelloEvent;
 
-    // TODO V10: We need a setup in which router is resolved from top level DI, then host and client are resolved from the DI of the router hub. (18.01.2024, Roland Buergi)
     [Inject] private IMessageHub Router { get; set; }
 
     public MessageHubHelloWorldTest(ITestOutputHelper output) : base(output)
     {
-        Services.AddSingleton<IMessageHub>(sp => sp.CreateMessageHub(new RouterAddress(), hubConf => hubConf
-            .WithHostedHub<HostAddress>(host => host.WithHandler<SayHelloRequest>((hub, request) =>
-            {
-                hub.Post(new HelloEvent(), options => options.ResponseFor(request));
-                return request.Processed();
-            }))
+        Services.AddMessageHubs(new RouterAddress(), hubConf => hubConf
+            .WithHostedHub<HostAddress>(host => host
+                .WithHandler<SayHelloRequest>((hub, request) =>
+                {
+                    hub.Post(new HelloEvent(), options => options.ResponseFor(request));
+                    return request.Processed();
+                }))
             .WithHostedHub<ClientAddress>(client => client)
-        ));
-
-        Services.AddSingleton(sp =>
-            sp
-                .CreateMessageHub
-                (
-                    new HostAddress(),
-                    hubConf => hubConf.WithHandler<SayHelloRequest>((hub, request) =>
-                    {
-                        hub.Post(new HelloEvent(), options => options.ResponseFor(request));
-                        return request.Processed();
-                    })));
-        Services.AddSingleton(sp => sp.CreateMessageHub(new ClientAddress(), hubConf => hubConf));
+        );
     }
 
     [Fact]
