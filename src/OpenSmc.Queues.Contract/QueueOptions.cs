cf6455d52a5a1393764822ca517e62586d78a04d
Dmitry Kalabin <dkalabin@systemorph.com>
2024-01-25 16:45:07 +0100

    moved OpenSmc.Messaging.Contract project

---
+++ src/OpenSmc.Queues.Contract/QueueOptions.cs
@@ -0,0 +1,6 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Systemorph.Queues;
+
+public record QueueOptions(ExecutionDataflowBlockOptions ExecutionDataflowBlockOptions, int DebounceOnEnqueue = 0);
+public record QueueDependency(object Address);
