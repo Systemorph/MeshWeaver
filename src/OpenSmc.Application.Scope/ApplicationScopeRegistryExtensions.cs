40f5e4576ef705f8a9c573934820deb28b5e24b1
Alexander Yolokhov <ayolokhov@systemorph.com>
2024-01-25 12:21:18 +0300

    add Application.Scope projects

---
+++ src/OpenSmc.Application.Scope/ApplicationScopeRegistryExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+using OpenSmc.Messaging;
+using OpenSmc.Messaging.Hub;
+
+[assembly: InternalsVisibleTo("Systemorph.ViewModel.Hub")]
+[assembly: InternalsVisibleTo("Systemorph.Application.Layout")]
+
+namespace OpenSmc.Application.Scope;
+
+public class ApplicationAddressOptions
+{
+    public ApplicationAddress Address { get; set; }
+}
+
+public record ApplicationVariable(IApplicationScope Scope) // todo delete Scope
+{
+    public IMessageHub Host { get; private set; }
+    public ApplicationAddress Address { get; private set; }
+
+    public void Initialize(IMessageHub host, ApplicationAddress applicationAddress)
+    {
+        Host = host;
+        Address = applicationAddress;
+    }
+}
+
+public static class ApplicationScopeRegistryExtensions
+{
+    public static MessageHubConfiguration ConfigureApplication(this MessageHubConfiguration conf, ApplicationAddress address)
+        => conf.WithServices(services => services.AddSingleton(serviceProvider => new ApplicationVariable(serviceProvider.GetRequiredService<IApplicationScope>())))
+               .WithBuildupAction(hub =>
+                                      hub.ServiceProvider.GetRequiredService<ApplicationVariable>().Initialize(hub, address));
+
+
+    public static MessageHubConfiguration AddExpressionSynchronization(this MessageHubConfiguration conf)
+    {
+        return conf
+                   .AddPlugin<ExpressionSynchronizationPlugin>();
+    }
+
+
+    public static MessageHubConfiguration AddApplicationScope(this MessageHubConfiguration conf)
+    {
+        return conf.AddPlugin<ApplicationScopePlugin>()
+                   .WithServices(s => s.AddSingleton(serviceProvider => serviceProvider.GetRequiredService<IScopeFactory>().ForSingleton().ToScope<IApplicationScope>()))
+                   .RegisterSerializationRule(rule => rule.ForType<IScope>(typeBuilder => typeBuilder.WithSerialization(SerializeScope)));
+    }
+
+
+    public static string SerializeScope()
+    {
+
+    }
+
+
+
+
+}
