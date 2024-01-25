a287d94b7bfe08cbd08029deb761123a968f746a
Alexander Yolokhov <ayolokhov@systemorph.com>
2024-01-25 12:49:54 +0300

    add OpenSmc.Scopes project

---
+++ src/OpenSmc.Scopes/FilterBuilder.cs
@@ -0,0 +1,14 @@
+﻿using OpenSmc.Scopes.Operations;
+
+namespace OpenSmc.Scopes
+{
+    public record FilterBuilder
+    {
+        public FilterBuilder ForScope<TScope>(Func<FilterBuilder<TScope>, FilterBuilder<TScope>> scopeBuilder)
+        {
+            return this with { Parts = Parts.Concat(scopeBuilder(new FilterBuilder<TScope>()).FilterParts).ToArray() };
+        }
+
+        internal IReadOnlyCollection<FilterPart> Parts { get; init; } = Array.Empty<FilterPart>();
+    }
+}
---
+++ src/OpenSmc.Scopes/Synchronization/DelayedTimeout.cs
@@ -0,0 +1,58 @@
+﻿namespace OpenSmc.Scopes.Synchronization;
+
+internal class DelayedTimeout
+{
+    private TaskCompletionSource taskCompletionSource;
+    private Timer timer;
+
+    private readonly long debounceMilliseconds;
+
+    public DelayedTimeout(long debounceMilliseconds)
+    {
+        this.debounceMilliseconds = debounceMilliseconds;
+    }
+
+    public void Touch()
+    {
+        if (timer != null)
+        {
+            timer.Change(debounceMilliseconds, Timeout.Infinite);
+            return;
+        }
+        Initialize();
+    }
+
+
+
+    private void Initialize()
+    {
+        lock (this)
+        {
+            if (timer != null)
+            {
+                timer.Change(debounceMilliseconds, Timeout.Infinite);
+                return;
+            }
+
+            taskCompletionSource = new();
+            timer = new Timer(_ =>
+                              {
+                                  lock (this)
+                                  {
+                                      timer = null;
+                                      taskCompletionSource?.SetResult();
+                                      taskCompletionSource = null;
+                                  }
+                              });
+            timer.Change(debounceMilliseconds, Timeout.Infinite);
+        }
+    }
+
+
+    public Task Trigger => GetTask();
+
+    private Task GetTask()
+    {
+        return taskCompletionSource?.Task ?? Task.CompletedTask;
+    }
+}
