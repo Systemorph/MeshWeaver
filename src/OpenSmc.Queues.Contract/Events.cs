a2f4d7e29c69124663111da3e75f740fec6e929c
Dmitry Kalabin <dkalabin@systemorph.com>
2024-01-25 16:45:07 +0100

    moved OpenSmc.Messaging.Contract project

---
+++ src/OpenSmc.Queues.Contract/Events.cs
@@ -0,0 +1,19 @@
+﻿using OpenSmc.Messaging;
+using OpenSmc.ShortGuid;
+
+namespace OpenSmc.Queues;
+
+public record EnqueueRequest(Func<Task> Action, string Id, int? Debounce) : IRequest<EvaluationFinished>
+{
+    public EnqueueRequest(Func<Task> Action, string Id)
+        : this(Action, Id, 0)
+    {
+    }
+    public EnqueueRequest(Func<Task> Action)
+        : this(Action, Guid.NewGuid().AsString())
+    {
+    }
+}
+
+public record EvaluationFinished(string Id);
+public record ForgetRequest(IReadOnlyCollection<string> Ids);
