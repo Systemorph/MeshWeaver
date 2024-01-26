f7f8800ce41445351b21c7c3a04984d2d9e37a93
Dmitry Bozhenok <dbozhenok@systemorph.com>
2024-01-26 09:46:40 +0000

    Moved Application.Layout further (few errors in AddLayout)

---
+++ src/OpenSmc.Application.Layout/UiActionContext.cs
@@ -0,0 +1,11 @@
+﻿using OpenSmc.Messaging;
+
+namespace OpenSmc.Application.Layout;
+
+public interface IUiActionContext
+{
+    public object Payload { get; }
+    public IMessageHub Hub { get; }
+}
+
+public record UiActionContext(object Payload, IMessageHub Hub) : IUiActionContext;
