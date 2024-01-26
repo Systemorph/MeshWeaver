7b4ba823132795881cbc791061159bb76ec34adc
Dmitry Bozhenok <dbozhenok@systemorph.com>
2024-01-26 14:15:14 +0000

    Removed Application.Layout project

--- src/OpenSmc.Application.Layout/DataBinding/Binding.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OpenSmc.Application.Layout.DataBinding;
-
-public record Binding(string Path)
-{
-    // TODO V10: Fix $type (2023/08/28, Alexander Yolokhov)
-    // [JsonProperty("$type")]
-    // public string Type => "Binding";
-}
