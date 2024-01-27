fc698e6ef30b7e51fa6e50c8838199c3bebd6abb
Roland Bürgi <rbuergi@systemorph.com>
2024-01-27 16:43:57 +0100

    fixing basic tests

--- src/OpenSmc.Messaging.Hub/IMessageHub.cs
+++ src/OpenSmc.Messaging.Hub/IMessageHub.cs
@@ -44,6 +44,8 @@ public interface IMessageHub : IMessageHandlerRegistry, IAsyncDisposable, IDispo
     T Get<T>(string context = "");
     Task AddPluginAsync(IMessageHubPlugin plugin);
     IMessageHub GetHostedHub<TAddress1>(TAddress1 address, Func<MessageHubConfiguration, MessageHubConfiguration> config);
+    IMessageHub WithDisposeAction(Action<IMessageHub> disposeAction);
+    IMessageHub WithDisposeAction(Func<IMessageHub, Task> disposeAction);
 }
 
 
