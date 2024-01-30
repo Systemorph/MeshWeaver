845216817ce27278457dbe6982416a0f1d57632d
Yury Pekishev <ypekishev@systemorph.com>
2024-01-30 19:23:02 +0300

    add OpenSmc.Scheduling

---
+++ src/OpenSmc.Scheduling/IDataScheduler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenSmc.DataSource.Api;
+using OpenSmc.Partition;
+
+namespace OpenSmc.Scheduling
+{
+    public interface IDataScheduler : IResetable
+    {
+        IEnumerable<PartitionChunk> GetModified();
+        IEnumerable<PartitionChunk> GetDeleted();
+        void AddModified(IGrouping<Type, object> group, IPartitionVariable partitionVariable, Func<UpdateOptionsBuilder, UpdateOptionsBuilder> options = default);
+        void AddModified(IEnumerable<object> entities, IPartitionVariable partitionVariable, Func<UpdateOptionsBuilder, UpdateOptionsBuilder> options = default);
+        void AddDeleted(IGrouping<Type, object> group, IPartitionVariable partitionVariable);
+        void AddDeleted(IEnumerable<object> entities, IPartitionVariable partitionVariable);
+    }
+}
