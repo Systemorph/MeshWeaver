28f22d250dd33495920b7168b58a0698d80a7c0d
Ekaterina Mishina <emishina@systemorph.com>
2024-01-30 09:08:35 +0100

    continue fixing pivot test, some started to pass

---
+++ src/OpenSmc.Scopes.DataCubes/DataCubeScopesExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using OpenSmc.DataCubes;
+
+namespace OpenSmc.Scopes.DataCubes
+{
+    public static class DataCubeScopesExtensions
+    {
+        public const string PropertyDimension = "__P";
+        public static bool IsDataCubeInterface(this Type type) => type == typeof(IDataCube) || (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IDataCube<>));
+
+        public static bool IsEnumerableInterface(this Type type) => type == typeof(IEnumerable) || type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEnumerable<>);
+    }
+}
---
+++ src/OpenSmc.Scopes.DataCubes/IsDataCubeScopeMapOverFunctionProvider.cs
@@ -0,0 +1,95 @@
+﻿using System.Reflection;
+using Castle.DynamicProxy;
+using OpenSmc.Arithmetics;
+using OpenSmc.Arithmetics.MapOver;
+using OpenSmc.DataCubes;
+using OpenSmc.Reflection;
+using OpenSmc.Scopes.Operations;
+using OpenSmc.Scopes.Proxy;
+
+namespace OpenSmc.Scopes.DataCubes
+{
+    public class IsDataCubeScopeMapOverFunctionProvider : IMapOverFunctionProvider
+    {
+        private readonly IsScopeMapOverFunctionProvider.MapOverProxyGenerator proxyGenerator = new IsScopeMapOverFunctionProvider.MapOverProxyGenerator();
+        
+        public Delegate GetDelegate(Type type, ArithmeticOperation method) => (Delegate)CreateMapOverScopeMethod.MakeGenericMethod(type, type.GetDataCubeElementType()).Invoke(this, new object[] { method });
+
+        private static readonly MethodInfo CreateMapOverScopeMethod = ReflectionHelper.GetMethodGeneric<IsDataCubeScopeMapOverFunctionProvider>(x => x.CreateMapOverScope<IDataCube<object>, object>(ArithmeticOperation.Plus));
+
+        internal Func<double, TScope, TScope> CreateMapOverScope<TScope, TElement>(ArithmeticOperation method)
+            where TScope : class, IDataCube<TElement>
+        {
+            return (scalar, scope) =>
+                   {
+                       var scopeInterceptor = proxyGenerator.GetMapOverInterceptor(scope, method, scalar);
+                       var dataCubeScopeInterceptor = new DataCubeScopeMapOverInterceptor<TScope, TElement>(scope, method, scalar);
+                       return proxyGenerator.CreateProxy<TScope>(dataCubeScopeInterceptor, scopeInterceptor);
+                   };
+        }
+    }
+
+    internal class DataCubeScopeMapOverInterceptor<TScope, TElement> : ScopeInterceptorBase
+        where TScope : IDataCube<TElement>
+    {
+        private readonly IDataCube<TElement> scope;
+        private readonly double scalar;
+        private readonly Func<double, TElement, TElement> mapOverFunction;
+
+        public DataCubeScopeMapOverInterceptor(TScope scope, ArithmeticOperation method, double scalar)
+        {
+            this.scope = scope;
+            this.scalar = scalar;
+            mapOverFunction = MapOverFields.GetMapOver<TElement>(method);
+        }
+
+
+        public override void Intercept(IInvocation invocation)
+        {
+            if (invocation.Method.DeclaringType.IsEnumerableInterface())
+            {
+                var enumerator = GetEnumerator();
+                invocation.ReturnValue = enumerator;
+                return;
+            }
+
+            if (invocation.Method.DeclaringType.IsDataCubeInterface())
+            {
+                switch (invocation.Method.Name)
+                {
+                    case nameof(IDataCube.GetSlices):
+                        invocation.ReturnValue = GetSlices(invocation);
+                        return;
+                    case nameof(IDataCube.GetDimensionDescriptors):
+                        invocation.ReturnValue = GetDimensionDescriptors(invocation);
+                        return;
+                    case nameof(IDataCube.Filter):
+                        invocation.ReturnValue =
+                            ((IFilterable)invocation.Proxy).Filter(((string, object)[])invocation.Arguments.First());
+                        return;
+                }
+            }
+
+
+            invocation.Proceed();
+        }
+
+        private object GetDimensionDescriptors(IInvocation invocation)
+        {
+            var isByRow = (bool)invocation.Arguments.First();
+            var dimensions = (string[])invocation.Arguments.Skip(1).First(); 
+            return scope.GetDimensionDescriptors(isByRow, dimensions);
+        }
+
+        private object GetSlices(IInvocation invocation)
+        {
+            return scope.GetSlices((string[])invocation.Arguments.First())
+                        .Select(x => new DataSlice<TElement>(mapOverFunction(scalar, x.Data), x.Tuple));
+        }
+
+        private object GetEnumerator()
+        {
+            return scope.Select(e => mapOverFunction(scalar, e)).GetEnumerator();
+        }
+    }
+}
