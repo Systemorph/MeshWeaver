eeef5ff60fb525aa038b8de0e83c3ad943024e8c
Yury Pekishev <ypekishev@systemorph.com>
2024-01-30 19:15:23 +0300

    add OpenSmc.Partition

---
+++ src/OpenSmc.Partition/ModuleSetup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using OpenSmc.ServiceProvider;
+using OpenSmc.DataSource.Api;
+
+[assembly: OpenSmc.Partition.ModuleSetup]
+
+namespace OpenSmc.Partition;
+
+public class ModuleSetup : Attribute, IModuleRegistry
+{
+    public void Register(IServiceCollection services)
+    {
+        services.AddTransient<IPartitionVariable, PartitionVariable>();
+    }
+}
---
+++ src/OpenSmc.Partition/PartitionKeyAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OpenSmc.Partition
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class PartitionKeyAttribute : Attribute
+    {
+        public PartitionKeyAttribute(string name, Type type)
+        {
+            PartitionName = name;
+            PartitionType = type;
+        }
+
+        public PartitionKeyAttribute(string name)
+        {
+            PartitionName = name;
+        }
+
+        public PartitionKeyAttribute(Type type)
+        {
+            PartitionType = type;
+        }
+
+        public PartitionKeyAttribute()
+        {
+        }
+
+        //rather optional for partitions of EntityTypes
+        public Type PartitionType;
+        public string PartitionName;
+    }
+}
