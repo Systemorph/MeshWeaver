995bcda59719832aeb677f26f00ea6e26ed98274
Ekaterina Mishina <emishina@systemorph.com>
2024-01-30 09:08:35 +0100

    continue fixing pivot test, some started to pass

---
+++ src/OpenSmc.Scopes.DataCubes/DimensionProperty.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using AspectCore.Extensions.Reflection;
+
+namespace OpenSmc.Scopes.DataCubes
+{
+    internal class DimensionProperty
+    {
+        public DimensionProperty(string dimensionSystemName, Type dimensionType, PropertyInfo property)
+        {
+            DimensionSystemName = dimensionSystemName;
+            DimensionType = dimensionType;
+            Property = property;
+            Reflector = property.GetReflector();
+        }
+
+        public PropertyInfo Property { get; }
+        public Type DimensionType { get; }
+        public string DimensionSystemName { get; }
+
+        public PropertyReflector Reflector { get; }
+    }
+}
