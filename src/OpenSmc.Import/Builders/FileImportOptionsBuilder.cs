11639419eaa8d1ff23a667f25746085bcc997ca9
vpetrov <vpetrov@systemorph.com>
2024-01-31 17:20:54 +0300

    Activities and Import (WIP)

---
+++ src/OpenSmc.Import/Builders/FileImportOptionsBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using OpenSmc.Activities;
+using OpenSmc.DataSetReader;
+using OpenSmc.DataSource.Abstractions;
+using OpenSmc.DataStructures;
+using OpenSmc.DomainDesigner.Abstractions;
+using OpenSmc.FileStorage;
+using OpenSmc.Import.Mapping;
+using OpenSmc.Import.Options;
+
+namespace OpenSmc.Import.Builders
+{
+    public record FileReaderImportOptionsBuilder : ImportOptionsBuilder
+    {
+        private string FilePath { get; }
+
+        internal FileReaderImportOptionsBuilder(IActivityService activityService,
+                                                IDataSetReaderVariable dataSetReaderVariable, 
+                                                IMappingService mappingService,
+                                                IFileReadStorage storage,
+                                                CancellationToken cancellationToken, 
+                                                DomainDescriptor domain, 
+                                                IDataSource targetSource,
+                                                IServiceProvider serviceProvider,
+                                                Dictionary<string, Func<ImportOptions, IDataSet, Task>> importFormatFunctions,
+                                                ImmutableList<Func<object, ValidationContext, Task<bool>>> defaultValidations, 
+                                                string filePath)
+            : base(activityService, dataSetReaderVariable, mappingService, storage, cancellationToken, domain, targetSource, serviceProvider, importFormatFunctions, defaultValidations)
+        {
+            FilePath = filePath;
+        }
+
+        protected override async Task<(IDataSet DataSet, string Format)> GetDataSetAsync()
+        {
+            if (Storage == null)
+                throw new ArgumentException("Please specify file storage to get file from.");
+
+            var stream = await Storage.ReadAsync(FilePath, CancellationToken);
+            var optionsBuilder = DataSetReaderOptionsBuilderFunc(DataSetReaderVariable.ReadFromStream(stream));
+            var contentType = ((DataSetReaderOptionsBuilder)optionsBuilder).ContentType ?? Path.GetExtension(FilePath);
+            return await optionsBuilder.WithContentType(contentType).ExecuteAsync();
+        }
+
+        public override FileImportOptions GetImportOptions()
+        {
+            return new FileImportOptions(FilePath, 
+                                         Format, 
+                                         Domain, 
+                                         TargetDataSource, 
+                                         Storage, 
+                                         Validations,
+                                         TableMappingBuilders.ToImmutableDictionary(x => x.Key,
+                                                                                    x => x.Value.GetMapper()),
+                                         IgnoredTables, 
+                                         IgnoredTypes, 
+                                         IgnoredColumns, 
+                                         SnapshotModeEnabled);
+        }
+    }
+}
---
+++ src/OpenSmc.Import/Builders/ImportTypeOptionBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using OpenSmc.DataStructures;
+
+namespace OpenSmc.Import.Builders;
+
+public interface IImportTypeOptionBuilder
+{
+    IImportTypeOptionBuilder SnapshotMode();
+}
+
+//this is needed to disable pass into WithType function with only default mappings
+public interface IImportMappingTypeOptionBuilder<T> : IImportTypeOptionBuilder
+{
+    IImportMappingTypeOptionBuilder<T> MapProperty<TProperty>(Expression<Func<T, TProperty>> selector, Expression<Func<IDataSet, IDataRow, TProperty>> propertyMapping);
+    new IImportMappingTypeOptionBuilder<T> SnapshotMode();//this one must return same type, otherwise after calling of this method we can not call other methods
+}
