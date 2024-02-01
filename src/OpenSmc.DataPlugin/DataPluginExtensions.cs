8faffed6ba2326bd6ece5f9532b3d07200a8a57e
Andrey Katz <akatz@systemorph.com>
2024-02-01 18:09:59 +0100

    organize DataPlugin

---
+++ src/OpenSmc.DataPlugin/DataPluginExtensions.cs
@@ -0,0 +1,10 @@
+﻿using OpenSmc.Messaging;
+
+namespace OpenSmc.DataPlugin;
+
+public static class DataPluginExtensions
+{
+    public static MessageHubConfiguration AddData(this MessageHubConfiguration configuration,
+        Func<DataPluginConfiguration, DataPluginConfiguration> dataConfiguration)
+        => configuration.AddPlugin(hub => new DataPlugin(hub, configuration, dataConfiguration));
+}
