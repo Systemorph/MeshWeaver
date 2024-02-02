09205b218e33ae44171b9d9e993c4524f57cf828
Alexander Kravets <akravets@systemorph.com>
2024-02-02 17:21:29 +0300

    playground draft

---
+++ ui/packages/playground/src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { PlaygroundPage } from "./PlaygroundPage.tsx";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <PlaygroundPage/>
+)
+
