17a7029898541125617bee7931971caf2e8260ca
Alexander Yolokhov <ayolokhov@systemorph.com>
2024-02-06 17:16:09 +0300

    MessageHubPluginTest (WIP)

--- test/OpenSmc.Messaging.Hub.Test/MessageHubPluginTest.cs
+++ test/OpenSmc.Messaging.Hub.Test/MessageHubPluginTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Immutable;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OpenSmc.Fixture;
 using OpenSmc.ServiceProvider;
 using Xunit;
@@ -11,11 +12,12 @@ namespace OpenSmc.Messaging.Hub.Test;
 public class MessageHubPluginTest : TestBase
 {
     protected record Address;
+    public record GetEvents : IRequest<ImmutableList<object>>;
     public record MyEvent : IRequest<object>;
-    MyPlugin plugin;
 
-    class MyPlugin : MessageHubPlugin
+    class MyPlugin : MessageHubPlugin, IMessageHandler<GetEvents>
     {
+        [Inject] private ILogger<MyPlugin> logger;
         public MyPlugin(IMessageHub hub) : base(hub)
         {
             Register(HandleMessage);
@@ -25,15 +27,24 @@ public class MessageHubPluginTest : TestBase
 
         public override async Task StartAsync()
         {
+            logger.LogInformation("Starting");
             Events = Events.Add("Starting");
             await Task.Delay(1000);
             Events = Events.Add("Started");
+            logger.LogInformation("Started");
         }
 
         public IMessageDelivery HandleMessage(IMessageDelivery request)
         {
+            logger.LogInformation("Handled");
             Events = Events.Add("Handled");
             Hub.Post("Handled", o => o.ResponseFor(request));
+            return request.Processed();
+        }
+
+        public IMessageDelivery HandleMessage(IMessageDelivery<GetEvents> request)
+        {
+            Hub.Post(Events, o => o.ResponseFor(request));
             return request.Processed();
         }
     }
@@ -43,14 +54,15 @@ public class MessageHubPluginTest : TestBase
     public MessageHubPluginTest(ITestOutputHelper output) : base(output)
     {
         Services.AddSingleton<IMessageHub>(sp => sp.CreateMessageHub(new Address(), 
-            conf => conf.AddPlugin(hub => plugin = new MyPlugin(hub))));
+            conf => conf.AddPlugin<MyPlugin>()));
     }
 
     [Fact]
     public async Task StartBeforeHandle()
     {
         await Hub.AwaitResponse(new MyEvent());
-        plugin.Events.Should().BeEquivalentTo(["Starting", "Started", "Handled"], o => o.WithStrictOrdering());
+        var events = await Hub.AwaitResponse(new GetEvents());
+        events.Message.Should().BeEquivalentTo(["Starting", "Started", "Handled"], o => o.WithStrictOrdering());
     }
 
     public override async Task DisposeAsync()
