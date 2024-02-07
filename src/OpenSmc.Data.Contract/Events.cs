937dad682117606c23eef829ce01c0da1e966a09
Dmitry Kalabin <dkalabin@systemorph.com>
2024-02-07 10:39:16 +0100

    moved related events to OpenSmc.Data.Contract project

---
+++ src/OpenSmc.Data.Contract/Events.cs
@@ -0,0 +1,19 @@
+﻿using OpenSmc.Messaging;
+
+namespace OpenSmc.Data;
+
+public record GetManyRequest<T>() : IRequest<GetManyResponse<T>>
+{
+    public int Page { get; init; }
+    public int? PageSize { get; init; }
+    public object Options { get; init; }
+};
+
+public record GetManyResponse<T>(int Total, IReadOnlyCollection<T> Items)
+{
+    public static GetManyResponse<T> Empty() => new(0, Array.Empty<T>());
+}
+
+public record UpdateDataRequest(IReadOnlyCollection<object> Elements) : IRequest<DataChanged>;
+
+public record DeleteDataRequest(IReadOnlyCollection<object> Elements) : IRequest<DataDeleted>;
