f4e8376fcb31873773b4ce9ddcdf28cf24a6c0ec
Dmitry Kalabin <dkalabin@systemorph.com>
2024-02-09 11:03:02 +0100

    added new IWorkspace interface

---
+++ src/OpenSmc.Data/IWorkspace.cs
@@ -0,0 +1,12 @@
+﻿using OpenSmc.DataSource.Abstractions;
+
+namespace OpenSmc.Data;
+
+public interface IWorkspace : IQuerySource
+{
+    void Update<T>(IEnumerable<T> instances, Func<UpdateOptionsBuilder, UpdateOptionsBuilder> options = default);
+    void Update<T>(T instance, Func<UpdateOptionsBuilder, UpdateOptionsBuilder> options = default);
+    void Delete<T>(IEnumerable<T> instances);
+    void Delete<T>(T instance);
+    void Commit(Func<CommitOptionsBuilder, CommitOptionsBuilder> options = default);
+}
