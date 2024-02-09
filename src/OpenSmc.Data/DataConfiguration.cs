ca145baa7960ab04e6f3a2aa028bc58e6414f683
Roland Bürgi <rbuergi@systemorph.com>
2024-02-09 12:13:07 +0100

    Iterating on design

--- src/OpenSmc.Data/DataConfiguration.cs
+++ src/OpenSmc.Data/DataConfiguration.cs
@@ -1,15 +1,28 @@
-﻿using OpenSmc.Messaging;
+﻿using OpenSmc.DataSource.Abstractions;
+using OpenSmc.Messaging;
+using System.Collections.Immutable;
+using OpenSmc.Collections;
 
 namespace OpenSmc.Data;
 
 public record DataConfiguration
 {
     internal Func<IMessageHub, IMessageHubPlugin> CreateSatellitePlugin { get; init; }
-    internal WorkspaceConfiguration Workspace { get; init; }
 
-    public DataConfiguration WithWorkspace(Func<WorkspaceConfiguration, WorkspaceConfiguration> configureWorkspace)
-        => this with { Workspace = configureWorkspace(new WorkspaceConfiguration()) };
 
-    public DataConfiguration WithPersistence(Func<DataPersistenceConfiguration, DataPersistenceConfiguration> configurePersistence)
-        => this with { CreateSatellitePlugin = persistenceHub => new DataPersistencePlugin(persistenceHub, configurePersistence) };
+    internal ImmutableDictionary<Type, TypeConfiguration> TypeConfigurations { get; init; } = ImmutableDictionary<Type, TypeConfiguration>.Empty;
+
+    public DataConfiguration WithType<T>(
+        Func<TypeConfiguration<T>, TypeConfiguration<T>> typeConfigurator)
+        => this with { TypeConfigurations = TypeConfigurations.SetItem(typeof(T), typeConfigurator.Invoke(new TypeConfiguration<T>())) };
+
+
+    public DataConfiguration WithType<T>(Func<T, object> key, IDataSource dataSource) =>
+        WithType<T>(o => o.WithKey(key)
+            .WithInitialization(async () => await dataSource.Query<T>().ToArrayAsync())
+            .WithSave(async entities => await dataSource.UpdateAsync(entities))
+            .WithDelete(async ids => await dataSource.DeleteAsync(ids))
+        );
+
+
 }
--- src/OpenSmc.Data/TypeConfiguration.cs
+++ src/OpenSmc.Data/TypeConfiguration.cs
@@ -1,19 +1,37 @@
-﻿using System.Collections.Generic;
-
-namespace OpenSmc.Data;
+﻿namespace OpenSmc.Data;
 
 public abstract record TypeConfiguration()
 {
     public abstract Task<IEnumerable<object>> DoInitialize();
+    public abstract object GetKey(object instance);
 }
 
-public record TypeConfiguration<T>(
-    Func<Task<IReadOnlyCollection<T>>> Initialize,
-    Func<IEnumerable<T>, Task> Save,
-    Func<IEnumerable<T>, Task> Delete) : TypeConfiguration
+public record TypeConfiguration<T> : TypeConfiguration
 {
+
     public override async Task<IEnumerable<object>> DoInitialize()
     {
-        return (await Initialize()).Cast<object>().ToArray();
+        return (await Initialization()).Cast<object>().ToArray();
     }
+
+    public override object GetKey(object instance)
+        => Key((T)instance);
+
+    internal Func<T, object> Key { get; init; }
+
+    public TypeConfiguration<T> WithKey(Func<T, object> key)
+        => this with { Key = key };
+    internal Func<Task<IReadOnlyCollection<T>>> Initialization { get; init; }
+    public TypeConfiguration<T> WithInitialization(Func<Task<IReadOnlyCollection<T>>> initialization)
+        => this with { Initialization = initialization };
+    internal Func<IEnumerable<T>, Task> Save { get; init; }
+
+    public TypeConfiguration<T> WithSave(Func<IEnumerable<T>, Task> save) => this with { Save = save };
+
+    internal Func<IEnumerable<object>, Task> Delete { get; init; }
+    public TypeConfiguration<T> WithDelete(Func<IEnumerable<object>, Task> delete) => this with
+    {
+        Delete = delete
+    };
+
 }
