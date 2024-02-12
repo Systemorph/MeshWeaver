0d32b9d40488240ff7203a91d0819271c381d9a0
Alexander Kravets <akravets@systemorph.com>
2024-02-12 12:59:56 +0300

    introducing message hub context

---
+++ ui/packages/playground/src/server/LayoutHub.ts
@@ -0,0 +1,11 @@
+import { SubjectHub } from "@open-smc/message-hub/src/SubjectHub";
+
+export const layoutAddress = "layout";
+
+export class LayoutHub extends SubjectHub {
+    constructor() {
+        super();
+        // TODO: TBD (2/12/2024, akravets)
+    }
+}
+
