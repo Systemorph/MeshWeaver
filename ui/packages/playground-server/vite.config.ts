184524a1753f4aa3c8678ba31b17607fcb841ff1
Alexander Kravets <akravets@systemorph.com>
2024-02-14 18:02:49 +0300

    notebook list in sidemenu

--- ui/packages/playground-server/vite.config.ts
+++ ui/packages/playground-server/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
             // into your library
-            external: ['rxjs'],
+            external: ['rxjs', 'fs'],
         },
     }
 });
