389fa988f3e3172d5335e01c471431cbce5b54fc
Alexander Kravets <akravets@systemorph.com>
2024-02-16 11:12:51 +0300

    cleanup

--- ui/packages/playground/src/PlaygroundPage.tsx
+++ ui/packages/playground/src/PlaygroundPage.tsx
@@ -1,7 +1,6 @@
 import { NotificationProvider } from "@open-smc/application/src/notifications/NotificationProvider";
 import { ControlStarter } from "@open-smc/application/src/ControlStarter";
 import { ViteHmrTransport } from './ViteHmrTransport';
-import './App.css'
 import { LayoutHub } from "@open-smc/application/src/LayoutHub";
 import { MessageRouter } from "@open-smc/application/src/MessageRouter";
 
