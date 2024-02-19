496e29f38928f9f67022635231973db1b95568c0
Alexander Kravets <akravets@systemorph.com>
2024-02-19 16:05:09 +0300

    sending full pathname via SetAreaRequest

--- ui/packages/application/src/AppPage.tsx
+++ ui/packages/application/src/AppPage.tsx
@@ -1,22 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { MessageRouter } from "./MessageRouter";
 import { LayoutHub } from "./LayoutHub";
-import { NotificationProvider } from "./notifications/NotificationProvider";
 import { ControlStarter } from "./ControlStarter";
 import { SignalrTransport } from "./SignalrTransport";
 
 export function AppPage() {
-    const {projectId, id} = useParams();
+    const {pathname} = useLocation();
 
     return (
-        <NotificationProvider>
-            <SignalrTransport>
-                <MessageRouter>
-                    <LayoutHub>
-                        <ControlStarter area={"app"} path={`application/${projectId}/${id}`}/>
-                    </LayoutHub>
-                </MessageRouter>
-            </SignalrTransport>
-        </NotificationProvider>
+        <SignalrTransport>
+            <MessageRouter>
+                <LayoutHub>
+                    <ControlStarter area={"app"} path={pathname}/>
+                </LayoutHub>
+            </MessageRouter>
+        </SignalrTransport>
     );
 }
