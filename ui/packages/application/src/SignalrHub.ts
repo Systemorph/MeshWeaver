cdd1b8c88d7927fddd43cb1867662518069bcc32
Alexander Kravets <akravets@systemorph.com>
2024-02-22 16:46:02 +0300

    unifying signalr method name

--- ui/packages/application/src/SignalrHub.ts
+++ ui/packages/application/src/SignalrHub.ts
@@ -3,10 +3,11 @@ import { Observable } from "rxjs";
 import { MessageDelivery } from "@open-smc/message-hub/src/api/MessageDelivery";
 import { MessageHub } from "@open-smc/message-hub/src/api/MessageHub";
 
+const methodName = "DeliverMessage";
+
 export class SignalrHub extends Observable<MessageDelivery> implements MessageHub {
     constructor(private connection: SignalrConnection) {
         super(subscriber => {
-            const methodName = "HandleEvent";
             const handler = (args: any) => subscriber.next(args);
             connection.connection.on(methodName, handler);
             subscriber.add(() => connection.connection.off(methodName, handler));
@@ -20,7 +21,7 @@ export class SignalrHub extends Observable<MessageDelivery> implements MessageHu
     }
 
     next(value: MessageDelivery) {
-        void this.connection.connection.invoke("DeliverMessage", value);
+        void this.connection.connection.invoke(methodName, value);
     }
 }
 
