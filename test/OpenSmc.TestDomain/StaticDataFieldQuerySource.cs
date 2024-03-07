c409f83a7127fee5f53f45f54fc50977188900a8
Ekaterina Mishina <emishina@systemorph.com>
2024-03-07 13:47:24 +0100

    clean up

--- test/OpenSmc.TestDomain/StaticDataFieldQuerySource.cs
+++ test/OpenSmc.TestDomain/StaticDataFieldQuerySource.cs
@@ -5,17 +5,13 @@ namespace OpenSmc.TestDomain
 {
     public class StaticDataFieldQuerySource : IQuerySource
     {
-        private IQueryable<T> Query<T>() where T : class
+        public IReadOnlyCollection<T> GetData<T>() where T : class
         {
             var dataProperty = typeof(T).GetField("Data", BindingFlags.Public | BindingFlags.Static);
             if (dataProperty == null)
                 return null;
-            return ((IEnumerable<T>)dataProperty.GetValue(null))?.AsQueryable();
-        }
 
-        public IReadOnlyCollection<T> GetData<T>() where T : class
-        {
-            return Query<T>()?.ToList().AsReadOnly();
+            return ((IEnumerable<T>)dataProperty.GetValue(null))?.ToList().AsReadOnly();
         }
     }
 }
