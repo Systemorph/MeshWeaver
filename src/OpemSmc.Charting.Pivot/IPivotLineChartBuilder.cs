63df79db47cf2b027588f15c16812cabacd61cca
Ekaterina Mishina <emishina@systemorph.com>
2024-03-13 14:40:59 +0100

    add charting.pivot

---
+++ src/OpemSmc.Charting.Pivot/IPivotLineChartBuilder.cs
@@ -0,0 +1,8 @@
+﻿using OpenSmc.Charting.Builders.OptionsBuilders;
+
+namespace OpenSmc.Charting.Pivot;
+
+public interface IPivotLineChartBuilder : IPivotArrayChartBuilder
+{
+    IPivotLineChartBuilder WithRangeOptionsBuilder(Func<LineOptionsBuilder, LineOptionsBuilder> func);
+}
