a13d3a6f9abcf35cffa7776928a70e10f3670139
Alexander Kravets <akravets@systemorph.com>
2024-04-01 10:28:45 +0300

    moving deserialize

--- ui/packages/application/src/contract/deserialize.ts
+++ ui/packages/application/src/contract/deserialize.ts
@@ -1,14 +1,12 @@
 import { map, Observable } from "rxjs";
-import { hasType } from "../contract/hasType";
+import { hasType } from "./hasType";
 import { getConstructor } from "@open-smc/utils/src/contractMessage";
 import { assign, cloneDeepWith } from "lodash-es";
 
 export const deserialize = <T>() =>
     (source: Observable<T>) =>
         source
-            .pipe(
-                map(tryDeserialize)
-            );
+            .pipe(map(tryDeserialize));
 
 const tryDeserialize = <T>(value: T): T => {
     return cloneDeepWith(
