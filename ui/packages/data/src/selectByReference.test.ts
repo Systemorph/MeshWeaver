7669db38f8eea8126016902ae0d2d2f966d2e4b0
Alexander Kravets <akravets@systemorph.com>
2024-04-02 17:39:31 +0300

    selectByReference fix to handle empty path properly

---
+++ ui/packages/data/src/selectByReference.test.ts
@@ -0,0 +1,24 @@
+import { expect, test, describe, jest } from "@jest/globals";
+import { selectByReference } from "./selectByReference";
+import { JsonPathReference } from "./data.contract";
+
+describe("selectByReference", () => {
+    test("data is string", () => {
+        expect(selectByReference("", new JsonPathReference("$"))).toBe("");
+        expect(selectByReference("foo", new JsonPathReference("$"))).toBe("foo");
+    });
+
+    test("data is number", () => {
+        expect(selectByReference(0, new JsonPathReference("$"))).toBe(0);
+        expect(selectByReference(42, new JsonPathReference("$"))).toBe(42);
+    });
+
+    test("value is string", () => {
+        expect(selectByReference({ name: "foo" }, new JsonPathReference("$.name"))).toBe("foo");
+    });
+
+    test("value is number", () => {
+        expect(selectByReference({ age: 0 }, new JsonPathReference("$.age"))).toBe(0);
+        expect(selectByReference({ age: 42 }, new JsonPathReference("$.age"))).toBe(42);
+    });
+});
--- ui/packages/data/src/selectByReference.ts
+++ ui/packages/data/src/selectByReference.ts
@@ -1,11 +1,16 @@
 import { JsonPathReference, WorkspaceReference } from "./data.contract";
 import { JSONPath } from "jsonpath-plus";
 
-export const selectByReference = (value: any, reference: WorkspaceReference) => {
+export const selectByReference = (data: any, reference: WorkspaceReference) => {
     if (reference instanceof JsonPathReference) {
+        // jsonpath-plus returns undefined if data is empty string or 0
+        if (reference.path === "$") {
+            return data;
+        }
+
         return JSONPath(
             {
-                json: value,
+                json: data,
                 path: reference.path,
                 wrap: false
             }
