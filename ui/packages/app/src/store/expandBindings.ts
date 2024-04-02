4981a5da0c13e5f0f2d1eb85eb457462551e22d5
Alexander Kravets <akravets@systemorph.com>
2024-04-02 17:41:22 +0300

    dont pass undefined to cloneDeep to avoid default behavior

--- ui/packages/app/src/store/expandBindings.ts
+++ ui/packages/app/src/store/expandBindings.ts
@@ -4,11 +4,11 @@ import { Binding } from "@open-smc/layout/src/contract/Binding";
 import { selectByReference } from "@open-smc/data/src/selectByReference";
 
 // TODO: respect parentDataContext (3/26/2024, akravets)
-export const expandBindings = <T>(props: T, parentDataContext: unknown) =>
+export const expandBindings = <T>(props: T, parentDataContext?: unknown) =>
     (source: unknown): T =>
         cloneDeepWith(
             props,
             value =>
                 value instanceof Binding
-                    ? selectByReference(source, new JsonPathReference(value.path)) : undefined
+                    ? selectByReference(source, new JsonPathReference(value.path)) ?? null : undefined
         );
