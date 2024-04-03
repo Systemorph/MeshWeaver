f152449b5d30b6a36d5fb26384290666d66893d3
Roland Bürgi <rbuergi@systemorph.com>
2024-04-03 12:41:03 +0200

    bug fixes

--- src/OpenSmc.Layout/LayoutAreaReference.cs
+++ src/OpenSmc.Layout/LayoutAreaReference.cs
@@ -8,6 +8,11 @@ public record LayoutAreaReference(string Area) : WorkspaceReference<LayoutAreaCo
     public object Options { get; init; }
 }
 
+
+/// <summary>
+/// If you are interested in a particular control, use collection.areas['area1/subarea']
+/// </summary>
+/// <param name="Reference"></param>
 public record LayoutAreaCollection(LayoutAreaReference Reference)
 {
     public ImmutableDictionary<string, UiControl> Areas { get; init; } = ImmutableDictionary<string, UiControl>.Empty;
