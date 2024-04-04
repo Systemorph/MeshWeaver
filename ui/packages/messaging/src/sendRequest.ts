aa8a473ca83bc0ed75ba3ed152d43e5908fe8427
Alexander Kravets <akravets@systemorph.com>
2024-04-04 13:45:51 +0300

    request response

---
+++ ui/packages/messaging/src/sendRequest.ts
@@ -0,0 +1,24 @@
+import { v4 } from "uuid";
+import { filter, map, take } from "rxjs";
+import { MessageHub } from "./api/MessageHub";
+import { messageOfType } from "./operators/messageOfType";
+import { Request } from "./api/Request";
+import { unpack } from "./operators/unpack";
+
+export const sendRequest = <T>(hub: MessageHub, message: Request<T>) => {
+    const id = v4();
+
+    const result$ =
+        hub
+            .pipe(filter(messageOfType(message.responseType)))
+            .pipe(filter(({properties}) => properties?.requestId === id))
+            .pipe(take(1))
+            .pipe(map(unpack));
+
+    hub.next({
+        id,
+        message
+    });
+
+    return result$;
+}
