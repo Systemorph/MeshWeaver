1d9787298326ad77c42233f0aa3280fac0b0e868
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-08 14:25:49 +0200

    added extension method to register SignalR-related dependencies in DI

---
+++ src/OpenSmc.Application.SignalR/SignalRExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OpenSmc.Application.SignalR;
+
+public static class SignalRExtensions
+{
+    public static IServiceCollection ConfigureApplicationSignalR(this IServiceCollection services)
+    {
+        services.AddSignalR(o =>
+            {
+                o.EnableDetailedErrors = true; // TODO: False for Prod environment (2021/05/14, Alexander Yolokhov)
+                o.MaximumReceiveMessageSize = 400000; // TODO: see what's recommended size (2021/12/07, Alexander Kravets)
+            });
+
+        return services;
+    }
+}
