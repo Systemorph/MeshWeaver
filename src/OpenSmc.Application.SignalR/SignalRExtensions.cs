2f684ff99396f98a2e275ef0b06cf286460e1f3c
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-08 14:44:07 +0200

    added UseApplicationSignalR() method to configure SignalR endpoint for application

--- src/OpenSmc.Application.SignalR/SignalRExtensions.cs
+++ src/OpenSmc.Application.SignalR/SignalRExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace OpenSmc.Application.SignalR;
 
@@ -15,5 +16,15 @@ public static class SignalRExtensions
             });
 
         return services;
+    }
+
+    public static IApplicationBuilder UseApplicationSignalR(this IApplicationBuilder app)
+    {
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHub<ApplicationHub>(DefaultSignalREndpoint);
+        });
+
+        return app;
     }
 }
