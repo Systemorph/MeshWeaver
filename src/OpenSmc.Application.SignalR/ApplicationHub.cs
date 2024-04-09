a23b0cb6017599139683a9d06dac8c1dd4cbf9f0
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-09 09:57:19 +0200

    added logging on Connect/disconnect to ApplicationHub

--- src/OpenSmc.Application.SignalR/ApplicationHub.cs
+++ src/OpenSmc.Application.SignalR/ApplicationHub.cs
@@ -5,4 +5,15 @@ namespace OpenSmc.Application.SignalR;
 
 public class ApplicationHub(ILogger<ApplicationHub> logger) : Hub
 {
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        logger.LogDebug("Attempt to disconnect for connection {ConnectionId} with exception {exception}", Context.ConnectionId, exception);
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        logger.LogDebug("Attempt to make new SignalR connection {ConnectionId} ", Context.ConnectionId);
+        return base.OnConnectedAsync();
+    }
 }
