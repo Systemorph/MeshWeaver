9efbde2a40ae9f96178f84eca9078444088701f4
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-12 13:22:09 +0200

    added empty RequestResponse() test method

--- test/OpenSmc.Application.SignalR.Integration.Test/SignalRBasicTest.cs
+++ test/OpenSmc.Application.SignalR.Integration.Test/SignalRBasicTest.cs
@@ -1,5 +1,16 @@
-﻿namespace OpenSmc.Application.SignalR.Integration.Test;
+﻿using Xunit;
+
+namespace OpenSmc.Application.SignalR.Integration.Test;
 
 public class SignalRBasicTest
 {
+    [Fact]
+    public void RequestResponse()
+    {
+        // arrange
+
+        // act
+
+        // assert
+    }
 }
