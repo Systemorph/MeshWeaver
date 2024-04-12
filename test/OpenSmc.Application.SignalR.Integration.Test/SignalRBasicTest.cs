1fd4a14aafbbb4a831424c834bee06ea70f61044
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-12 18:28:28 +0200

    added HubConnection to SignalRBasicTest and corresponded code to initialize it

--- test/OpenSmc.Application.SignalR.Integration.Test/SignalRBasicTest.cs
+++ test/OpenSmc.Application.SignalR.Integration.Test/SignalRBasicTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.DependencyInjection;
 using OpenSmc.Application.Host;
 using OpenSmc.Fixture;
@@ -7,6 +8,7 @@ using OpenSmc.Messaging;
 using OpenSmc.ServiceProvider;
 using Xunit;
 using Xunit.Abstractions;
+using static OpenSmc.Application.SignalR.SignalRExtensions;
 
 namespace OpenSmc.Application.SignalR.Integration.Test;
 
@@ -17,6 +19,7 @@ public class SignalRBasicTest : TestBase, IClassFixture<WebApplicationFactory<Pr
     private static readonly ApplicationAddress ApplicationAddress = new("testApp", "dev");
 
     [Inject] private IMessageHub Client;
+    private HubConnection Connection { get; set; }
 
     public SignalRBasicTest(WebApplicationFactory<Program> webAppFactory, ITestOutputHelper toh) : base(toh)
     {
@@ -41,6 +44,18 @@ public class SignalRBasicTest : TestBase, IClassFixture<WebApplicationFactory<Pr
     public async override Task InitializeAsync()
     {
         await base.InitializeAsync();
+
+        var uriBuilder = new UriBuilder(webAppFactory.Server.BaseAddress) { Path = DefaultSignalREndpoint, };
+        Connection = new HubConnectionBuilder()
+            .WithUrl(uriBuilder.Uri,
+                o =>
+                {
+                    o.HttpMessageHandlerFactory = _ => webAppFactory.Server.CreateHandler();
+                }
+            )
+            .Build();
+
+        await Connection.StartAsync();
     }
 
     record TestRequest : IRequest<TestResponse>;
