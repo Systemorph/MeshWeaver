396176c0bc89da6e7524315a9811a08008ead96d
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-16 16:03:59 +0200

    added dummy implementation of MessageHubServiceProviderFactory
    (which implements IServiceProviderFactory<IServiceCollection> interface)

---
+++ src/OpenSmc.Hosting/MessageHubServiceProviderFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OpenSmc.Hosting;
+
+internal class MessageHubServiceProviderFactory : IServiceProviderFactory<IServiceCollection>
+{
+    public IServiceCollection CreateBuilder(IServiceCollection services) => services;
+
+    public IServiceProvider CreateServiceProvider(IServiceCollection services)
+    {
+        throw new NotImplementedException();
+    }
+}
