949cf80f2eba56cde48c004d5b6493fc0f2d7601
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-16 11:11:06 +0200

    added dummy overrides for OnActivateAsync and OnDeactivateAsync in ApplicationGrain

--- src/OpenSmc.Application.Orleans/ApplicationGrain.cs
+++ src/OpenSmc.Application.Orleans/ApplicationGrain.cs
@@ -4,6 +4,16 @@ namespace OpenSmc.Application.Orleans;
 
 public class ApplicationGrain : Grain, IApplicationGrain
 {
+    public override async Task OnActivateAsync(CancellationToken cancellationToken)
+    {
+        await base.OnActivateAsync(cancellationToken);
+    }
+
+    public override async Task OnDeactivateAsync(DeactivationReason reason, CancellationToken cancellationToken)
+    {
+        await base.OnDeactivateAsync(reason, cancellationToken);
+    }
+
     public Task<IMessageDelivery> DeliverMessage(IMessageDelivery delivery)
     {
         throw new NotImplementedException();
