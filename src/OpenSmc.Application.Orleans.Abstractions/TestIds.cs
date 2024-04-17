81f43d57b783471837845fcbc034baf5efe4f88c
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-17 18:31:28 +0200

    [TBC] added consts with name and environment for test application

--- src/OpenSmc.Application.Orleans.Abstractions/TestIds.cs
+++ src/OpenSmc.Application.Orleans.Abstractions/TestIds.cs
@@ -4,3 +4,10 @@ public static class TestUiIds
 {
     public const string HardcodedUiId = "MyTestApp"; // HACK V10: get rid of this hardcoding (2024/04/17, Dmitry Kalabin)
 }
+
+// HACK V10: move these constants to the test level (2024/04/17, Dmitry Kalabin)
+public static class TestApplication
+{
+    public const string Name = "testApp";
+    public const string Environment = "dev";
+}
