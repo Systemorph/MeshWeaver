e7d16fe246433ad115d2710269d9660519526c2a
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-17 17:33:38 +0200

    [TBC] added const with HardcodedUiId

---
+++ src/OpenSmc.Application.Orleans.Abstractions/TestIds.cs
@@ -0,0 +1,6 @@
+﻿namespace OpenSmc.Application.Orleans;
+
+public static class TestUiIds
+{
+    public const string HardcodedUiId = "MyTestApp"; // HACK V10: get rid of this hardcoding (2024/04/17, Dmitry Kalabin)
+}
