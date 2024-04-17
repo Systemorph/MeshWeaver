ba0c113382bd54b1056bdbef65724c4bd25d5f6a
Alexander Kravets <akravets@systemorph.com>
2024-04-17 11:39:49 +0300

    using update action for bindings

--- ui/packages/app/src/store/bindingToPatchAction.ts
+++
@@ -1,6 +0,0 @@
-import { Binding } from "@open-smc/layout/src/contract/Binding";
-import { toPointer } from "@open-smc/data/src/toPointer";
-import { pathToPatchAction } from "@open-smc/data/src/operators/pathToPatchAction";
-
-export const bindingToPatchAction = (binding: Binding) =>
-    pathToPatchAction(toPointer(binding.path));
