d7bf5d25fae8761093d3f4700e3d6a393d89833b
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-17 17:19:37 +0200

    [TBRv] changed Orleans Hub configuration to span hosted hub to respond to a TestRequest sent to particular ApplicationAddress
    (this is temporary code for test purposes)

--- src/OpenSmc.Application.Host/Program.cs
+++ src/OpenSmc.Application.Host/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenSmc.Application.SignalR;
+﻿using OpenSmc.Application.Orleans;
+using OpenSmc.Application.SignalR;
 using OpenSmc.Messaging;
 using Orleans.Serialization;
 using static OpenSmc.Hosting.HostBuilderExtensions;
@@ -47,6 +48,14 @@ public static class OrleansMessageHubExtensions
     public static IMessageHub GetOrleansHub(this IServiceProvider serviceProvider) 
         => serviceProvider.CreateMessageHub(new OrleansAddress(), conf => 
             conf
+                .WithHostedHub(new ApplicationAddress(TestApplication.Name, TestApplication.Environment), config =>
+                    // HACK V10: this is just for testing and should not be a part of Prod setup (2024/04/17, Dmitry Kalabin)
+                    config.WithHandler<TestRequest>((hub, request) =>
+                    {
+                        hub.Post(new TestResponse(), options => options.ResponseFor(request));
+                        return request.Processed();
+                    })
+                )
         );
 }
 
