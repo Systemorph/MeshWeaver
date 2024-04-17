8ea84b863d15396d4625b26127f3d110e5020fa7
Roland Bürgi <rbuergi@systemorph.com>
2024-04-17 14:03:23 +0200

    Adding Northwind domain

---
+++ src/OpenSmc.Northwind/Domain.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenSmc.Northwind;
+
+public record Category(int CategoryId, string CategoryName, string Description, string Picture);
+public record Customer(string CustomerId, string CompanyName, string ContactName, string ContactTitle, string Address, string City, string Region, string PostalCode, string Country, string Phone, string Fax);
+public record Employee(int EmployeeId, string LastName, string FirstName, string Title, string TitleOfCourtesy, DateTime BirthDate, DateTime HireDate, string Address, string City, string Region, string PostalCode, string Country, string HomePhone, string Extension, byte[] Photo, string Notes, int ReportsTo, string PhotoPath);
+public record Order(int OrderId, string CustomerId, int EmployeeId, DateTime OrderDate, DateTime RequiredDate, DateTime ShippedDate, int ShipVia, decimal Freight, string ShipName, string ShipAddress, string ShipCity, string ShipRegion, string ShipPostalCode, string ShipCountry);
+public record Product(int ProductId, string ProductName, int SupplierId, int CategoryId, string QuantityPerUnit, decimal UnitPrice, short UnitsInStock, short UnitsOnOrder, short ReorderLevel, bool Discontinued);
+public record Region(int RegionId, string RegionDescription);
+public record Shipper(int ShipperId, string CompanyName, string Phone);
+public record Supplier(int SupplierId, string CompanyName, string ContactName, string ContactTitle, string Address, string City, string Region, string PostalCode, string Country, string Phone, string Fax, string HomePage);
+public record Territory(string TerritoryId, string TerritoryDescription, int RegionId);
+
+
---
+++ src/OpenSmc.Northwind/Hubs.cs
@@ -0,0 +1,24 @@
+﻿using OpenSmc.Messaging;
+using static OpenSmc.Data.DataPluginExtensions;
+
+namespace OpenSmc.Northwind;
+
+public static class NorthwindHubConfiguration
+{
+    public static IMessageHub GetReferenceDataHub(this IServiceProvider serviceProvider, object address)
+    {
+        return serviceProvider.CreateMessageHub(address, config => config.AddData(data => data));
+    }
+}
+
+public class ReferenceDataAddress;
+public class CustomerAddress;
+public class ProductAddress;
+
+public class EmployeeAddress;
+
+public class OrderAddress;
+
+public class ShipperAddress;
+public class SupplierAddress;
+
