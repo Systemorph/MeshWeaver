1a34592156f49d81cfb2468480e1ddaf9d74de20
Alexander Kravets <akravets@systemorph.com>
2024-04-19 17:53:02 +0300

    ItemTemplateRenderer draft

--- ui/packages/app/src/store/AreaCollectionRenderer.ts
+++ ui/packages/app/src/store/AreaCollectionRenderer.ts
@@ -4,13 +4,15 @@ import { Collection } from "@open-smc/data/src/contract/Collection";
 import { keys } from "lodash-es";
 import { appStore } from "./appStore";
 import { removeArea } from "./appReducer";
-import { ControlRenderer } from "./ControlRenderer";
+import { ControlModelWorkspace } from "./ControlModelWorkspace";
 import { EntityReference } from "@open-smc/data/src/contract/EntityReference";
 import { selectByReference } from "@open-smc/data/src/operators/selectByReference";
+import { renderControlTo } from "./renderControlTo";
 
+// TODO: replace with function renderAreaCollection (4/19/2024, akravets)
 export class AreaCollectionRenderer {
     subscription = new Subscription();
-    private state: Record<string, ControlRenderer> = {};
+    private state: Record<string, Subscription> = {};
 
     constructor(areaReferences$: Observable<EntityReference[]>, collections: Workspace<Collection<Collection>>) {
         this.subscription.add(
@@ -21,7 +23,7 @@ export class AreaCollectionRenderer {
                             .forEach(reference => {
                                 const control$ = collections.pipe(map(selectByReference(reference)));
                                 this.state[reference.id] =
-                                    new ControlRenderer(control$, collections, reference.id);
+                                    renderControlTo(new ControlModelWorkspace(control$, collections, reference.id), reference.id);
                             });
                     }
                 )
@@ -34,7 +36,7 @@ export class AreaCollectionRenderer {
                         keys(this.state).forEach(id => {
                             if (!references?.find(reference => reference.id === id)) {
                                 appStore.dispatch(removeArea(id));
-                                this.state[id].subscription.unsubscribe();
+                                this.state[id].unsubscribe();
                                 delete this.state[id];
                             }
                         })
