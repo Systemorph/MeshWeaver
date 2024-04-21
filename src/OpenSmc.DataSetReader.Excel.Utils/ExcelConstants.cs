7fee675f1d0177e00c161f95c9ccbcbf36dff7c1
Roland Bürgi <rbuergi@systemorph.com>
2024-04-21 23:50:36 +0200

    Installing additional analyzers and fixing warnings

--- src/OpenSmc.DataSetReader.Excel.Utils/ExcelConstants.cs
+++ src/OpenSmc.DataSetReader.Excel.Utils/ExcelConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace OpenSmc.DataSetReader.Excel.Utils
 {
-    public class ExcelConstants
+    public static class ExcelConstants
     {
         #region regex
         //would transform " @ % 1Va\1l ueD#ata" into "ValueData"
@@ -8,11 +8,16 @@
         public const string NumberPattern = @"\d+";
         public const string NumberAtEndPattern = @"^[A-Za-z]*[0-9]{1,}$";
 
-        public const string DoubleNumberingFormatPattern = @"(0{1,20}(%))|(((#{1,20})|(0{1,20}))(\.|\,)((#{0,20})|(0{0,20})))|(\d{1,}(e\+|e-)\d{0,})|(((#{0,20})|(0{0,20}))(\.|\,)((#{1,20})|(0{1,20})))";
+        public const string DoubleNumberingFormatPattern =
+            @"(0{1,20}(%))|(((#{1,20})|(0{1,20}))(\.|\,)((#{0,20})|(0{0,20})))|(\d{1,}(e\+|e-)\d{0,})|(((#{0,20})|(0{0,20}))(\.|\,)((#{1,20})|(0{1,20})))";
+
         //Currency
-        public const string FloatNumberingFormatPattern = @"(.{0,1}(#{1,20})(,)(#{1,20})(0{1,20})(.)(0{1,20}))";
-        public const string DateNumberingFormatPattern = @"((m{1,5})|(d{1,5})|(y{1,5}))(\/|\-|\\-)((m{1,5})|(d{1,5})|(y{1,5}))(\/|\-|\\-){0,1}((m{0,5})|(d{0,5})|(y{0,5}))";
-        public const string TimeNumberingFormatPattern = @"((h{1,6})|(\[h\])|(m{1,6}))(\:|\.)((m{1,6})|(s{1,6}))((\:|\.){0,1})((s{0,6}))";
+        public const string FloatNumberingFormatPattern =
+            @"(.{0,1}(#{1,20})(,)(#{1,20})(0{1,20})(.)(0{1,20}))";
+        public const string DateNumberingFormatPattern =
+            @"((m{1,5})|(d{1,5})|(y{1,5}))(\/|\-|\\-)((m{1,5})|(d{1,5})|(y{1,5}))(\/|\-|\\-){0,1}((m{0,5})|(d{0,5})|(y{0,5}))";
+        public const string TimeNumberingFormatPattern =
+            @"((h{1,6})|(\[h\])|(m{1,6}))(\:|\.)((m{1,6})|(s{1,6}))((\:|\.){0,1})((s{0,6}))";
         #endregion
     }
 }
--- src/OpenSmc.Domain.Abstractions/Attributes/MapToAttribute.cs
+++ src/OpenSmc.Domain.Abstractions/Attributes/MapToAttribute.cs
@@ -1,12 +1,13 @@
 ﻿namespace OpenSmc.Domain.Abstractions.Attributes
 {
-
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class MapToAttribute : Attribute
     {
         public MapToAttribute(string propertyName)
         {
             PropertyName = propertyName;
         }
+
         public string PropertyName { get; set; }
     }
 }
