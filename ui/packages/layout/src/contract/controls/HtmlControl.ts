06397a375e708bb4dbea89e59ed9c1a356d4b890
Alexander Kravets <akravets@systemorph.com>
2024-04-24 09:51:01 +0300

    nested item templates example

---
+++ ui/packages/layout/src/contract/controls/HtmlControl.ts
@@ -0,0 +1,6 @@
+import { type } from "@open-smc/serialization/src/type";
+import { UiControl } from "./UiControl";
+
+@type("OpenSmc.Layout.HtmlControl")
+export class HtmlControl extends UiControl {
+}
--- ui/packages/layout/src/examples/itemTemplateExample.ts
+++ ui/packages/layout/src/examples/itemTemplateExample.ts
@@ -2,11 +2,11 @@ export const itemTemplateExample = {
     $type: "OpenSmc.Data.EntityStore",
     reference: {
         $type: "OpenSmc.Data.LayoutAreaReference",
-        area: "Main"
+        area: "LineOfBusiness"
     },
     collections: {
         "OpenSmc.Layout.UiControl": {
-            "Main": {
+            "LineOfBusiness": {
                 $type: "OpenSmc.Layout.ItemTemplateControl",
                 dataContext: {
                     $type: "OpenSmc.Data.CollectionReference",
@@ -22,27 +22,60 @@ export const itemTemplateExample = {
                         {
                             $type: "OpenSmc.Data.EntityReference",
                             collection: "OpenSmc.Layout.UiControl",
-                            id: "Main/DisplayName"
-                        }
+                            id: "LineOfBusiness/DisplayName"
+                        },
+                        {
+                            $type: "OpenSmc.Data.EntityReference",
+                            collection: "OpenSmc.Layout.UiControl",
+                            id: "LineOfBusiness/DisplayNameEditor"
+                        },
+                        {
+                            $type: "OpenSmc.Data.EntityReference",
+                            collection: "OpenSmc.Layout.UiControl",
+                            id: "LineOfBusiness/Currencies"
+                        },
                     ]
                 }
             },
-            "Main/DisplayName": {
+            "LineOfBusiness/DisplayName": {
+                $type: "OpenSmc.Layout.HtmlControl",
+                data: {
+                    $type: "OpenSmc.Layout.DataBinding.Binding",
+                    path: "$.DisplayName"
+                }
+            },
+            "LineOfBusiness/DisplayNameEditor": {
                 $type: "OpenSmc.Layout.TextBoxControl",
                 data: {
                     $type: "OpenSmc.Layout.DataBinding.Binding",
                     path: "$.DisplayName"
+                }
+            },
+            "LineOfBusiness/Currencies": {
+                $type: "OpenSmc.Layout.ItemTemplateControl",
+                data: {
+                    $type: "OpenSmc.Layout.DataBinding.Binding",
+                    path: "$.Currencies"
+                },
+                view: {
+                    $type: "OpenSmc.Layout.HtmlControl",
+                    data: {
+                        $type: "OpenSmc.Layout.DataBinding.Binding",
+                        path: "$"
+                    }
                 }
             }
         },
         LineOfBusiness: {
             "1": {
                 SystemName: "1",
-                DisplayName: "1"
+                DisplayName: "1",
+                Currencies: ["CHF", "EUR"]
             },
             "2": {
                 SystemName: "2",
-                DisplayName: "2"
+                DisplayName: "2",
+                Currencies: ["USD"]
             }
         }
     }
