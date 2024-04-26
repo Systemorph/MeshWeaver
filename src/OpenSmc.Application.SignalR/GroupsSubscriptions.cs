5dc9a539e93f91379f3b968cb6599b087cdd2f88
Dmitry Kalabin <dkalabin@systemorph.com>
2024-04-26 13:20:29 +0200

    introduced new GroupsSubscriptions.SubscribeAsync() method
    (which doesn't do anything special yet)

--- src/OpenSmc.Application.SignalR/GroupsSubscriptions.cs
+++ src/OpenSmc.Application.SignalR/GroupsSubscriptions.cs
@@ -3,4 +3,11 @@
 public class GroupsSubscriptions<TIdentity>
 {
     private readonly AsyncLock @lock = new();
+
+    internal async ValueTask SubscribeAsync(string connectionId, TIdentity groupId)
+    {
+        using (await @lock.LockAsync()) // TODO V10: think about reducing locking for the cases when there is nothing to do (2024/04/26, Dmitry Kalabin)
+        {
+        }
+    }
 }
