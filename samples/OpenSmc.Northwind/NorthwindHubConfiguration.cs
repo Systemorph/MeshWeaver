69a5d8dc69368f744dd58e2c11f673fb18d10372
Roland Bürgi <rbuergi@systemorph.com>
2024-04-30 10:57:19 +0200

    Adding configurations for all hubs

--- samples/OpenSmc.Northwind/NorthwindHubConfiguration.cs
+++ samples/OpenSmc.Northwind/NorthwindHubConfiguration.cs
@@ -10,7 +10,7 @@ public static class NorthwindHubConfiguration
 {
     private static readonly Assembly NorthwindAssembly = typeof(NorthwindHubConfiguration).Assembly;
 
-    public static MessageHubConfiguration AddNorthwindReferenceDataFromFile(
+    public static MessageHubConfiguration AddNorthwindReferenceData(
         this MessageHubConfiguration configuration
     )
     {
@@ -18,16 +18,91 @@ public static class NorthwindHubConfiguration
             .AddImport()
             .AddData(data =>
                 data.FromEmbeddedResource<Category>(
-                    "Files.categories.csv",
-                    config => config.WithType<Category>()
+                        "Files.categories.csv",
+                        config => config.WithType<Category>()
+                    )
+                    .FromEmbeddedResource<Region>(
+                        "Files.regions.csv",
+                        config => config.WithType<Region>()
+                    )
+                    .FromEmbeddedResource<Territory>(
+                        "Files.territories.csv",
+                        config => config.WithType<Territory>()
+                    )
+            );
+    }
+
+    public static MessageHubConfiguration AddNorthwindOrders(
+        this MessageHubConfiguration configuration
+    )
+    {
+        return configuration
+            .AddImport()
+            .AddData(data =>
+                data.FromEmbeddedResource<Order>(
+                    "Files.orders.csv",
+                    config => config.WithType<Order>()
                 )
             );
+    }
+
+    public static MessageHubConfiguration AddNorthwindSuppliers(
+        this MessageHubConfiguration configuration
+    )
+    {
+        return configuration
+            .AddImport()
+            .AddData(data =>
+                data.FromEmbeddedResource<Supplier>(
+                    "Files.suppliers.csv",
+                    config => config.WithType<Supplier>()
+                )
+            );
+    }
+
+    public static MessageHubConfiguration AddNorthwindEmployees(
+        this MessageHubConfiguration configuration
+    )
+    {
+        return configuration
+            .AddImport()
+            .AddData(data =>
+                data.FromEmbeddedResource<Employee>(
+                    "Files.employees.csv",
+                    config => config.WithType<Employee>()
+                )
+            );
+    }
+
+    public static MessageHubConfiguration AddNorthwindProducts(
+        this MessageHubConfiguration configuration
+    )
+    {
+        return configuration
+            .AddImport()
+            .AddData(data =>
+                data.FromEmbeddedResource<Product>(
+                    "Files.products.csv",
+                    config => config.WithType<Product>()
+                )
+            );
+    }
+
+    public static TDataSource AddNorthwindDomain<TDataSource>(this TDataSource dataSource)
+        where TDataSource : DataSource<TDataSource>
+    {
+        return dataSource
+            .AddNorthwindReferenceData()
+            .WithType<Order>()
+            .WithType<Supplier>()
+            .WithType<Employee>()
+            .WithType<Product>();
     }
 
     public static TDataSource AddNorthwindReferenceData<TDataSource>(this TDataSource dataSource)
         where TDataSource : DataSource<TDataSource>
     {
-        return dataSource.WithType<Category>();
+        return dataSource.WithType<Category>().WithType<Region>().WithType<Territory>();
     }
 
     public static IMessageHub GetCustomerHub(this IServiceProvider serviceProvider, object address)
--- test/OpenSmc.Northwind.Test/NorthwindTest.cs
+++ test/OpenSmc.Northwind.Test/NorthwindTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reactive.Linq;
+using FluentAssertions;
 using OpenSmc.Data;
 using OpenSmc.Data.Persistence;
 using OpenSmc.Hub.Fixture;
@@ -10,9 +11,37 @@ namespace OpenSmc.Northwind.Test;
 
 public class NorthwindTest(ITestOutputHelper output) : HubTestBase(output)
 {
+    protected override MessageHubConfiguration ConfigureRouter(
+        MessageHubConfiguration configuration
+    )
+    {
+        return base.ConfigureRouter(configuration)
+            .WithRoutes(forward =>
+                forward
+                    .RouteAddressToHostedHub<ReferenceDataAddress>(c =>
+                        c.AddNorthwindReferenceData()
+                    )
+                    .RouteAddressToHostedHub<EmployeeAddress>(c => c.AddNorthwindEmployees())
+                    .RouteAddressToHostedHub<OrderAddress>(c => c.AddNorthwindOrders())
+                    .RouteAddressToHostedHub<SupplierAddress>(c => c.AddNorthwindSuppliers())
+                    .RouteAddressToHostedHub<ProductAddress>(c => c.AddNorthwindProducts())
+            );
+    }
+
     protected override MessageHubConfiguration ConfigureHost(MessageHubConfiguration configuration)
     {
-        return configuration.AddNorthwindReferenceDataFromFile();
+        return base.ConfigureHost(configuration)
+            .AddData(data =>
+                data.FromHub(
+                        new ReferenceDataAddress(),
+                        dataSource => dataSource.AddNorthwindDomain()
+                    )
+                    .FromHub(new CustomerAddress(), c => c.WithType<Customer>())
+                    .FromHub(new ProductAddress(), c => c.WithType<Product>())
+                    .FromHub(new EmployeeAddress(), c => c.WithType<Employee>())
+                    .FromHub(new OrderAddress(), c => c.WithType<Order>())
+                    .FromHub(new SupplierAddress(), c => c.WithType<Supplier>())
+            );
     }
 
     protected override MessageHubConfiguration ConfigureClient(
@@ -20,17 +49,24 @@ public class NorthwindTest(ITestOutputHelper output) : HubTestBase(output)
     ) =>
         base.ConfigureClient(configuration)
             .AddData(data =>
-                data.FromHub(
-                    new HostAddress(),
-                    dataSource => dataSource.AddNorthwindReferenceData()
-                )
+                data.FromHub(new HostAddress(), dataSource => dataSource.AddNorthwindDomain())
             );
 
     [Fact]
     public async Task DataInitialization()
     {
         var client = GetClient();
-        var response = await client.GetWorkspace().GetObservable<Category>().FirstAsync();
-        Assert.NotNull(response);
+        var categories = await client.GetWorkspace().GetObservable<Category>().FirstAsync();
+        categories.Should().HaveCountGreaterThan(0);
+        var territories = await client.GetWorkspace().GetObservable<Territory>().FirstAsync();
+        territories.Should().HaveCountGreaterThan(0);
+        var employees = await client.GetWorkspace().GetObservable<Employee>().FirstAsync();
+        employees.Should().HaveCountGreaterThan(0);
+        var products = await client.GetWorkspace().GetObservable<Product>().FirstAsync();
+        products.Should().HaveCountGreaterThan(0);
+        var orders = await client.GetWorkspace().GetObservable<Order>().FirstAsync();
+        orders.Should().HaveCountGreaterThan(0);
+        var suppliers = await client.GetWorkspace().GetObservable<Supplier>().FirstAsync();
+        suppliers.Should().HaveCountGreaterThan(0);
     }
 }
