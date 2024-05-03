b858564a7ac76bec21de600d49df1b169a49a7a8
Dmitry Kalabin <dkalabin@systemorph.com>
2024-05-03 15:39:43 +0200

    changed Program.cs to call UseOrleans() for Host configuration to start in "Localhost Clustering" mode

--- samples/OpenSmc.Northwind.Host/Program.cs
+++ samples/OpenSmc.Northwind.Host/Program.cs
@@ -10,6 +10,12 @@ public class Program
 
         builder.Services.ConfigureApplicationSignalR();
 
+        builder.Host
+            .UseOrleans(static siloBuilder =>
+            {
+                siloBuilder.UseLocalhostClustering();
+            });
+
         await using var app = builder.Build();
 
         app
