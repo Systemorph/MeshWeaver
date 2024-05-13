eee04f6b7b3f812ba5fc2de4c1957483bbc92d41
Dmitry Kalabin <dkalabin@systemorph.com>
2024-05-13 11:21:49 +0200

    added SignalRClientConfiguration record

---
+++ test/OpenSmc.SignalR.Fixture/SignalRClientHubExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace OpenSmc.SignalR.Fixture;
+
+public record SignalRClientConfiguration
+{
+    internal Func<IHubConnectionBuilder, IHubConnectionBuilder> hubConnectionBuilderConfig;
+    public SignalRClientConfiguration WithHubConnectionConfiguration(Func<IHubConnectionBuilder, IHubConnectionBuilder> hubConnectionBuilderConfig)
+        => this with { hubConnectionBuilderConfig = hubConnectionBuilderConfig, };
+}
