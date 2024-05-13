8644d838afae59560a9a2b2ea06999fba903449e
Dmitry Kalabin <dkalabin@systemorph.com>
2024-05-13 11:19:06 +0200

    moved SignalRTestClientConfig into OpenSmc.SignalR.Fixture project

---
+++ test/OpenSmc.SignalR.Fixture/SignalRTestClientConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using OpenSmc.Messaging;
+
+namespace OpenSmc.SignalR.Fixture;
+
+public static class SignalRTestClientConfig
+{
+    public static async Task<IMessageDelivery> SendThroughSignalR(IMessageDelivery delivery, HubConnection connection, CancellationToken cancellationToken)
+    {
+        await connection.InvokeAsync("DeliverMessageAsync", delivery, cancellationToken);
+
+        return delivery.Forwarded();
+    }
+}
