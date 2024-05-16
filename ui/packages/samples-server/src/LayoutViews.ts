9526c33894a4c471659edb6a63652dedccb4c06c
Alexander Kravets <akravets@systemorph.com>
2024-05-16 12:57:28 +0300

    proceeding with samples

--- ui/packages/samples-server/src/LayoutViews.ts
+++ ui/packages/samples-server/src/LayoutViews.ts
@@ -1,15 +1,45 @@
+import { Collection } from "@open-smc/data/src/contract/Collection";
 import { EntityReference } from "@open-smc/data/src/contract/EntityReference";
+import { Workspace } from "@open-smc/data/src/Workspace";
+import { distinctUntilEqual } from "@open-smc/data/src/operators/distinctUntilEqual";
 import { UiControl } from "@open-smc/layout/src/contract/controls/UiControl.ts";
+import { map, Observable, Subscription, tap } from "rxjs";
+import { updateStore } from "@open-smc/data/src/workspaceReducer";
 
 export const uiControlType = (UiControl as any).$type;
 
-export class LayoutViews extends Map<string, UiControl> {
-    addView(area: string, control: UiControl) {
-        this.set(area, control);
+export class LayoutViews extends Workspace<Collection<UiControl>> {
+    subscription = new Subscription();
+
+    constructor() {
+        super({});
+    }
+    
+    addView(area = this.getAreaName(), control: UiControl) {
+        this.update(state => {
+            state[area] = control
+        });
+
         return new EntityReference(uiControlType, area);
     }
 
-    toCollection() {
-        return Object.fromEntries(this);
+    addViewStream(area = this.getAreaName(), control$: Observable<UiControl>) {
+        this.subscription.add(
+            control$
+                .pipe(distinctUntilEqual())
+                .pipe(
+                    map(value => updateStore(state => {
+                        state[area] = value;
+                    }))
+                )
+                .subscribe(this)
+        );
+        return new EntityReference(uiControlType, area);
     }
+
+    private getAreaName() {
+        return `area_${this.n++}`;
+    }
+
+    private n = 0;
 }
