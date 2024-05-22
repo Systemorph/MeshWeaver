f15e3f7df644c8ebff5a5a0c26a076e4bf231ec1
Roland Bürgi <rbuergi@systemorph.com>
2024-05-22 21:32:07 +0200

    fixing entity framework test

--- test/OpenSmc.Data-EntityFramework.Test/EntityFrameworkDataContextTest.cs
+++ test/OpenSmc.Data-EntityFramework.Test/EntityFrameworkDataContextTest.cs
@@ -14,21 +14,27 @@ namespace OpenSmc.Data_EntityFramework.Test;
 
 public class EntityFrameworkDataContextTest(ITestOutputHelper output) : HubTestBase(output)
 {
-    private const string ConnectionString = $"Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;Database=Testing";
-    public record MyDataRecord([property: Key] string Id, string BusinessUnit, string LoB, double Value);
+    private const string ConnectionString =
+        $"Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;Database=Testing";
 
+    public record MyDataRecord(
+        [property: Key] string Id,
+        string BusinessUnit,
+        string LoB,
+        double Value
+    );
 
     private const string SqlServer = nameof(SqlServer);
+
     protected override MessageHubConfiguration ConfigureHost(MessageHubConfiguration configuration)
     {
-        return base
-            .ConfigureHost(configuration)
-                    
-            .AddData(data => data
-                .FromEntityFramework(SqlServer, 
+        return base.ConfigureHost(configuration)
+            .AddData(data =>
+                data.FromEntityFramework(
+                    SqlServer,
                     database => database.UseSqlServer(ConnectionString),
-                    dataSource => dataSource
-
+                    dataSource =>
+                        dataSource
                         // here, all the entity types need to be listed as exposed to framework
                         // if desired, we can add mapping logic between data source and data hub, please request on GitHub.
                         .WithType<MyDataRecord>()
@@ -36,44 +42,46 @@ public class EntityFrameworkDataContextTest(ITestOutputHelper output) : HubTestB
             );
     }
 
+    protected override MessageHubConfiguration ConfigureClient(
+        MessageHubConfiguration configuration
+    )
+    {
+        return base.ConfigureClient(configuration)
+            .AddData(data =>
+                data.FromHub(new HostAddress(), dataSource => dataSource.WithType<MyDataRecord>())
+            );
+    }
+
 #if CIRun
-        [Fact(Skip = "Hangs")]
+    [Fact(Skip = "Hangs")]
 #else
-        [Fact(Timeout = 5000)]
+    [Fact(Timeout = 5000)]
 #endif
     public async Task TestBasicCrud()
     {
         var client = GetClient();
         var myDataRecord = new MyDataRecord("1", "BU1", "Lob1", 10);
-        var hostAddress = new HostAddress();
-        var dataChanged = 
-            await client.AwaitResponse(new UpdateDataRequest(new [] { myDataRecord }),
-                o => o.WithTarget(hostAddress));
 
-        dataChanged.Message.Version.Should().Be(1);
+        var workspace = client.GetWorkspace();
+
+        var dataChanged = await client.AwaitResponse(
+            new UpdateDataRequest(new[] { myDataRecord }),
+            o => o.WithTarget(new ClientAddress())
+        );
+
+        dataChanged.Message.Version.Should().Be(6);
 
         // check on data hub
-        var items = await GetItems(client, hostAddress, myDataRecord);
+        var hostWorkspace = GetHost().GetWorkspace();
+        var items = await hostWorkspace.GetObservable<MyDataRecord>().FirstAsync();
+        ;
         items.Should().ContainSingle().Which.Should().Be(myDataRecord);
 
         await DisposeAsync();
         await InitializeAsync();
 
         client = GetClient();
-        items = await GetItems(client, hostAddress, myDataRecord);
+        items = await client.GetWorkspace().GetObservable<MyDataRecord>().FirstAsync();
         items.Should().ContainSingle().Which.Should().Be(myDataRecord);
     }
-
-    
-
-
-        
-    private static async Task<IReadOnlyCollection<MyDataRecord>> GetItems(IMessageHub client, object address, MyDataRecord myDataRecord)
-    {
-        var workspace = client.ServiceProvider.GetRequiredService<IWorkspace>();
-        var items = await workspace.GetObservable<MyDataRecord>().FirstAsync();
-
-        return items;
-    }
 }
-
