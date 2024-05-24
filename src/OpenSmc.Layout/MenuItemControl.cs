e0aebfb4fd3eddbaa3cdf88e58108120cb47a561
Roland Bürgi <rbuergi@systemorph.com>
2024-05-24 15:56:17 +0200

    Creating some setup for basic components

--- src/OpenSmc.Layout/MenuItemControl.cs
+++ src/OpenSmc.Layout/MenuItemControl.cs
@@ -1,28 +1,7 @@
-﻿using System.Collections.Immutable;
-using System.Reflection;
+﻿using System.Reflection;
 using OpenSmc.Reflection;
 
 namespace OpenSmc.Layout.Views;
-
-/// <summary>
-/// ActivityControl is a control that represents an activity in the system.
-/// </summary>
-/// <param name="User">UserInfo, for now populated with Email, DisplayName and Photo</param>
-/// <param name="Title">Text</param>
-/// <param name="Summary">Control</param>
-/// <param name="View">Control of main body</param>
-/// <param name="Menu">Control</param>
-/// <param name="Options">Control</param>
-public record ActivityControl(
-    object User,
-    object Title,
-    object Summary,
-    object View,
-    string Color,
-    DateTime Date,
-    ImmutableList<MenuItemControl> Menu,
-    ImmutableList<MenuItemControl> Options
-) : UiControl<ActivityControl>(ModuleSetup.ModuleName, ModuleSetup.ApiVersion, null);
 
 public record MenuItemControl(object Title, object Icon)
     : ExpandableUiControl<MenuItemControl>(ModuleSetup.ModuleName, ModuleSetup.ApiVersion, null)
