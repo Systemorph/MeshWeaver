eaa7ddc9920f60c14a1ab8edba28020a89fc4d2e
Dmitry Kalabin <dkalabin@systemorph.com>
2024-05-29 12:18:49 +0200

    made MessageDeliveryRawJsonConverter to inherit from JsonConverter<MessageDelivery<RawJson>>

--- src/OpenSmc.Messaging.Hub/Serialization/MessageDeliveryJsonConverter.cs
+++ src/OpenSmc.Messaging.Hub/Serialization/MessageDeliveryJsonConverter.cs
@@ -1,5 +1,16 @@
-﻿namespace OpenSmc.Messaging.Serialization;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using OpenSmc.Serialization;
 
-public class MessageDeliveryRawJsonConverter
+namespace OpenSmc.Messaging.Serialization;
+
+public class MessageDeliveryRawJsonConverter : JsonConverter<MessageDelivery<RawJson>>
 {
+    public override MessageDelivery<RawJson> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, MessageDelivery<RawJson> value, JsonSerializerOptions options)
+        => throw new NotImplementedException();
 }
