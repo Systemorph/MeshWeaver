c71b5e8619a8c32164cdfa16a407ebe9cc02010d
Dmitry Kalabin <dkalabin@systemorph.com>
2024-05-29 13:16:20 +0200

    added code to deserialize message body as RawJson

--- src/OpenSmc.Messaging.Hub/Serialization/MessageDeliveryJsonConverter.cs
+++ src/OpenSmc.Messaging.Hub/Serialization/MessageDeliveryJsonConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using OpenSmc.Serialization;
 
@@ -8,7 +9,14 @@ public class MessageDeliveryRawJsonConverter : JsonConverter<MessageDelivery<Raw
 {
     public override MessageDelivery<RawJson> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        throw new NotImplementedException();
+        var node = JsonNode.Parse(ref reader);
+
+        var content = node["message"].ToJsonString();
+        var rawJson = new RawJson(content);
+
+        var postOptions =
+            new PostOptions(null, null);
+        return new MessageDelivery<RawJson>(rawJson, postOptions);
     }
 
     public override void Write(Utf8JsonWriter writer, MessageDelivery<RawJson> value, JsonSerializerOptions options)
