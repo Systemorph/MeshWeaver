1b0e33bdea14fb084b3eb60cabff555583dc3b00
Dmitry Kalabin <dkalabin@systemorph.com>
2024-05-30 17:39:38 +0200

    added logic to update $type property with unpacked type

--- src/OpenSmc.Messaging.Hub/Serialization/MessageDeliveryJsonConverter.cs
+++ src/OpenSmc.Messaging.Hub/Serialization/MessageDeliveryJsonConverter.cs
@@ -7,6 +7,8 @@ namespace OpenSmc.Messaging.Serialization;
 
 public class MessageDeliveryRawJsonConverter : JsonConverter<MessageDelivery<RawJson>>
 {
+    private const string TypeProperty = "$type";
+
     public override MessageDelivery<RawJson> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var node = JsonNode.Parse(ref reader);
@@ -44,12 +46,22 @@ public class MessageDeliveryRawJsonConverter : JsonConverter<MessageDelivery<Raw
 
     public override void Write(Utf8JsonWriter writer, MessageDelivery<RawJson> value, JsonSerializerOptions options)
     {
+        string internalTypeName = null;
         writer.WriteStartObject();
         writer.WritePropertyName("message");
         if (string.IsNullOrWhiteSpace(value.Message?.Content))
+        {
             writer.WriteNullValue();
+        }
         else
+        {
+            using var doc = JsonDocument.Parse(value.Message.Content);
+            var root = doc.RootElement;
+            if (root.TryGetProperty(TypeProperty, out var typeElement))
+                internalTypeName = typeElement.GetString();
+
             writer.WriteRawValue(value.Message.Content);
+        }
 
         writer.WritePropertyName("sender");
         JsonSerializer.SerializeToNode(value.Sender, value.Sender.GetType(), options)!.WriteTo(writer);
@@ -61,6 +73,13 @@ public class MessageDeliveryRawJsonConverter : JsonConverter<MessageDelivery<Raw
         writer.WriteStringValue(value.State);
         writer.WritePropertyName("id");
         writer.WriteStringValue(value.Id);
+
+        if (!string.IsNullOrWhiteSpace(internalTypeName))
+        {
+            var dispatchedTypeName = TypeRegistry.FormatType(typeof(MessageDelivery<>), _ => [internalTypeName]);
+            writer.WritePropertyName(TypeProperty);
+            writer.WriteStringValue(dispatchedTypeName);
+        }
 
         writer.WriteEndObject();
     }
