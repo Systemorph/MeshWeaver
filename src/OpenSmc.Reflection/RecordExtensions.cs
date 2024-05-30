340a72b34afe7225465d9dfbfbb4a42dec976902
Roland Bürgi <rbuergi@systemorph.com>
2024-05-30 13:04:16 +0200

    Create empty components library for Northwind

---
+++ src/OpenSmc.Reflection/RecordExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace OpenSmc.Reflection;
+
+public static class RecordExtensions
+{
+    public static T With<T>(this T obj, params Expression<Func<T, object>>[] propertyExpressions)
+    {
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var expressions = propertyExpressions.Select(expr =>
+        {
+            var memberExpr = (MemberExpression)expr.Body;
+            var property = (PropertyInfo)memberExpr.Member;
+            var value = Expression.Convert(expr.Body, property.PropertyType);
+            return Expression.Bind(property, value);
+        });
+
+        var memberInit = Expression.MemberInit(Expression.New(typeof(T)), expressions);
+        var lambda = Expression.Lambda<Func<T, T>>(memberInit, parameter);
+        var compiled = lambda.Compile();
+        return compiled(obj);
+    }
+}
