8922b397172beda3947b46e5d21cf3e3c1ecad3a
Alexander Kravets <akravets@systemorph.com>
2024-06-06 17:54:27 +0300

    proceeding with navigation, adding icons

---
+++ src/OpenSmc.Application.Styles/FluentUiIcons.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenSmc.Application.Styles;
+
+public static class FluentUiIcons
+{
+    public const string Provider = "fluent-ui";
+
+    public static Icon Home => new (Provider, "Home");
+    public static Icon Grid => new (Provider, "Grid");
+    public static Icon Box => new (Provider, "Box");
+    public static Icon Person => new (Provider, "Person");
+
+}
--- src/OpenSmc.Layout/NavMenuControl.cs
+++ src/OpenSmc.Layout/NavMenuControl.cs
@@ -21,42 +21,50 @@ public record NavMenuControl()
         Items = Items.Add(navLink)
     };
 
-    public NavMenuControl WithGroup(string title, string href) =>
-        WithGroup(Controls.NavGroup.WithTitle(title).WithHref(href));
+    public NavMenuControl WithGroup(string title) =>
+        WithGroup(Controls.NavGroup.WithTitle(title));
 
-    public NavMenuControl WithGroup(string title, string href, Icon icon) =>
-        WithGroup(Controls.NavGroup.WithTitle(title).WithHref(href).WithIcon(icon));
+    public NavMenuControl WithGroup(string area, string title, Icon icon) =>
+        WithGroup(Controls.NavGroup.WithArea(area).WithTitle(title).WithIcon(icon));
 
-    public NavMenuControl WithNavLink(string title, string href) =>
-        WithNavLink(Controls.NavLink.WithTitle(title).WithHref(href));
+    public NavMenuControl WithNavLink(string area) =>
+        WithNavLink(Controls.NavLink(area));
 
-    public NavMenuControl WithNavLink(string title, string href, Icon icon) =>
-        WithNavLink(Controls.NavLink.WithTitle(title).WithHref(href).WithIcon(icon));
+    public NavMenuControl WithNavLink(string area, Icon icon) => 
+        WithNavLink(Controls.NavLink(area).WithIcon(icon));
+
+    public NavMenuControl WithNavLink(string area, string title, Icon icon) =>
+        WithNavLink(Controls.NavLink(area).WithTitle(title).WithIcon(icon));
 }
 
 public interface INavItem
 {
     string Title { get; init; }
-    string Href { get; init; }
+    string Area { get; init; }
     Icon Icon { get; init; }
 }
 
 public abstract record NavItem<TItem> : INavItem where TItem : NavItem<TItem>
 {
-    public string Title { get; init; }
+    public string Area { get; init; }
 
-    public string Href { get; init; }
+    public string Title { get; init; }
 
     public Icon Icon { get; init; }
 
     public TItem WithTitle(string title) => (TItem)(this with { Title = title });
 
-    public TItem WithHref(string href) => (TItem)(this with { Href = href });
+    public TItem WithArea(string area) => (TItem)(this with { Area = area });
 
     public TItem WithIcon(Icon icon) => (TItem)(this with { Icon = icon });
 }
 
-
-public record NavLink : NavItem<NavLink>;
+public record NavLink : NavItem<NavLink>
+{
+    public NavLink(string area)
+    {
+        Area = area;
+    }
+}
 
 public record NavGroup : NavItem<NavGroup>;
