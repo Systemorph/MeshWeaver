bf1ff5713bc48e290d5f8fc6458814430231675b
Roland Bürgi <rbuergi@systemorph.com>
2024-06-10 20:35:17 +0200

    Fixing handling of generics

--- src/OpenSmc.Messaging.Hub/Serialization/TypeRegistry.cs
+++ src/OpenSmc.Messaging.Hub/Serialization/TypeRegistry.cs
@@ -5,8 +5,34 @@ namespace OpenSmc.Serialization;
 
 public class TypeRegistry(ITypeRegistry parent) : ITypeRegistry
 {
-    private readonly ConcurrentDictionary<string, Type> typeByName = new();
-    private readonly ConcurrentDictionary<Type, string> nameByType = new();
+    private static readonly Type[] BasicTypes =
+    [
+        typeof(string),
+        typeof(int),
+        typeof(long),
+        typeof(short),
+        typeof(byte),
+        typeof(sbyte),
+        typeof(uint),
+        typeof(ulong),
+        typeof(ushort),
+        typeof(float),
+        typeof(double),
+        typeof(decimal),
+        typeof(char),
+        typeof(bool),
+        typeof(DateTime),
+        typeof(DateTimeOffset),
+        typeof(TimeSpan),
+        typeof(Guid),
+        typeof(Uri),
+        typeof(byte[])
+    ];
+
+    private readonly ConcurrentDictionary<string, Type> typeByName =
+        new(BasicTypes.Select(t => new KeyValuePair<string, Type>(t.Name, t)));
+    private readonly ConcurrentDictionary<Type, string> nameByType =
+        new(BasicTypes.Select(t => new KeyValuePair<Type, string>(t, t.Name)));
     private readonly ConcurrentDictionary<string, Func<object, object>> keysByType = new();
 
     public ITypeRegistry WithType(Type type) => WithType(type, FormatType(type));
@@ -29,19 +55,75 @@ public class TypeRegistry(ITypeRegistry parent) : ITypeRegistry
     {
         if (typeByName.TryGetValue(name, out type))
             return true;
+        if (name.Contains('[') && name.EndsWith(']'))
+        {
+            var typeName = name.Substring(0, name.IndexOf('['));
+            var baseType = typeByName.GetValueOrDefault(typeName);
+
+            if (baseType == null)
+                return false;
+
+            var genericArgs = name.Substring(
+                    name.IndexOf('[') + 1,
+                    name.Length - name.IndexOf('[') - 2
+                )
+                .Split(',');
+            var genericTypeArgs = new Type[genericArgs.Length];
+
+            for (int i = 0; i < genericArgs.Length; i++)
+            {
+                if (TryGetType(genericArgs[i].Trim(), out var genericTypeArg))
+                {
+                    genericTypeArgs[i] = genericTypeArg;
+                }
+                else
+                {
+                    baseType = null;
+                    return false;
+                }
+            }
+            type = baseType.MakeGenericType(genericTypeArgs);
+            return true;
+        }
         return parent?.TryGetType(name, out type) ?? false;
     }
 
-    public bool TryGetTypeName(Type type, out string typeName) =>
-        nameByType.TryGetValue(type, out typeName);
+    public bool TryGetTypeName(Type type, out string typeName)
+    {
+        if (nameByType.TryGetValue(type, out typeName))
+            return true;
+
+        if (type.IsGenericType)
+        {
+            var genericTypeDefinition = type.GetGenericTypeDefinition();
+            var genericArguments = type.GetGenericArguments();
+            var genericTypeArguments = new string[genericArguments.Length];
+            for (int i = 0; i < genericArguments.Length; i++)
+            {
+                if (!TryGetTypeName(genericArguments[i], out var genericTypeArgument))
+                {
+                    typeName = null;
+                    return false;
+                }
+                genericTypeArguments[i] = genericTypeArgument;
+            }
+            typeName =
+                $"{FormatType(genericTypeDefinition)}[{string.Join(',', genericTypeArguments)}]";
+            return true;
+        }
+
+        typeName = null;
+        return false;
+    }
 
     public string GetOrAddTypeName(Type type)
     {
         if (nameByType.TryGetValue(type, out var typeName))
             return typeName;
 
-        typeByName[type.FullName!] = type;
-        return nameByType[type] = type.FullName;
+        typeName = FormatType(type);
+        typeByName[typeName] = type;
+        return nameByType[type] = typeName;
     }
 
     public ITypeRegistry WithTypesFromAssembly(Type type, Func<Type, bool> filter) =>
@@ -54,14 +136,18 @@ public class TypeRegistry(ITypeRegistry parent) : ITypeRegistry
         return this;
     }
 
-    public static string FormatType(Type mainType)
+    public string FormatType(Type mainType)
     {
         var mainTypeName = (mainType.FullName ?? mainType.Name).Replace('\u002B', '.');
-        if (!mainType.IsGenericType)
+        if (!mainType.IsGenericType || mainType.IsGenericTypeDefinition)
             return mainTypeName;
 
+        var typeDefinition = mainType.GetGenericTypeDefinition();
+        if (typeDefinition == typeof(Nullable<>))
+            return FormatType(mainType.GetGenericArguments()[0]) + "?";
+
         var text =
-            $"{mainTypeName[..mainTypeName.IndexOf('`')]}[{string.Join(',', mainType.GetGenericArguments().Select(FormatType))}]";
+            $"{GetOrAddTypeName(typeDefinition)}[{string.Join(',', mainType.GetGenericArguments().Select(GetOrAddTypeName))}]";
         return text;
     }
 }
