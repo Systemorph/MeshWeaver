49dcf9629ee879bb5211bf67608c2137ba38f49b
Dmitry Kalabin <dkalabin@systemorph.com>
2024-06-11 20:04:03 +0200

    [TBRv] let SignalRClientPlugin to receive RawJson

--- test/OpenSmc.SignalR.Fixture/SignalRClientHubExtensions.cs
+++ test/OpenSmc.SignalR.Fixture/SignalRClientHubExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using OpenSmc.Messaging;
+using OpenSmc.Messaging.Serialization;
 
 namespace OpenSmc.SignalR.Fixture;
 
@@ -8,6 +9,7 @@ public static class SignalRClientHubExtensions
     public static MessageHubConfiguration AddSignalRClient(this MessageHubConfiguration config, Func<SignalRClientConfiguration, SignalRClientConfiguration> clientConfiguration)
         => config
             .Set(clientConfiguration)
+            .WithTypes(typeof(RawJson)) // HACK V10: currently we might push RawJson to client side so we do this workaround to be able to deserialize on client SignalR layer side. Later on we might change to unwrap from RawJson on SignalR server side and this might be removed then. (2024/06/11, Dmitry Kalabin)
             .AddPlugin<SignalRClientPlugin>();
 }
 
