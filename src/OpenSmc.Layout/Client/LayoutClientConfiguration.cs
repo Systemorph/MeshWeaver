c20f307a37a585344effa70172cde22b69cbb4e2
Roland Bürgi <rbuergi@systemorph.com>
2024-06-12 14:09:46 +0200

    Modularizing configuration for ChartJs

--- src/OpenSmc.Layout/Client/LayoutClientConfiguration.cs
+++ src/OpenSmc.Layout/Client/LayoutClientConfiguration.cs
@@ -20,9 +20,28 @@ public record LayoutClientConfiguration
     public LayoutClientConfiguration WithView<T>(ViewMap<T> viewMap)
         => this with { ViewMaps = ViewMaps.Insert(0, (i, s, a) => i is not T t ? default : viewMap.Invoke(t, s, a)) };
 
+    public LayoutClientConfiguration WithView<TViewModel, TView>()
+        => WithView((i, s, a) => i is not TViewModel vm ? null : StandardView<TViewModel, TView>(vm, s, a));
+
     public ViewDescriptor GetViewDescriptor(object instance, IChangeStream<JsonElement, LayoutAreaReference> stream, string area) =>
         ViewMaps.Select(m => m.Invoke(instance, stream, area)).FirstOrDefault(d => d is not null);
 
+    public const string ViewModel = nameof(ViewModel);
+
+    public static ViewDescriptor StandardView<TViewModel, TView>(
+        TViewModel instance,
+        IChangeStream<JsonElement, LayoutAreaReference> stream,
+        string area
+    ) =>
+        new(
+            typeof(TView),
+            new Dictionary<string, object>
+            {
+                { ViewModel, instance },
+                { nameof(Stream), stream },
+                { nameof(Area), area }
+            }
+        );
 
 
 }
