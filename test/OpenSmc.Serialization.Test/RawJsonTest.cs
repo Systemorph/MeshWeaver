04623d0c30e9f2a6c71b2fa39dbd94772f2e5d6a
Dmitry Kalabin <dkalabin@systemorph.com>
2024-06-13 13:01:31 +0200

    added RawJsonTest.WayBack() test

--- test/OpenSmc.Serialization.Test/RawJsonTest.cs
+++ test/OpenSmc.Serialization.Test/RawJsonTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using FluentAssertions;
+using FluentAssertions.Json;
 using Microsoft.Extensions.DependencyInjection;
 using OpenSmc.Data;
 using OpenSmc.Fixture;
@@ -78,5 +79,32 @@ public class RawJsonTest : TestBase
         var serializedOnServer = JsonSerializer.Serialize(deserialized, Router.JsonSerializerOptions);
 
         serializedOnServer.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void WayBack()
+    {
+        // arrange
+        var postOptions = new PostOptions(Client.Address, Client)
+            .WithTarget(new HostAddress())
+            .WithProperties(
+                new Dictionary<string, object>
+                {
+                    { "MyId", "394" },
+                    { "MyAddress", new ClientAddress() },
+                    { "MyId2", "22394" },
+                }
+            );
+        var dataChanged = new DataChangedEvent(new HostAddress(), new WorkspaceStateReference(), 10, new RawJson("{}"), ChangeType.Full, null);
+        var delivery = new MessageDelivery<DataChangedEvent>(dataChanged, postOptions);
+        var packedDelivery = delivery.Package(Router.JsonSerializerOptions);
+
+        // act
+        var serialized = JsonSerializer.Serialize(packedDelivery, Router.JsonSerializerOptions);
+
+        // assert
+        var actual = serialized.Should().NotBeNull().And.BeValidJson().Which;
+        var actualMessage = actual.Should().HaveElement("message").Which;
+        actualMessage.Should().HaveElement("$type").Which.Should().HaveValue(typeof(DataChangedEvent).FullName);
     }
 }
