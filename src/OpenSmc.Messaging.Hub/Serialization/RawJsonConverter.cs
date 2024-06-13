aaaf92f4a6ec83162376e2e8093e9d9606b220d2
Dmitry Kalabin <dkalabin@systemorph.com>
2024-06-13 11:09:14 +0200

    changed RawJsonConverter to unwrap RawJson on serialize

--- src/OpenSmc.Messaging.Hub/Serialization/RawJsonConverter.cs
+++ src/OpenSmc.Messaging.Hub/Serialization/RawJsonConverter.cs
@@ -15,15 +15,13 @@ public class RawJsonConverter : JsonConverter<RawJson>
 
     public override void Write(Utf8JsonWriter writer, RawJson value, JsonSerializerOptions options)
     {
-        // TODO V10: we just do the base logic here keeping RawJson in the output, but we might think about unwrapping it. However this requires to change generic type on top from MessageDelivery<RawJson> to be MessageDelivery<> around particular type (2024/06/11, Dmitry Kalabin)
-        var clonedOptions = CloneOptions(options);
-        JsonSerializer.Serialize(writer, value, clonedOptions);
-    }
-
-    private JsonSerializerOptions CloneOptions(JsonSerializerOptions options)
-    {
-        var clonedOptions = new JsonSerializerOptions(options);
-        clonedOptions.Converters.Remove(this);
-        return clonedOptions;
+        if (string.IsNullOrWhiteSpace(value?.Content))
+        {
+            writer.WriteNullValue();
+        }
+        else
+        {
+            writer.WriteRawValue(value.Content);
+        }
     }
 }
