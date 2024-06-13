20b150de0d48d3417e528299551746cc8e94650c
Alexander Kravets <akravets@systemorph.com>
2024-06-13 15:12:12 +0300

    first ag-grid draft

--- samples/OpenSmc.Northwind.Application/HubConfiguration.cs
+++ samples/OpenSmc.Northwind.Application/HubConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using OpenSmc.Application;
 using OpenSmc.Blazor;
+using OpenSmc.Blazor.AgGrid;
 using OpenSmc.Blazor.ChartJs;
 using OpenSmc.Messaging;
 using OpenSmc.Northwind.Model;
@@ -16,6 +17,7 @@ public static class HubConfiguration
         // TODO V10: add pre-compiled statement to remove all northwind related config (05.06.2024, Alexander Kravets)
         return configuration
                 .AddBlazor(x => x.AddChartJs())
+                .AddBlazor(x => x.AddAgGrid())
                 .WithHostedHub(new ApplicationAddress("Northwind", "dev"),
                     application => application
                         .AddNorthwindViewModels()
