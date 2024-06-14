f44fc05ea1685ea8e7c2c223aa980d1dc6b4641b
Dmitry Kalabin <dkalabin@systemorph.com>
2024-06-14 15:12:11 +0200

    a bit more real usage for DataChangedEvent in WayBack() test

--- test/OpenSmc.Serialization.Test/RawJsonTest.cs
+++ test/OpenSmc.Serialization.Test/RawJsonTest.cs
@@ -108,7 +108,9 @@ public class RawJsonTest : TestBase
                     { "MyId2", "22394" },
                 }
             );
-        var dataChanged = new DataChangedEvent(new HostAddress(), new WorkspaceStateReference(), 10, new RawJson("{}"), ChangeType.Full, null);
+        var entityStore = new EntityStore();
+        var entityStoreSerialized = JsonSerializer.Serialize(entityStore, Router.JsonSerializerOptions);
+        var dataChanged = new DataChangedEvent(new HostAddress(), new WorkspaceStateReference(), 10, new RawJson(entityStoreSerialized), ChangeType.Full, null);
         var delivery = new MessageDelivery<DataChangedEvent>(dataChanged, postOptions);
         var packedDelivery = delivery.Package(Router.JsonSerializerOptions);
 
