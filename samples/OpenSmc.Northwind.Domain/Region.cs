28e7e8ae1a37409c9ad8ac0fd5d04a43554474ca
Roland Bürgi <rbuergi@systemorph.com>
2024-06-29 23:05:57 +0200

    Refactoring Northwind

--- samples/OpenSmc.Northwind.Domain/Region.cs
+++ samples/OpenSmc.Northwind.Domain/Region.cs
@@ -1,5 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using OpenSmc.Domain;
 
 namespace OpenSmc.Northwind.Domain;
 
-public record Region([property: Key] int RegionId, string RegionDescription);
+public record Region([property: Key] int RegionId, string RegionDescription) : INamed
+{
+    string INamed.DisplayName => RegionDescription;
+}
