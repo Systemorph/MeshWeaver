7b00d24840f62a578a3966cda3464ea6967f793c
Roland Bürgi <rbuergi@systemorph.com>
2024-07-13 00:05:24 +0200

    Draft for DynamicMarkdown

---
+++ test/OpenSmc.Documentation.Test/DynamicMarkdownTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using OpenSmc.Documentation.Markdown;
+using OpenSmc.Hub.Fixture;
+using Xunit.Abstractions;
+
+namespace OpenSmc.Documentation.Test;
+
+public class DynamicMarkdownTest(ITestOutputHelper output) : HubTestBase(output)
+{
+
+    [HubFact]
+    public void TestMarkdownParser()
+    {
+        // Define a sample markdown string
+        var markdown =
+            @"
+[LayoutArea Area=""Main"", Options=""{ 'key': 'value' }""]
+[LayoutArea Area=""Sidebar"", SourceReference=""SomeReference""]";
+
+        // Assuming ParseMarkdown is a method of a class that parses the markdown
+        var parser = new MarkdownComponentParser(GetHost());
+        var components = parser.ParseMarkdown(markdown);
+
+        // Verify the results
+        components.Should().NotBeNull();
+        components.Should().HaveCount(2);
+        components[0].Should().BeOfType<LayoutAreaComponentInfo>();
+        ((LayoutAreaComponentInfo)components[0]).Area.Should().Be("Main");
+        ((LayoutAreaComponentInfo)components[0]).Options.Should().ContainKey("key");
+
+    }
+}
