d7b73da4af39cf65b0d075c8e89d8a95af79f025
Dmitry Kalabin <dkalabin@systemorph.com>
2024-07-30 13:45:55 +0200

    added simple Terms and Conditions acceptance control demo example
    (simple checkbox has issues currently)

---
+++ samples/OpenSmc.Demo.ViewModel/CkeckBox/TermsAgreementTickArea.cs
@@ -0,0 +1,24 @@
+﻿using OpenSmc.Application.Styles;
+using OpenSmc.Layout;
+using OpenSmc.Layout.Composition;
+
+namespace OpenSmc.Demo.ViewModel.CkeckBox;
+
+public static class TermsAgreementTickArea
+{
+    public static object TermsTick(LayoutAreaHost area, RenderingContext context)
+        => Controls
+            .Stack()
+            .WithVerticalGap(16)
+            .WithView(
+                "AgreeWithTerms",
+                Controls.Html("I agree with the Terms and Conditions")
+                //Controls.CheckBox("I agree with the Terms and Conditions", false)
+            )
+            .WithView(
+                "SubmitAgreement",
+                Controls.Button("Submit")
+                    .WithIconStart(FluentIcons.Edit)
+            )
+        ;
+}
