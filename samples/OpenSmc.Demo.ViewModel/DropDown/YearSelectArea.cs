f02589ec9c8517b8e8ed353c2c7646df834191de
Dmitry Kalabin <dkalabin@systemorph.com>
2024-07-30 09:11:32 +0200

    added simple year selection dropdown control demo example

---
+++ samples/OpenSmc.Demo.ViewModel/DropDown/YearSelectArea.cs
@@ -0,0 +1,16 @@
+﻿using OpenSmc.Layout;
+using OpenSmc.Layout.Composition;
+
+namespace OpenSmc.Demo.ViewModel.DropDown;
+
+public static class YearSelectArea
+{
+    public static object YearSelect(LayoutAreaHost area, RenderingContext context)
+        => Controls
+            .Stack()
+            .WithView(
+                "SelectYear",
+                Controls.Select(2022).WithOptions([new Option<int>(2023, "2023"), new Option<int>(2022, "2022"), new Option<int>(2021, "2021"),])
+            )
+        ;
+}
