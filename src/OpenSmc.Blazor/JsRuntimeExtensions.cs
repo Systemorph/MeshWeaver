8173b4017566d99501318b479036089f88f8c463
Alexander Kravets <akravets+gh1@systemorph.com>
2024-07-30 16:32:30 +0300

    generalizing script import

---
+++ src/OpenSmc.Blazor/JsRuntimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.JSInterop;
+
+namespace OpenSmc.Blazor
+{
+    public static class JsRuntimeExtensions
+    {
+        public static ValueTask<IJSObjectReference> Import(this IJSRuntime jsRuntime, Assembly assembly, string script) =>
+            jsRuntime.InvokeAsync<IJSObjectReference>("import", $"./_content/{assembly.GetName().Name}/{script}");
+
+        public static ValueTask<IJSObjectReference> Import(this IJSRuntime jsRuntime, string script) =>
+            jsRuntime.Import(Assembly.GetCallingAssembly(), script);
+    }
+}
