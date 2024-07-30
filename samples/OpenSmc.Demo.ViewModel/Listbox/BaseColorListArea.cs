3b5ebaf70101c34d5cee18feec149613a95400d1
Dmitry Kalabin <dkalabin@systemorph.com>
2024-07-30 13:42:21 +0200

    added simple base color selection listbox control demo example

---
+++ samples/OpenSmc.Demo.ViewModel/Listbox/BaseColorListArea.cs
@@ -0,0 +1,24 @@
+﻿using OpenSmc.Layout.Composition;
+using OpenSmc.Layout;
+
+namespace OpenSmc.Demo.ViewModel.Listbox;
+
+public static class BaseColorListArea
+{
+    public static object BaseColorList(LayoutAreaHost area, RenderingContext context)
+        => Controls
+            .Stack()
+            .WithView(
+                "BaseColorSelect",
+                Controls.Listbox("Y")
+                    .WithOptions([
+                        new Option<string>("R", "Red"),
+                        new Option<string>("G", "Green"),
+                        new Option<string>("B", "Blue"),
+                        new Option<string>("Y", "Yellow"),
+                        new Option<string>("M", "Magenta"),
+                        new Option<string>("C", "Cyan"),
+                    ])
+            )
+        ;
+}
