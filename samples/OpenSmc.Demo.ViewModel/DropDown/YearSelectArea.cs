e841b94838df136f49f7da301a92e2070755caa9
Dmitry Kalabin <dkalabin@systemorph.com>
2024-07-31 16:26:41 +0200

    a bit of formatting

--- samples/OpenSmc.Demo.ViewModel/DropDown/YearSelectArea.cs
+++ samples/OpenSmc.Demo.ViewModel/DropDown/YearSelectArea.cs
@@ -8,28 +8,29 @@ public static class YearSelectArea
 {
     public static object YearSelect(LayoutAreaHost area, RenderingContext context)
         => Controls
-                .Stack()
-                .WithView(
-                    (a, _) =>
-                        a.Bind(
-                            new ChosenYear(2022),
-                            nameof(ChosenYear),
-                            sy => Controls.Select(sy.Year)
-                                .WithOptions(new[]
-                                {
-                                    new Option<int>(2023, "2023"),
-                                    new Option<int>(2022, "2022"),
-                                    new Option<int>(2021, "2021"),
-                                })
-                        )
-                )
-                .WithView(
-                    nameof(ShowSelectedYear),
-                    (a, _) => a
-                        .GetDataStream<ChosenYear>(nameof(ChosenYear))
-                        .Select(x => ShowSelectedYear(x.Year))
-                )
-            ;
+            .Stack()
+            .WithVerticalGap(16)
+            .WithView(
+                (a, _) =>
+                    a.Bind(
+                        new ChosenYear(2022),
+                        nameof(ChosenYear),
+                        sy => Controls.Select(sy.Year)
+                            .WithOptions(new[]
+                            {
+                                new Option<int>(2023, "2023"),
+                                new Option<int>(2022, "2022"),
+                                new Option<int>(2021, "2021"),
+                            })
+                    )
+            )
+            .WithView(
+                nameof(ShowSelectedYear),
+                (a, _) => a
+                    .GetDataStream<ChosenYear>(nameof(ChosenYear))
+                    .Select(x => ShowSelectedYear(x.Year))
+            )
+        ;
 
     private static object ShowSelectedYear(int year) => Controls.Html($"Year selected: {year}");
 }
