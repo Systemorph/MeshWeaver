3149cc39c0d4baf97dcf4656a1efb65309ba45a6
Dmitry Kalabin <dkalabin@systemorph.com>
2024-07-31 10:44:42 +0200

    updated to make databinding work with Year Selection control demo

--- samples/OpenSmc.Demo.ViewModel/DropDown/YearSelectArea.cs
+++ samples/OpenSmc.Demo.ViewModel/DropDown/YearSelectArea.cs
@@ -1,4 +1,5 @@
-﻿using OpenSmc.Layout;
+﻿using System.Reactive.Linq;
+using OpenSmc.Layout;
 using OpenSmc.Layout.Composition;
 
 namespace OpenSmc.Demo.ViewModel.DropDown;
@@ -7,10 +8,30 @@ public static class YearSelectArea
 {
     public static object YearSelect(LayoutAreaHost area, RenderingContext context)
         => Controls
-            .Stack()
-            .WithView(
-                "SelectYear",
-                Controls.Select(2022).WithOptions([new Option<int>(2023, "2023"), new Option<int>(2022, "2022"), new Option<int>(2021, "2021"),])
-            )
-        ;
+                .Stack()
+                .WithView(
+                    (a, _) =>
+                        a.Bind(
+                            new ChosenYear(2022),
+                            nameof(ChosenYear),
+                            sy => Controls.Select(sy.Year)
+                                .WithOptions(new[]
+                                {
+                                    new Option<int>(2023, "2023"),
+                                    new Option<int>(2022, "2022"),
+                                    new Option<int>(2021, "2021"),
+                                })
+                        )
+                )
+                .WithView(
+                    nameof(ShowSelectedYear),
+                    (a, _) => a
+                        .GetDataStream<ChosenYear>(nameof(ChosenYear))
+                        .Select(x => ShowSelectedYear(x.Year))
+                )
+            ;
+
+    private static object ShowSelectedYear(int year) => Controls.Html($"Year selected: {year}");
 }
+
+internal record ChosenYear(int Year);
