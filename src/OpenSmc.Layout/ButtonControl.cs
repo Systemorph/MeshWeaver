051a6af4b0aef2e4687c345fbbfb7c6207aa7f53
Dmitry Kalabin <dkalabin@systemorph.com>
2024-08-01 13:03:59 +0200

    added Disabled  functionality to button control

--- src/OpenSmc.Layout/ButtonControl.cs
+++ src/OpenSmc.Layout/ButtonControl.cs
@@ -5,9 +5,12 @@ public record ButtonControl(object Data)
 {
     public object IconStart { get; init; }
     public object IconEnd { get; init; }
+    public bool? Disabled { get; init; }
     public object Appearance { get; set; }
 
     public ButtonControl WithIconStart(object icon) => this with { IconStart = icon };
 
     public ButtonControl WithIconEnd(object icon) => this with { IconEnd = icon };
+
+    public ButtonControl WithDisabled(bool disabled) => this with { Disabled = disabled };
 }
