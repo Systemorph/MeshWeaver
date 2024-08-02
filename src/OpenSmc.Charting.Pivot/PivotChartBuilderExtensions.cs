a2cd5dfea47e04755d5afd77ad8f4332ce0bb898
Alexander Kravets <akravets+gh1@systemorph.com>
2024-08-02 15:22:12 +0300

    renaming, adding chart builder extension

---
+++ src/OpenSmc.Charting.Pivot/PivotChartBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenSmc.Charting.Pivot;
+
+public static class PivotChartBuilderExtensions
+{
+    public static ChartControl ToChartControl(this IPivotChartBuilder builder)
+    {
+        return new (builder.Execute());
+    }
+}
---
+++ src/OpenSmc.Charting/ChartControl.cs
@@ -0,0 +1,6 @@
+﻿using OpenSmc.Layout;
+
+namespace OpenSmc.Charting;
+
+public record ChartControl(object Data)
+    : UiControl<ChartControl>(typeof(ChartControl).Namespace, "1.0", Data);
