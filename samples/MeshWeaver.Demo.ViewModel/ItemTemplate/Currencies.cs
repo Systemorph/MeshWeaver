50d9ba854ddcc98429328b6049a5df80f77ea579
Dmitry Kalabin <dkalabin@systemorph.com>
2024-08-02 13:53:51 +0200

    added show case with Currency checkboxes

---
+++ samples/MeshWeaver.Demo.ViewModel/ItemTemplate/Currencies.cs
@@ -0,0 +1,32 @@
+﻿using MeshWeaver.Layout.Composition;
+using MeshWeaver.Layout;
+
+namespace MeshWeaver.Demo.ViewModel.ItemTemplate;
+
+public static class Currencies
+{
+    private static readonly List<(string SystemName, string DisplayName)> currencyNames =
+        [
+            ("CHF", "Swiss Franc"),
+            ("EUR", "Euro"),
+            ("USD", "US Dollar"),
+            ("GBP", "British Pound"),
+            ("CNY", "Chinese Yuan"),
+        ];
+
+    public static object CurrenciesFilter(LayoutAreaHost area, RenderingContext context)
+        => Controls
+            .Stack
+            .WithView(
+                (a, _) =>
+                    Template.Bind(
+                        new CurrenciesFilter(currencyNames.Select(c => new CurrenciesFilterItem(c.SystemName, c.DisplayName, false)).ToList()),
+                        nameof(CurrenciesFilter),
+                        cf => Template.ItemTemplate(cf.Data, c => Controls.CheckBox(c.Label, c.Selected))
+                    )
+            );
+}
+
+internal record CurrenciesFilter(List<CurrenciesFilterItem> Data);
+
+public record CurrenciesFilterItem(object Id, string Label, bool Selected);
--- samples/MeshWeaver.Demo.ViewModel/ItemTemplate/ItemTemplateDemoArea.cs
+++ samples/MeshWeaver.Demo.ViewModel/ItemTemplate/ItemTemplateDemoArea.cs
@@ -1,5 +1,19 @@
-﻿namespace MeshWeaver.Demo.ViewModel.ItemTemplate;
+﻿using MeshWeaver.Application.Styles;
+using MeshWeaver.Layout;
+using MeshWeaver.Layout.Composition;
+using MeshWeaver.Layout.Domain;
+
+namespace MeshWeaver.Demo.ViewModel.ItemTemplate;
 
 public static class ItemTemplateDemoArea
 {
+    public static LayoutDefinition AddCurrenciesItemTemplateDemo(this LayoutDefinition layout)
+        => layout.WithView(nameof(Currencies.CurrenciesFilter), Currencies.CurrenciesFilter)
+            .WithNavMenu((menu, _, _) => menu
+                .WithNavLink(
+                    "Raw: ItemTemplate (currencies)",
+                    new LayoutAreaReference(nameof(Currencies.CurrenciesFilter)).ToHref(layout.Hub.Address),
+                    FluentIcons.Person
+                )
+            );
 }
