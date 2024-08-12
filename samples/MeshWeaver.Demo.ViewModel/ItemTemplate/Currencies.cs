ab2f485481a4c6ccf36998d8d660479a006d61f4
Dmitry Kalabin <dkalabin@systemorph.com>
2024-08-12 10:07:16 +0200

    added data binding to show info about selected currencies

--- samples/MeshWeaver.Demo.ViewModel/ItemTemplate/Currencies.cs
+++ samples/MeshWeaver.Demo.ViewModel/ItemTemplate/Currencies.cs
@@ -1,4 +1,5 @@
-﻿using MeshWeaver.Layout.Composition;
+﻿using System.Reactive.Linq;
+using MeshWeaver.Layout.Composition;
 using MeshWeaver.Layout;
 
 namespace MeshWeaver.Demo.ViewModel.ItemTemplate;
@@ -17,6 +18,7 @@ public static class Currencies
     public static object CurrenciesFilter(LayoutAreaHost area, RenderingContext context)
         => Controls
             .Stack
+            .WithVerticalGap(16)
             .WithView(
                 (a, _) =>
                     Template.Bind(
@@ -24,7 +26,14 @@ public static class Currencies
                         nameof(CurrenciesFilter),
                         cf => Template.ItemTemplate(cf.Data, c => Controls.CheckBox(c.Label, c.Selected))
                     )
+            )
+            .WithView((a, _) => a
+                .GetDataStream<CurrenciesFilter>(nameof(CurrenciesFilter))
+                .Select(x => ShowSelectedCurrencies(x.Data)), nameof(ShowSelectedCurrencies)
             );
+
+    private static object ShowSelectedCurrencies(IEnumerable<CurrenciesFilterItem> currencyData)
+        => Controls.Html($"Currencies: {string.Join(",", currencyData.Where(x => x.Selected).Select(x => x.Id.ToString()))}");
 }
 
 internal record CurrenciesFilter(List<CurrenciesFilterItem> Data);
