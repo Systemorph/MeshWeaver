80a217ebcbab0159746fe61be43c8218f6c70cad
Roland Bürgi <rbuergi@systemorph.com>
2024-08-16 10:14:17 +0200

    changes during demo

--- src/MeshWeaver.Layout/ComboboxControl.cs
+++ src/MeshWeaver.Layout/ComboboxControl.cs
@@ -1,4 +1,8 @@
-﻿namespace MeshWeaver.Layout;
+﻿using System.Reflection;
+using MeshWeaver.Layout.DataBinding;
+using MeshWeaver.Reflection;
+
+namespace MeshWeaver.Layout;
 
 public record ComboboxControl(object Data) : ListControlBase<ComboboxControl>(Data), IListControl
 {
@@ -8,11 +12,29 @@ public record ComboboxControl(object Data) : ListControlBase<ComboboxControl>(Da
     public object Position { get; init; }
     public object Disabled { get; init; }
 
-    ComboboxControl WithAutofocus(object autofocus) => this with {Autofocus = autofocus};
-    ComboboxControl WithAutocomplete(object autocomplete) => this with {Autocomplete = autocomplete};
-    ComboboxControl WithPlaceholder(object placeholder) => this with {Placeholder = placeholder};
-    ComboboxControl WithPosition(object position) => this with {Position = position};
-    ComboboxControl WithDisabled(object disabled) => this with {Disabled = disabled};
+    [ReplaceMethods]
+    public ComboboxControl WithAutofocus(bool autofocus) => this with {Autofocus = autofocus};
+    public ComboboxControl WithAutocomplete(object autocomplete) => this with {Autocomplete = autocomplete};
+    public ComboboxControl WithPlaceholder(object placeholder) => this with {Placeholder = placeholder};
+    public ComboboxControl WithPosition(object position) => this with {Position = position};
+    public ComboboxControl WithDisabled(object disabled) => this with {Disabled = disabled};
+
+    internal ComboboxControl WithAutofocus(object autofocus) => this with { Autofocus = autofocus };
+
+    private class ReplaceMethodsAttribute : ReplaceMethodInTemplateAttribute
+    {
+        private static readonly Dictionary<MethodInfo, MethodInfo> MethodMap = new()
+        {
+            {
+                ReflectionHelper.GetMethod<ComboboxControl>(x => x.WithAutofocus(default(bool))),
+                ReflectionHelper.GetMethod<ComboboxControl>(x => x.WithAutofocus(default(object)))
+            }
+        };
+        public override MethodInfo Replace(MethodInfo expression)
+        {
+            return MethodMap[expression];
+        }
+    }
 }
 
 public enum ComboboxAutocomplete
