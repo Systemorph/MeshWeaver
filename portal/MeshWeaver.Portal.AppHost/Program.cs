4217d04817adfe280c23028e0cacfb101cd7a392
Roland Bürgi <rbuergi@systemorph.com>
2024-08-23 13:00:36 +0200

    An iteration on the project layout

--- portal/MeshWeaver.Portal.AppHost/Program.cs
+++ portal/MeshWeaver.Portal.AppHost/Program.cs
@@ -2,11 +2,26 @@
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.MeshWeaver_Portal_Orleans>("apiservice");
+var apiService = builder.AddProject<Projects.MeshWeaver_Portal_Backend>("backend");
 
-builder.AddProject<Projects.MeshWeaver_Portal_Web>("webfrontend")
+builder.AddProject<Projects.MeshWeaver_Portal_Web>("frontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
+
+// Add the Orleans resource to the Aspire DistributedApplication
+// builder, then configure it with Redis for clustering.
+// We do not explicitly include any grain storage, as this is 
+// typically done lower level in the message hubs.
+var orleans = builder.AddOrleans("default").WithClustering(cache);
+
+// Add our server project and reference your 'orleans' resource from it.
+// it can join the Orleans cluster as a service.
+// This implicitly add references to the required resources.
+// In this case, that is the 'clusteringTable' resource declared earlier.
+builder.AddProject<Projects.MeshWeaver_Portal_Orleans>("silo")
+       .WithReference(orleans)
+       .WithReplicas(3);
+
 
 builder.Build().Run();
