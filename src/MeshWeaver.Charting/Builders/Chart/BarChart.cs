953578bae5b63815538f99b2cd1f052808266292
Dmitry Kalabin <dkalabin@systemorph.com>
2024-08-27 12:58:26 +0200

    updates for Horizontal Bar charts

--- src/MeshWeaver.Charting/Builders/Chart/BarChart.cs
+++ src/MeshWeaver.Charting/Builders/Chart/BarChart.cs
@@ -1,4 +1,5 @@
-﻿using MeshWeaver.Charting.Enums;
+﻿using System.Text.Json.Serialization;
+using MeshWeaver.Charting.Enums;
 using MeshWeaver.Charting.Models;
 
 namespace MeshWeaver.Charting.Builders.Chart;
@@ -13,5 +14,6 @@ public record BarChart
         return WithOptions(options => options.WithIndexAxis("y"));
     }
 
+    [JsonIgnore]
     public bool IsHorizontal => Options.IndexAxis == "y";
 }
