cea6c8f60587ee7d561840eb2e56dcf908c402d9
Dmitry Kalabin <dkalabin@systemorph.com>
2024-08-27 14:31:26 +0200

    moved styling support to WaterfallChartOptions

--- src/MeshWeaver.Charting/Builders/Options/WaterfallChartOptions.cs
+++ src/MeshWeaver.Charting/Builders/Options/WaterfallChartOptions.cs
@@ -1,3 +1,11 @@
-﻿namespace MeshWeaver.Charting.Builders.Options;
+﻿using MeshWeaver.Charting.Builders.OptionsBuilders;
 
-public record WaterfallChartOptions;
+namespace MeshWeaver.Charting.Builders.Options;
+
+public record WaterfallChartOptions
+{
+    internal Func<WaterfallStylingBuilder, WaterfallStylingBuilder> StylingOptions { get; init; }
+
+    public WaterfallChartOptions WithStylingOptions(Func<WaterfallStylingBuilder, WaterfallStylingBuilder> styling)
+        => this with { StylingOptions = styling, };
+}
