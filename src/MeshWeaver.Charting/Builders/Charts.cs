e9b2d710199f2fe9c79545d3b7156764c672f0c3
Dmitry Kalabin <dkalabin@systemorph.com>
2024-09-03 14:46:34 +0200

    returned Charts.Waterfall and Charts.HorizontalWaterfall as a bit of syntax sugar

--- src/MeshWeaver.Charting/Builders/Charts.cs
+++ src/MeshWeaver.Charting/Builders/Charts.cs
@@ -1,4 +1,5 @@
 ﻿using MeshWeaver.Charting.Builders.Chart;
+using MeshWeaver.Charting.Builders.Options;
 using MeshWeaver.Charting.Enums;
 using MeshWeaver.Charting.Models;
 using MeshWeaver.Charting.Models.Segmented;
@@ -16,6 +17,10 @@ public static class Charts
     public static Models.Chart FloatingBar(IReadOnlyCollection<FloatingBarDataSet> dataSets) => new(dataSets, ChartType.Bar);
     public static Models.Chart HorizontalFloatingBar(IReadOnlyCollection<HorizontalFloatingBarDataSet> dataSets)
         => new Models.Chart(dataSets, ChartType.Bar).WithOptions(o => o.WithIndexAxis("y"));
+    public static Models.Chart Waterfall(List<double> deltas, Func<WaterfallChartOptions, WaterfallChartOptions> options = null)
+        => Bar([]).ToWaterfallChart(deltas, options);
+    public static Models.Chart HorizontalWaterfall(List<double> deltas, Func<HorizontalWaterfallChartOptions, HorizontalWaterfallChartOptions> options = null)
+        => Bar([]).ToHorizontalWaterfallChart(deltas, options);
     public static Models.Chart Scatter(IReadOnlyCollection<LineScatterDataSet> dataSets) => new(dataSets, ChartType.Scatter);
     public static Models.Chart Bubble(IReadOnlyCollection<BubbleDataSet> dataSets) => new(dataSets, ChartType.Bubble);
     public static Models.Chart TimeLine(IReadOnlyCollection<TimeLineDataSet> dataSets) => new(dataSets, ChartType.Line);
