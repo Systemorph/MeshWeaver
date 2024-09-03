6e6e22cadb832f94a73b6c8d120d0b5158d8f3ea
Dmitry Kalabin <dkalabin@systemorph.com>
2024-09-03 17:40:42 +0200

    changed PivotBarChartBuilder to try to operate on basic Chart rather than on specific BarChart

--- src/MeshWeaver.Charting.Pivot/IPivotBarChartBuilder.cs
+++ src/MeshWeaver.Charting.Pivot/IPivotBarChartBuilder.cs
@@ -1,11 +1,11 @@
-﻿using MeshWeaver.Charting.Builders.Chart;
+﻿using MeshWeaver.Charting.Models;
 
 namespace MeshWeaver.Charting.Pivot;
 
 public interface IPivotBarChartBuilder : IPivotChartBuilder
 {
     new IPivotBarChartBuilder WithOptions(Func<PivotChartModel, PivotChartModel> postProcessor);
-    IPivotBarChartBuilder WithChartBuilder(Func<BarChart, BarChart> builder);
+    IPivotBarChartBuilder WithChartBuilder(Func<Chart, Chart> builder);
     IPivotBarChartBuilder AsStackedWithScatterTotals(); // rename it something better, and/or split into several methods
     IPivotChartBuilder WithRowsAsLine(params string[] rowLinesNames);
 }
