2f09d0ebdf116a34c166f55acba604b204ffd94e
Alexander Kravets <akravets@systemorph.com>
2024-09-12 18:04:46 +0300

    category selector draft

--- modules/Northwind/MeshWeaver.Northwind.ViewModel/SalesOverviewArea.cs
+++ modules/Northwind/MeshWeaver.Northwind.ViewModel/SalesOverviewArea.cs
@@ -21,7 +21,7 @@ public static class SalesOverviewArea
         => 
             layout
                 .WithView(nameof(SalesByCategory), Controls.Stack.WithView(SalesByCategory))
-                .WithView(nameof(TopProducts), Controls.Stack.WithView(TopProducts))
+                .WithView(nameof(TopProducts), TopProducts)
     ;
 
     public static IObservable<object> SalesByCategory(this LayoutAreaHost layoutArea, RenderingContext context)
@@ -44,22 +44,49 @@ public static class SalesOverviewArea
             );
     }
 
-    public static IObservable<object> TopProducts(this LayoutAreaHost layoutArea, RenderingContext context)
+    record ProductsToolbar(int Category);
+
+    public static LayoutStackControl TopProducts(this LayoutAreaHost layoutArea, RenderingContext context)
     {
-        return layoutArea.YearlyNorthwindData()
-            .Select(data =>
-                layoutArea.Workspace
-                    .State
-                    .Pivot(data.ToDataCube())
-                    .SliceColumnsBy(nameof(Product))
-                    .ToBarChart(builder => builder
-                        .WithOptions(o => o.OrderByValueDescending().TopValues(5))
-                        .WithChartBuilder(o =>
-                            o
-                                .AsHorizontal()
-                                .WithDataLabels()
-                        )
+        return Controls.Stack
+            .WithView(Controls.H3("Top products"))
+            .WithView(Controls.Toolbar.WithView(
+                    Template.Bind(new ProductsToolbar(0), nameof(ProductsToolbar),
+                        tb =>
+                            Controls.Select(tb.Category)
+                                .WithOptions(new Option[]
+                                {
+                                    new Option<int>(0, "All"),
+                                })
                     )
-            );
+                )
+            )
+            .WithView((area, _) =>
+                area.GetProductsDataCube()
+                .Select(d =>
+                    area.Workspace
+                        .State
+                        .Pivot(d)
+                        .SliceColumnsBy(nameof(Product))
+                        .ToBarChart(builder => builder
+                            .WithOptions(o => o.OrderByValueDescending().TopValues(5))
+                            .WithChartBuilder(o =>
+                                o
+                                    .AsHorizontal()
+                                    .WithDataLabels()
+                            )
+                        )
+                ));
     }
+
+    private static IObservable<IDataCube<NorthwindDataCube>> GetProductsDataCube(this LayoutAreaHost layoutArea)
+        => layoutArea.YearlyNorthwindData()
+            .CombineLatest(
+                layoutArea.GetDataStream<ProductsToolbar>(nameof(ProductsToolbar)).DistinctUntilChanged(),
+                (data, tb) => (data, tb))
+            .Select(x => x.data.ToDataCube()
+                .Filter(d => x.tb.Category == 0 || d.Category == x.tb.Category)
+            )
+            ;
+
 }
