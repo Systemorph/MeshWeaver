646d8d62ceadd9f9591a42dc4184c49414ca0cdb
Dmitry Kalabin <dkalabin@systemorph.com>
2024-09-12 16:06:59 +0200

    added code to show Top Clients chart

---
+++ modules/Northwind/MeshWeaver.Northwind.ViewModel/ClientsOverviewArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Reactive.Linq;
+using MeshWeaver.Charting.Models;
+using MeshWeaver.Charting.Pivot;
+using MeshWeaver.DataCubes;
+using MeshWeaver.Layout;
+using MeshWeaver.Layout.Composition;
+using MeshWeaver.Northwind.Domain;
+using MeshWeaver.Pivot.Builder;
+
+namespace MeshWeaver.Northwind.ViewModel;
+
+public static class ClientsOverviewArea
+{
+    public static LayoutDefinition AddClientsOverview(this LayoutDefinition layout)
+        => layout.WithView(nameof(TopClients), Controls.Stack.WithView(TopClients));
+
+    public static IObservable<object> TopClients(this LayoutAreaHost layoutArea, RenderingContext context)
+        => layoutArea.GetDataCube()
+            .Select(data =>
+                layoutArea.Workspace
+                    .State
+                    .Pivot(data.ToDataCube())
+                    .SliceColumnsBy(nameof(Customer))
+                    .ToBarChart(builder => builder
+                        .WithOptions(o => o.OrderByValueDescending().TopValues(5))
+                        .WithChartBuilder(o => o
+                            .AsHorizontal()
+                            .WithDataLabels()
+                        )
+                    )
+            );
+
+    private static IObservable<IEnumerable<NorthwindDataCube>> GetDataCube(this LayoutAreaHost area)
+        => area.GetNorthwindDataCubeData()
+            .Select(dc => dc.Where(x => x.OrderDate >= new DateTime(1997, 12, 1) && x.OrderDate < new DateTime(1998, 1, 1)));
+}
