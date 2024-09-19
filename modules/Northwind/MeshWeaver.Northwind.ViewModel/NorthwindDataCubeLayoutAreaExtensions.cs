aea27d673eebcde8b3476679c2dfa7ca547a8265
Alexander Kravets <akravets@systemorph.com>
2024-09-19 10:52:28 +0300

    deduplicating order details

--- modules/Northwind/MeshWeaver.Northwind.ViewModel/NorthwindDataCubeLayoutAreaExtensions.cs
+++ modules/Northwind/MeshWeaver.Northwind.ViewModel/NorthwindDataCubeLayoutAreaExtensions.cs
@@ -39,8 +39,6 @@ public static class NorthwindDataCubeLayoutAreaExtensions
                                 (od, product) => (od.order, od.detail, product)
                             )
                             .Select(data => new NorthwindDataCube(data.order, data.detail, data.product))
-                            // TODO V10: fix duplicate data loading (18.09.2024, Alexander Kravets)
-                            .DistinctBy(d => new {d.OrderId, d.Product})
                     )
             );
 
