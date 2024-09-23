4efdbcea21937ad7f4951a1231077490ddb6d5f7
Anna Kuleshova <akuleshova@systemorph.com>
2024-09-23 13:47:39 +0300

    xml comments added

--- src/MeshWeaver.Layout/LayoutAreaControl.cs
+++ src/MeshWeaver.Layout/LayoutAreaControl.cs
@@ -1,12 +1,32 @@
 ﻿using MeshWeaver.Utils;
 
 namespace MeshWeaver.Layout;
+/// <summary>
+/// Represents a layout area control with customizable properties.
+/// </summary>
+/// <remarks>
+/// For more information, visit the 
+/// <a href="https://www.fluentui-blazor.net/layout">Fluent UI Blazor Layout documentation</a>.
+/// </remarks>
+/// <param name="Address">The address associated with the layout area control.</param>
+/// <param name="Reference">The reference to the layout area.</param>
 
 public record LayoutAreaControl(object Address, LayoutAreaReference Reference)
     : UiControl<LayoutAreaControl>(ModuleSetup.ModuleName, ModuleSetup.ApiVersion)
 {
+     /// <summary>
+    /// Gets or initializes the display area of the layout area control.
+    /// </summary>
     public object DisplayArea { get; init; } = Reference.Area.Wordify();
+    /// <summary>
+    /// Gets or initializes the progress display state of the layout area control.
+    /// </summary>
     public object ShowProgress { get; init; } = true;
+    /// <summary>
+    /// Sets the display area of the layout area control.
+    /// </summary>
+    /// <param name="displayArea">The display area to set.</param>
+    /// <returns>A new <see cref="LayoutAreaControl"/> instance with the specified display area.</returns>
 
     public LayoutAreaControl WithDisplayArea(string displayArea) => this with { DisplayArea = displayArea };
 
@@ -22,12 +42,18 @@ public record LayoutAreaControl(object Address, LayoutAreaReference Reference)
                && JsonObjectEqualityComparer.Instance.Equals(Address, other.Address)
                ;
     }
-
+/// <summary>
+    /// Returns a string that represents the current <see cref="LayoutAreaControl"/>.
+    /// </summary>
+    /// <returns>A string that represents the current <see cref="LayoutAreaControl"/>.</returns>
     public override string ToString()
     {
         return Reference.ToAppHref(Address);
     }
-
+ /// <summary>
+    /// Serves as the default hash function.
+    /// </summary>
+    /// <returns>A hash code for the current <see cref="LayoutAreaControl"/>.</returns>
     public override int GetHashCode()
     {
         return HashCode.Combine(
--- src/MeshWeaver.Layout/LayoutControl.cs
+++ src/MeshWeaver.Layout/LayoutControl.cs
@@ -1,12 +1,39 @@
 ﻿namespace MeshWeaver.Layout;
 
+/// <summary>
+/// Represents a layout control with customizable properties.
+/// </summary>
+/// <remarks>
+/// For more information, visit the 
+/// <a href="https://www.fluentui-blazor.net/layout">Fluent UI Blazor Layout documentation</a>.
+/// </remarks>
 public record LayoutControl() : ContainerControl<LayoutControl, LayoutSkin>(ModuleSetup.ModuleName, ModuleSetup.ApiVersion, new())
 {
-
 }
 
-
+/// <summary>
+/// Represents the skin for a layout control.
+/// </summary>
+/// <remarks>
+/// For more information, visit the 
+/// <a href="https://www.fluentui-blazor.net/layout">Fluent UI Blazor Layout documentation</a>.
+/// </remarks>
 public record LayoutSkin : Skin<LayoutSkin>;
+
+/// <summary>
+/// Represents the skin for a header control.
+/// </summary>
+/// <remarks>
+/// For more information, visit the 
+/// <a href="https://www.fluentui-blazor.net/header">Fluent UI Blazor Header documentation</a>.
+/// </remarks>
 public record HeaderSkin : Skin<HeaderSkin>;
 
+/// <summary>
+/// Represents the skin for a footer control.
+/// </summary>
+/// <remarks>
+/// For more information, visit the 
+/// <a href="https://www.fluentui-blazor.net/footer">Fluent UI Blazor Footer documentation</a>.
+/// </remarks>
 public record FooterSkin : Skin<FooterSkin>;
