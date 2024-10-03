45d48a82fb7f55c2d6a4d670952e547fbf150d61
Dmitry Kalabin <dkalabin@systemorph.com>
2024-10-03 10:24:22 +0200

    xml comments added
    (cherry picked from commit 7c86f17729f46cc0a8246e541fb807a1073597f9)

--- src/MeshWeaver.Layout/LayoutAreaProperties.cs
+++ src/MeshWeaver.Layout/LayoutAreaProperties.cs
@@ -3,22 +3,54 @@ using MeshWeaver.Layout.Composition;
 
 namespace MeshWeaver.Layout
 {
+    /// <summary>
+    /// Represents the properties of a layout area.
+    /// </summary>
     public record LayoutAreaProperties
     {
+        /// <summary>
+        /// The name of the properties.
+        /// </summary>
         public const string Properties = nameof(Properties);
+
+        /// <summary>
+        /// Gets or initializes the page title of the layout area.
+        /// </summary>
         public string PageTitle { get; init; }
+
+        /// <summary>
+        /// Gets or initializes the heading of the layout area.
+        /// </summary>
         public string Heading { get; init; }
+
+        /// <summary>
+        /// Gets or initializes the heading menu items of the layout area.
+        /// </summary>
         public ImmutableList<object> HeadingMenu { get; init; } = ImmutableList<object>.Empty;
 
+        /// <summary>
+        /// Gets or initializes the menu controls of the layout area.
+        /// </summary>
         internal ImmutableList<NavLinkControl> MenuControls { get; init; } = ImmutableList<NavLinkControl>.Empty;
 
+        /// <summary>
+        /// Gets or initializes the menu order of the layout area.
+        /// </summary>
         internal int MenuOrder { get; init; }
+
+        /// <summary>
+        /// Sets the menu order of the layout area.
+        /// </summary>
+        /// <param name="order">The menu order to set.</param>
+        /// <returns>A new <see cref="LayoutAreaProperties"/> instance with the specified menu order.</returns>
         public LayoutAreaProperties WithMenuOrder(int order) => this with { MenuOrder = order };
 
-
+        /// <summary>
+        /// Sets the menu controls of the layout area.
+        /// </summary>
+        /// <param name="items">The menu controls to set.</param>
+        /// <returns>A new <see cref="LayoutAreaProperties"/> instance with the specified menu controls.</returns>
         public LayoutAreaProperties WithMenu(params NavLinkControl[] items)
             => this with { MenuControls = MenuControls.AddRange(items) };
-
-
     }
 }
