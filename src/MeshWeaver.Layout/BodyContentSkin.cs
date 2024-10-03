667f2cf3a6dfe2537fd15abfc89edc6f3b9fa76d
Dmitry Kalabin <dkalabin@systemorph.com>
2024-10-03 10:21:30 +0200

    xml comments added
    (cherry picked from commit 9d8a99a37ed0ccb995b771a9f35e73c1503afe1f)

--- src/MeshWeaver.Layout/BodyContentSkin.cs
+++ src/MeshWeaver.Layout/BodyContentSkin.cs
@@ -1,3 +1,10 @@
 ﻿namespace MeshWeaver.Layout;
 
+/// <summary>
+/// Represents the skin for body content.
+/// </summary>
+/// <remarks>
+/// For more information, visit the 
+/// <a href="https://www.fluentui-blazor.net/bodycontent">Fluent UI Blazor BodyContent documentation</a>.
+/// </remarks>
 public record BodyContentSkin : Skin<BodyContentSkin>;
--- src/MeshWeaver.Layout/CardSkin.cs
+++ src/MeshWeaver.Layout/CardSkin.cs
@@ -1,17 +1,58 @@
 ﻿namespace MeshWeaver.Layout;
 
+/// <summary>
+/// Represents the skin for a card control with customizable properties.
+/// </summary>
+/// <remarks>
+/// For more information, visit the 
+/// <a href="https://www.fluentui-blazor.net/card">Fluent UI Blazor Card documentation</a>.
+/// </remarks>
 public record CardSkin : Skin<CardSkin>
 {
+    /// <summary>
+    /// Gets or initializes the area restriction state of the card.
+    /// </summary>
     public object AreaRestricted { get; init; }
+
+    /// <summary>
+    /// Gets or initializes the height of the card.
+    /// </summary>
     public object Height { get; init; }
+
+    /// <summary>
+    /// Gets or initializes the width of the card.
+    /// </summary>
     public object Width { get; init; }
+
+    /// <summary>
+    /// Gets or initializes the minimal style state of the card.
+    /// </summary>
     public object MinimalStyle { get; init; }
 
+    /// <summary>
+    /// Sets the area restriction state of the card.
+    /// </summary>
+    /// <param name="areaRestricted">The area restriction state to set.</param>
+    /// <returns>A new <see cref="CardSkin"/> instance with the specified area restriction state.</returns>
     public CardSkin WithAreaRestricted(object areaRestricted) => This with { AreaRestricted = areaRestricted };
 
+    /// <summary>
+    /// Sets the height of the card.
+    /// </summary>
+    /// <param name="height">The height to set.</param>
+    /// <returns>A new <see cref="CardSkin"/> instance with the specified height.</returns>
     public CardSkin WithHeight(object height) => This with { Height = height };
 
+    /// <summary>
+    /// Sets the width of the card.
+    /// </summary>
+    /// <param name="width">The width to set.</param>
+    /// <returns>A new <see cref="CardSkin"/> instance with the specified width.</returns>
     public CardSkin WithWidth(object width) => This with { Width = width };
 
+    /// <summary>
+    /// Sets the minimal style state of the card.
+    /// </summary>
+    /// <returns>A new <see cref="CardSkin"/> instance with the minimal style state set to true.</returns>
     public CardSkin WithMinimalStyle() => This with { MinimalStyle = true };
 }
