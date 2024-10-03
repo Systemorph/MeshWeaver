31dabe81eb0809c089c94f6d709ded213cc5cb4d
Dmitry Kalabin <dkalabin@systemorph.com>
2024-10-03 10:35:35 +0200

    xml comments added
    (cherry picked from commit 8ef0d73ecc451ec4f4d43acab9c907eae3df8013)

--- src/MeshWeaver.Layout/ExpandRequest.cs
+++ src/MeshWeaver.Layout/ExpandRequest.cs
@@ -2,7 +2,14 @@
 
 namespace MeshWeaver.Layout;
 
+/// <summary>
+/// Represents an event that occurs when an area is clicked.
+/// </summary>
+/// <param name="Area">The area that was clicked.</param>
 public record ClickedEvent(string Area) : WorkspaceMessage
 {
+    /// <summary>
+    /// Gets or initializes the payload associated with the clicked event.
+    /// </summary>
     public object Payload { get; init; }
-};
+}
--- src/MeshWeaver.Layout/IFormComponent.cs
+++ src/MeshWeaver.Layout/IFormComponent.cs
@@ -1,8 +1,13 @@
 ﻿namespace MeshWeaver.Layout
 {
+    /// <summary>
+    /// Represents a form component with customizable properties.
+    /// </summary>
     public interface IFormComponent : IUiControl
     {
+        /// <summary>
+        /// Gets or initializes the data associated with the form component.
+        /// </summary>
         object Data { get; init; }
-
     }
 }
