7e2ba51fc736a9e90561576be9bb6dba9cbcfb61
Roland Bürgi <rbuergi@systemorph.com>
2024-10-06 16:16:02 +0200

    Iterating on tool

--- src/MeshWeaver.Azure.Publish/Program.cs
+++ src/MeshWeaver.Azure.Publish/Program.cs
@@ -29,13 +29,16 @@ namespace MeshWeaver.Azure.Publish
                     "--container",
                     "The name of the blob container"),
                 new Option<string>(
+                    "--html-container",
+                    "The name of the blob container containing pre-rendered html"),
+                new Option<string>(
                     "--address",
                     "The address to use")
             };
 
             rootCommand.Description = "MeshWeaver Build Tasks";
 
-            rootCommand.Handler = CommandHandler.Create<string, string, string, string>(async (path, connectionString, container, address) =>
+            rootCommand.Handler = CommandHandler.Create<string, string, string, string, string>(async (path, connectionString, container, htmlContainer, address) =>
             {
                 if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(container) || string.IsNullOrEmpty(address))
                 {
@@ -44,29 +47,28 @@ namespace MeshWeaver.Azure.Publish
                 }
 
                 // Call your custom task logic here
-                logger.LogInformation($"Processing files from path: {path}");
-                logger.LogInformation($"Blob Storage Connection String: {connectionString}");
-                logger.LogInformation($"Blob Container Name: {container}");
-                logger.LogInformation($"Address: {address}");
-
-                await ExecuteAsync(connectionString, container, path, address, logger);
+                logger.LogInformation("Processing files from\npath: {path}\nstorage: {connectionString}\ncontainer: {container}\nhtmlContainer: {htmlContainer}\naddress: {address}", path, connectionString, container, htmlContainer, address);
+                await ExecuteAsync(connectionString, container, htmlContainer, path, address, logger);
             });
 
             await rootCommand.InvokeAsync(args);
         }
 
-        public static async Task<bool> ExecuteAsync(string connectionString, string container, string path, string address, ILogger logger)
+        public static async Task<bool> ExecuteAsync(string connectionString, string container, string htmlContainer, string path, string address, ILogger logger)
         {
             try
             {
                 var blobServiceClient = new BlobServiceClient(connectionString);
-                var blobContainerClient = blobServiceClient.GetBlobContainerClient(container);
+                var blobContainer = blobServiceClient.GetBlobContainerClient(container);
+                var htmlBlobContainer = htmlContainer == null ? null : blobServiceClient.GetBlobContainerClient(container);
 
                 // Create the container if it doesn't exist
-                await blobContainerClient.CreateIfNotExistsAsync();
+                await blobContainer.CreateIfNotExistsAsync();
+                if(htmlContainer != null)
+                    await htmlBlobContainer.CreateIfNotExistsAsync();
 
                 var localFiles = Directory.GetFiles(path).Select(f => Path.Combine(address, Path.GetRelativePath(path, f))).ToHashSet();
-                var blobs = blobContainerClient.GetBlobs().ToDictionary(b => b.Name);
+                var blobs = blobContainer.GetBlobs().ToDictionary(b => b.Name);
 
                 // Delete blobs that no longer exist in the local directory
                 foreach (var blob in blobs)
@@ -74,32 +76,43 @@ namespace MeshWeaver.Azure.Publish
                     if (!localFiles.Contains(blob.Key))
                     {
                         logger.LogInformation($"Deleting blob {blob.Key} as it no longer exists in the local directory.");
-                        await blobContainerClient.DeleteBlobAsync(blob.Key);
+                        await blobContainer.DeleteBlobAsync(blob.Key);
                     }
                 }
 
                 foreach (var inputFile in Directory.GetFiles(path))
                 {
-                    var filePath = Path.Combine(address, Path.GetRelativePath(path, inputFile));
-                    var fileContent = await File.ReadAllTextAsync(inputFile);
-                    var fileLastModified = File.GetLastWriteTimeUtc(inputFile);
-
-                    // Check if the blob exists and if the local file is newer
-                    if (blobs.TryGetValue(filePath, out var blobItem) && blobItem.Properties.LastModified >= fileLastModified)
+                    try
                     {
-                        logger.LogInformation($"Skipping upload for {filePath} as the blob is up-to-date.");
-                        continue;
+                        var filePath = Path.Combine(address, Path.GetRelativePath(path, inputFile)).Replace('\\', '/');
+                        var fileContent = await File.ReadAllTextAsync(inputFile);
+                        var fileLastModified = File.GetLastWriteTimeUtc(inputFile);
+
+                        // Check if the blob exists and if the local file is newer
+                        if (blobs.TryGetValue(filePath, out var blobItem) && blobItem.Properties.LastModified >= fileLastModified)
+                        {
+                            logger.LogInformation($"Skipping upload for {filePath} as the blob is up-to-date.");
+                            continue;
+                        }
+
+                        var extension = Path.GetExtension(filePath);
+                        logger.LogInformation($"Uploading file {inputFile} to {filePath}. Parsing as {extension}");
+
+                        var metadata = extension switch
+                        {
+                            ".md" => await ParseMarkdown(address, logger, filePath, fileContent, htmlBlobContainer),
+                            _ => DefaultMetadata(filePath, extension.Trim('.'))
+                        };
+
+                        logger.LogInformation("Uploading {filePath} with {metadata}", filePath, JsonSerializer.Serialize(metadata));
+                        var blob = blobContainer.GetBlobClient(filePath);
+                        await blob.UploadAsync(new BinaryData(fileContent), overwrite: true);
+                        await blob.SetMetadataAsync(metadata);
                     }
-
-                    logger.LogInformation($"Uploading file {inputFile} to {filePath}");
-
-                    if (Path.GetExtension(filePath) == "md")
-                        await ParseAndUploadHtml(address, logger, filePath, fileContent, blobContainerClient);
-
-                    // upload the file
-                    var markdownBlobClient = blobContainerClient.GetBlobClient(filePath);
-                    await markdownBlobClient.UploadAsync(new BinaryData(fileContent), overwrite: true);
-
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred during file upload.");
+                    }
                 }
 
                 return true;
@@ -111,21 +124,35 @@ namespace MeshWeaver.Azure.Publish
             }
         }
 
-        private static async Task ParseAndUploadHtml(string address, ILogger logger, string filePath, string fileContent,
-            BlobContainerClient blobContainerClient)
+        private static Dictionary<string, string> DefaultMetadata(string filePath, string type)
+        {
+            return new Dictionary<string, string>
+            {
+                { "type", type },
+                { "path", filePath }
+            };
+}
+
+        private static async Task<IDictionary<string, string>> ParseMarkdown(string address, ILogger logger, string filePath, string fileContent, BlobContainerClient htmlContainer)
         {
             // Parse metadata and HTML
             var (article, html) = MarkdownIndexer.ParseArticle(filePath, fileContent, address);
 
-            // Store metadata and HTML in blob storage
-            var htmlFilePath = Path.ChangeExtension(filePath, ".html");
-            var htmlBlobClient = blobContainerClient.GetBlobClient(htmlFilePath);
-            await htmlBlobClient.UploadAsync(new BinaryData(html), overwrite: true);
-
             // Set metadata on the HTML blob
-            var metadata = article.ToMetadata();
-            logger.LogInformation($"{JsonSerializer.Serialize(metadata)}");
-            await htmlBlobClient.SetMetadataAsync(metadata);
+            var metadata = article?.ToMetadata(filePath, "md") ?? DefaultMetadata(filePath, "md");
+
+            if (htmlContainer != null)
+            {
+                // Store metadata and HTML in blob storage
+                var htmlFilePath = Path.ChangeExtension(filePath, ".html");
+                logger.LogInformation("Uploading pre-rendered html {path}", htmlFilePath);
+                var htmlBlobClient = htmlContainer.GetBlobClient(htmlFilePath);
+                await htmlBlobClient.UploadAsync(new BinaryData(html), overwrite: true);
+                await htmlBlobClient.SetMetadataAsync(metadata);
+
+            }
+
+            return metadata;
         }
     }
 }
