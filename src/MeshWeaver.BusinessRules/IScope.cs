6130d408adbd382b845a9e66f55eace401a16048
Roland Bürgi <rbuergi@systemorph.com>
2024-11-14 11:47:04 +0100

    Initial draft

---
+++ src/MeshWeaver.BusinessRules/IScope.cs
@@ -0,0 +1,12 @@
+﻿namespace MeshWeaver.BusinessRules;
+
+public interface IScope
+{
+    TScope GetScope<TScope>(object identity) where TScope : IScope;
+
+}
+public interface IScope<out TIdentity, out TState> : IScope
+{
+    TIdentity Identity { get; }
+    TState GetStorage();
+}
---
+++ src/MeshWeaver.BusinessRules/IScopeRegistryFactory.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using MeshWeaver.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeshWeaver.BusinessRules;
+
+public static class BusinessRulesRegistryExtensions
+{
+    public static MessageHubConfiguration AddBusinessRules(this MessageHubConfiguration configuration, params Assembly[] assemblies)
+    => configuration.WithServices(services =>
+            services
+                .AddScoped<IScopeRegistryFactory, ScopeRegistryFactory>()
+                .AddScoped<IScopeFactory>(sp => new ScopeFactory(assemblies))
+    );
+}
+
+public interface IScopeRegistryFactory
+{
+    ScopeRegistry<TState> Create<TState>(TState state);
+}
+
+public interface IScopeFactory
+{
+    object Create<TScope, TState>(object identity, TState state);
+}
+
+public class ScopeFactory(Assembly[] assemblies) : IScopeFactory
+{
+    private readonly IReadOnlyDictionary<Type, Type> scopeImplementations = assemblies
+        .SelectMany(a => a.GetTypes()
+            .Where(t => !t.IsAbstract && t.IsGenericType && t.GetGenericTypeDefinition() == typeof(ScopeBase<,,>))
+        )
+        .ToDictionary(t => t.GetGenericArguments().First());
+    public object Create<TScope, TState>(object identity, TState state)
+    {
+        if (!scopeImplementations.TryGetValue(typeof(TScope), out var type))
+            throw new NotSupportedException($"No implementation found for type {typeof(TScope).Name}. Is the compiler installed?");
+        return Activator.CreateInstance(type, identity, state);
+    }
+}
+
+public class ScopeRegistryFactory(IScopeFactory scopeFactory) : IScopeRegistryFactory
+{
+    public ScopeRegistry<TState> Create<TState>(TState state)
+    {
+        return new(state, scopeFactory);
+    }
+}
+
+public partial class ScopeRegistry<TState>(TState state, IScopeFactory scopeFactory)
+{
+    private ImmutableDictionary<(Type ScopeType, object Identity), object> scopes = ImmutableDictionary<(Type ScopeType, object Identity), object>.Empty;
+    private readonly object locker = new();
+    public TState State => state;
+    public TScope GetScope<TScope>(object identity) where TScope : IScope
+    {
+        var key = (typeof(TScope), identity);
+        lock (locker)
+        {
+            if (scopes.TryGetValue(key, out var scope))
+            {
+                return (TScope)scope;
+            }
+            var newScope = scopeFactory.Create<TScope, TState>(identity, state);
+            scopes = scopes.Add(key, newScope);
+            return (TScope)newScope;
+
+        }
+    }
+}
