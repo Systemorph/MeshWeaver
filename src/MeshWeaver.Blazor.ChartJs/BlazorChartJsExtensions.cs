87b64ea33ded5bd282c1f82d608edb4abbb55da1
Roland Bürgi <rbuergi@systemorph.com>
2024-11-26 16:12:51 +0100

    Fixing Dashboard test

--- src/MeshWeaver.Blazor.ChartJs/BlazorChartJsExtensions.cs
+++ src/MeshWeaver.Blazor.ChartJs/BlazorChartJsExtensions.cs
@@ -1,10 +1,15 @@
 ﻿using MeshWeaver.Charting;
+using MeshWeaver.Domain;
 using MeshWeaver.Layout.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MeshWeaver.Blazor.ChartJs;
 
 public static class BlazorChartJsExtensions
 {
     public static LayoutClientConfiguration AddChartJs(this LayoutClientConfiguration config)
-        => config.WithView<ChartControl, ChartView>();
+    {
+        config.Hub.ServiceProvider.GetRequiredService<ITypeRegistry>().GetOrAddType(typeof(ChartControl));
+        return config.WithView<ChartControl, ChartView>();
+    }
 }
