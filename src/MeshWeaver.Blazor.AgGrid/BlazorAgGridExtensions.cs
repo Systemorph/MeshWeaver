5cb5270919b482380102c472db0633dabc01572d
Roland Bürgi <rbuergi@systemorph.com>
2024-11-26 16:12:51 +0100

    Fixing Dashboard test

--- src/MeshWeaver.Blazor.AgGrid/BlazorAgGridExtensions.cs
+++ src/MeshWeaver.Blazor.AgGrid/BlazorAgGridExtensions.cs
@@ -1,10 +1,15 @@
-﻿using MeshWeaver.GridModel;
+﻿using MeshWeaver.Domain;
+using MeshWeaver.GridModel;
 using MeshWeaver.Layout.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MeshWeaver.Blazor.AgGrid;
 
 public static class BlazorAgGridExtensions
 {
     public static LayoutClientConfiguration AddAgGrid(this LayoutClientConfiguration config)
-        => config.WithView<GridControl, AgGrid>();
+    {
+        config.Hub.ServiceProvider.GetRequiredService<ITypeRegistry>().GetOrAddType(typeof(GridControl));
+        return config.WithView<GridControl, AgGrid>();
+    }
 }
