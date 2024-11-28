5b271eb5ffb2ba3b9c7266d8ca5b068d2caa67e8
Roland Bürgi <rbuergi@systemorph.com>
2024-11-28 17:48:42 +0100

    Refactoring MeshTest

--- test/MeshWeaver.Notebooks.Test/NotebookTest.cs
+++ test/MeshWeaver.Notebooks.Test/NotebookTest.cs
@@ -1,10 +1,29 @@
-﻿using MeshWeaver.Fixture;
+﻿using MeshWeaver.Application;
+using MeshWeaver.Fixture;
+using MeshWeaver.Hosting.Monolith;
+using MeshWeaver.Mesh.Contract;
+using MeshWeaver.Notebooks.Hub;
 using Xunit.Abstractions;
 
 namespace MeshWeaver.Notebooks.Test;
 
-public class NotebookTest(ITestOutputHelper output) : HubTestBase(output)
+public class NotebookTest(ITestOutputHelper output) : MeshTestBase(output)
 {
+    protected override MeshBuilder CreateBuilder()
+        => base
+            .CreateBuilder()
+            .AddMonolithMesh()
+            .ConfigureMesh(mesh =>
+                mesh
+                    .AddNotebooks()
+                    .WithMeshNodeFactory((addressType, id) =>
+                    addressType == typeof(ApplicationAddress).FullName && id == TestApplicationAttribute.Test
+                        ? MeshExtensions.GetMeshNode(addressType, id, GetType().Assembly.Location)
+                        : null));
 
 
+    public async Task HelloWorld()
+    {
+        var address = new NotebookAddress();
+    }
 }
