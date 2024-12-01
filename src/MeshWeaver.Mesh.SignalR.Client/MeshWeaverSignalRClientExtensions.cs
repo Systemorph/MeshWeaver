0fd33e8e90b28d7b9ddbf6ea0a4ede59c3929d6a
Roland Bürgi <rbuergi@systemorph.com>
2024-12-01 12:53:11 +0100

    Iterating on SignalRTest

--- src/MeshWeaver.Mesh.SignalR.Client/MeshWeaverSignalRClientExtensions.cs
+++ src/MeshWeaver.Mesh.SignalR.Client/MeshWeaverSignalRClientExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using MeshWeaver.Domain;
 using MeshWeaver.Mesh.Contract;
 using MeshWeaver.Messaging;
+using Microsoft.AspNetCore.Http.Connections.Client;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -10,45 +11,56 @@ namespace MeshWeaver.Mesh.SignalR.Client;
 public static class MeshWeaverSignalRClientExtensions
 {
     public static MessageHubConfiguration AddSignalRClient(
-        this MessageHubConfiguration config, string connectionGateway, 
-        HttpMessageHandler httpMessageHandler)
+        this MessageHubConfiguration config, string connectionGateway,
+        Action<HttpConnectionOptions> options = null)
     {
-
-        var hubConnection = new HubConnectionBuilder()
-            .WithUrl(connectionGateway, options =>
-            {
-                options.HttpMessageHandlerFactory = _ => httpMessageHandler;
-            })
-            .WithAutomaticReconnect()
-            .Build();
-        hubConnection.Closed += OnHubConnectionClosed;
-        config.WithDisposeAction(_ => hubConnection.DisposeAsync());
         return config
-            .WithInitialization(async (hub,ct) =>
-            {
-                var logger = hub.ServiceProvider.GetRequiredService<ILoggerFactory>()
-                    .CreateLogger(typeof(MeshWeaverSignalRClientExtensions));
-                var typeRegistry = hub.ServiceProvider.GetRequiredService<ITypeRegistry>();
-                var address = hub.Address;
-                var addressType = typeRegistry.GetCollectionName(address.GetType());
-                var id = address.ToString();
-
-                try
+            .WithServices(services => services.AddScoped(_ => 
+                new HubConnectionBuilder()
+                .WithUrl(connectionGateway, options ?? (_ => { }))
+                .WithAutomaticReconnect()
+                .Build()))
+            .WithInitialization(async (hub, ct) =>
                 {
-                    logger.LogInformation("Creating SignalR connection for {AddressType} {Id}", addressType, id);
-                    await hubConnection.StartAsync(ct);
+                    var logger = hub.ServiceProvider.GetRequiredService<ILoggerFactory>()
+                        .CreateLogger(typeof(MeshWeaverSignalRClientExtensions));
+                    var typeRegistry = hub.ServiceProvider.GetRequiredService<ITypeRegistry>();
+                    var address = hub.Address;
+                    var addressType = typeRegistry.GetCollectionName(address.GetType());
+                    var id = address.ToString();
 
-                    var connection = await hubConnection.InvokeCoreAsync<MeshConnection>("Connect", [addressType, address], ct);
-                    if (connection.Status != ConnectionStatus.Connected)
-                        throw new SignalRException("Couldn't connect.");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError("Unable connecting SignalR connection for {AddressType} {Id}:\n{Exception}", addressType, id, ex);
-                    throw;
-                }
-            })
-            .WithRoutes(routes => AddSignalRRoute(routes, hubConnection));
+                    try
+                    {
+                        var hubConnection = hub.ServiceProvider.GetRequiredService<HubConnection>();
+                        hubConnection.Closed += OnHubConnectionClosed;
+                        hub.WithDisposeAction(_ => hubConnection.DisposeAsync());
+                        logger.LogInformation("Creating SignalR connection for {AddressType} {Id}", addressType, id);
+                        await hubConnection.StartAsync(ct);
+
+
+                        var meshConnection = await hubConnection.InvokeAsync<MeshConnection>("Connect", "MyAddress", "MyId");
+
+
+                        var connection =
+                            await hubConnection.InvokeAsync<MeshConnection>("Connect", addressType, id);
+                        if (connection.Status != ConnectionStatus.Connected)
+                            throw new SignalRException("Couldn't connect.");
+                        hubConnection.On<IMessageDelivery>("ReceiveMessage", message =>
+                        {
+                            // Handle the received message
+                            logger.LogDebug($"Received message for address {address}: {message}");
+                            hub.DeliverMessage(message);
+                        });
+
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError("Unable connecting SignalR connection for {AddressType} {Id}:\n{Exception}",
+                            addressType, id, ex);
+                        throw;
+                    }
+                })
+                .WithRoutes(AddSignalRRoute);
     }
 
     private static Task OnHubConnectionClosed(Exception arg)
@@ -56,12 +68,13 @@ public static class MeshWeaverSignalRClientExtensions
         return Task.CompletedTask;
     }
 
-    private static RouteConfiguration AddSignalRRoute(RouteConfiguration routes, HubConnection hubConnection)
+    private static RouteConfiguration AddSignalRRoute(RouteConfiguration routes)
     {
         var logger = routes.Hub.ServiceProvider.GetRequiredService<ILoggerFactory>()
             .CreateLogger(typeof(MeshWeaverSignalRClientExtensions));
 
         var address = routes.Hub.Address;
+        var hubConnection = routes.Hub.ServiceProvider.GetRequiredService<HubConnection>();
         hubConnection.On<IMessageDelivery>("ReceiveMessage", message =>
         {
             // Handle the received message
