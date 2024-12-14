0fe830b79eaed4e50791107f410f7340f5f8bfe4
Roland Bürgi <rbuergi@systemorph.com>
2024-12-14 15:21:28 +0100

    Simplifying KernelTests

--- test/MeshWeaver.Mesh.Monolith.Test/MonolithKernelTest.cs
+++ test/MeshWeaver.Mesh.Monolith.Test/MonolithKernelTest.cs
@@ -6,7 +6,6 @@ using MeshWeaver.Kernel.Hub;
 using MeshWeaver.Layout;
 using MeshWeaver.Mesh;
 using MeshWeaver.Messaging;
-using MeshWeaver.Northwind.ViewModel;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Events;
 using Xunit;
@@ -14,15 +13,32 @@ using Xunit.Abstractions;
 
 namespace MeshWeaver.Hosting.Monolith.Test;
 
+public static class TestHubExtensions
+{
+    public static IMessageHub CreateTestHub(IMessageHub mesh)
+    => mesh.ServiceProvider.CreateMessageHub(new ApplicationAddress(nameof(Test)), config => config
+        .AddLayout(layout =>
+            layout.WithView(ctx =>
+                    ctx.Area == "Dashboard",
+                (_, ctx) => new LayoutGridControl()
+            )
+        )
+    );
+
+}
+
 public class MonolithKernelTest(ITestOutputHelper output) : MonolithMeshTestBase(output)
 {
+    private const string Test = nameof(Test);
     protected override MeshBuilder ConfigureMesh(MeshBuilder builder)
     {
         return base.ConfigureMesh(builder)
             .AddKernel()
-            .ConfigureMesh(config => config.AddMeshNodes(NorthwindApplicationAttribute.Northwind with
+            .ConfigureMesh(config => config.AddMeshNodes(
+                new MeshNode(ApplicationAddress.TypeName, Test, "Test", GetType().Assembly.FullName)
                 {
-                    StartupScript = NorthwindApplicationAttribute.Northwind.StartupScript.Replace("nuget:", "")
+                    StartupScript = @$"#r ""{typeof(TestHubExtensions).Namespace}""
+{typeof(TestHubExtensions).FullName}.{nameof(TestHubExtensions.CreateTestHub)}(Mesh);"
                 }
             ));
     }
@@ -35,7 +51,7 @@ public class MonolithKernelTest(ITestOutputHelper output) : MonolithMeshTestBase
         client.Post(
             new KernelCommandEnvelope(Microsoft.DotNet.Interactive.Connection.KernelCommandEnvelope.Serialize(Microsoft.DotNet.Interactive.Connection.KernelCommandEnvelope.Create(command))),
             o => o.WithTarget(new KernelAddress()));
-        var kernelEvent = await kernelEvents
+        var kernelEvent = await kernelEventsStream
             .Select(e => Microsoft.DotNet.Interactive.Connection.KernelEventEnvelope.Deserialize(e.Event).Event)
             .TakeUntil(e => e is CommandSucceeded || e is CommandFailed)
             .ToArray()
@@ -47,20 +63,52 @@ public class MonolithKernelTest(ITestOutputHelper output) : MonolithMeshTestBase
     }
 
     [Fact]
-    public async Task RoutingNorthwind()
+    public async Task RoutingToHub()
     {
         var client = CreateClient();
-        var area = await client.GetLayoutAreaAsync(new ApplicationAddress("Northwind"), "Dashboard");
+        var area = await client.GetLayoutAreaAsync(new ApplicationAddress(Test), "Dashboard");
         area.Should().NotBeNull();
         area.Should().BeOfType<LayoutGridControl>();
     }
+    [Fact]
+    public async Task HubViaKernel()
+    {
+        var client = CreateClient();
+        var command = 
+@"
+using MeshWeaver.Layout;
+using MeshWeaver.Mesh;
+await Mesh.GetLayoutAreaAsync(new ApplicationAddress(""Test""), ""Dashboard"")";
+        client.Post(
+            new SubmitCodeRequest(command),
+            o => o.WithTarget(new KernelAddress()));
+        var kernelEvents = await kernelEventsStream
+            .Select(e => Microsoft.DotNet.Interactive.Connection.KernelEventEnvelope.Deserialize(e.Event).Event)
+            .TakeUntil(e => e is CommandSucceeded || e is CommandFailed)
+            //.TakeUntil(e => e is StandardOutputValueProduced)
+            .ToArray()
+            .FirstAsync();
 
-    private readonly ReplaySubject<KernelEventEnvelope> kernelEvents = new();
+        kernelEvents.OfType<CommandSucceeded>().Should().NotBeEmpty();
+
+        await Task.Delay(1000);
+        kernelEventsStream.OnCompleted();
+        var kernelEvents2 = await kernelEventsStream
+            .Select(e => Microsoft.DotNet.Interactive.Connection.KernelEventEnvelope.Deserialize(e.Event).Event)
+            .TakeUntil(e => e is CommandSucceeded || e is CommandFailed)
+            .ToArray()
+            .FirstAsync();
+        var standardOutput = kernelEvents.OfType<ReturnValueProduced>().Single();
+        var value = standardOutput.FormattedValues.Single();
+        value.Value.Should().Contain("iframe");
+    }
+
+    private readonly ReplaySubject<KernelEventEnvelope> kernelEventsStream = new();
     protected override MessageHubConfiguration ConfigureClient(MessageHubConfiguration configuration)
     {
         return base.ConfigureClient(configuration).AddLayoutClient().WithHandler<KernelEventEnvelope>((_, e) =>
         {
-            kernelEvents.OnNext(e.Message);
+            kernelEventsStream.OnNext(e.Message);
             return e.Processed();
         });
     }
