85b5ae2a7dd31005cbf3c78b80ef7237f8db98e0
Roland Bürgi <rbuergi@systemorph.com>
2024-12-22 23:14:44 +0100

    Fixing auto-labels

--- src/MeshWeaver.Charting/Models/ChartData.cs
+++ src/MeshWeaver.Charting/Models/ChartData.cs
@@ -1,44 +1,46 @@
 ﻿using System.Collections.Immutable;
 using System.Text.Json.Serialization;
 
-namespace MeshWeaver.Charting.Models
+namespace MeshWeaver.Charting.Models;
+
+public record ChartData
 {
-    public record ChartData
+    // ReSharper disable once StringLiteralTypo
+    [JsonPropertyName("datasets")] public ImmutableList<DataSet> DataSets { get; init; } = [];
+
+    public IReadOnlyCollection<string> Labels { get; internal set; }
+
+    public IEnumerable<string> XLabels { get; init; }
+
+    public IEnumerable<string> YLabels { get; init; }
+
+    public ChartData WithLabels(IReadOnlyCollection<string> labels) => this with { Labels = labels };
+
+    public ChartData WithLabels(params string[] labels) => this with { Labels = labels };
+
+    public ChartData WithDataSets(IEnumerable<DataSet> dataSets)
     {
-        // ReSharper disable once StringLiteralTypo
-        [JsonPropertyName("datasets")] public ImmutableList<DataSet> DataSets { get; init; } = [];
-
-        public IReadOnlyCollection<string> Labels { get; internal set; }
-
-        public IEnumerable<string> XLabels { get; init; }
-
-        public IEnumerable<string> YLabels { get; init; }
-
-        public ChartData WithLabels(IReadOnlyCollection<string> labels) => this with { Labels = labels };
-
-        public ChartData WithLabels(params string[] labels) => this with { Labels = labels };
-
-        public ChartData WithDataSets(IEnumerable<DataSet> dataSets)
-        {
-            return this with { DataSets = DataSets.AddRange(dataSets), Labels = GetUpdatedLabels() };
-        }
-
-
-        public ChartData WithDataSets(params DataSet[] dataSets) => WithDataSets(dataSets.AsEnumerable());
-
-
-
-        protected IReadOnlyCollection<string> GetUpdatedLabels()
-        {
-            if (DataSets.Count > 1)
-            {
-                var maxLen = DataSets.Select(ds => ds.Data?.Count() ?? 0).DefaultIfEmpty(1).Max();
-
-                return Enumerable.Range(1, maxLen).Select(i => i.ToString()).ToArray();
-            }
-            return Labels;
-        }
-
+        return (this with { DataSets = DataSets.AddRange(dataSets) }).WithAutoUpdatedLabels();
     }
+
+
+    public ChartData WithDataSets(params DataSet[] dataSets) => WithDataSets(dataSets.AsEnumerable());
+
+
+
+
+    private bool AutoLabels => DataSets.Count > 1;
+    protected IReadOnlyCollection<string> GetUpdatedLabels()
+    {
+        if (AutoLabels && DataSets.Count > 0)
+        {
+            var maxLen = DataSets.Select(ds => ds.Data?.Count() ?? 0).DefaultIfEmpty(1).Max();
+
+            return Enumerable.Range(1, maxLen).Select(i => i.ToString()).ToArray();
+        }
+        return Labels;
+    }
+
+    internal ChartData WithAutoUpdatedLabels() => this with { Labels = GetUpdatedLabels(), };
 
 }
