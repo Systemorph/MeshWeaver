f03733616b1ee3d4db47a6e265cc746cd5d392a5
Roland Bürgi <rbuergi@systemorph.com>
2024-12-30 11:58:07 +0100

    Moving portal projects

---
+++ portal/MeshWeaver.Portal/Components/SiteSettings.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace MeshWeaver.Portal.Components;
+
+public partial class SiteSettings
+{
+    private IDialogReference dialog;
+
+    private async Task OpenSiteSettingsAsync()
+    {
+        dialog = await DialogService.ShowPanelAsync<SiteSettingsPanel>(new DialogParameters()
+        {
+            ShowTitle = true,
+            Title = "Site settings",
+            Alignment = HorizontalAlignment.Right,
+            PrimaryAction = "OK",
+            SecondaryAction = null,
+            ShowDismiss = true
+        });
+
+        var result = await dialog.Result;
+    }
+}
