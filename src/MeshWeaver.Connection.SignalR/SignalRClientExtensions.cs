074cb681ffca9c08bffdd296d9492bd3171e7b22
Roland Bürgi <rbuergi@systemorph.com>
2025-01-02 21:39:07 +0100

    Simplifying setup

--- src/MeshWeaver.Connection.SignalR/SignalRClientExtensions.cs
+++ src/MeshWeaver.Connection.SignalR/SignalRClientExtensions.cs
@@ -1,52 +1,106 @@
 ﻿using MeshWeaver.Mesh;
 using MeshWeaver.Messaging;
+using Microsoft.AspNetCore.Http.Connections.Client;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.SignalR.Protocol;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace MeshWeaver.Connection.SignalR;
 
 public static class SignalRClientExtensions
 {
+    public static Action<HttpConnectionOptions> HttpConnectionOptions { get; set; } = _ => { };
 
     public static MessageHubConfiguration UseSignalRClient(
         this MessageHubConfiguration config, 
         string url,
-        Func<SignalRMeshConnectionBuilder, SignalRMeshConnectionBuilder> connectionConfiguration)
+        Func<IHubConnectionBuilder,IHubConnectionBuilder> configuration = null)
     {
         return config
-            .WithServices(services => services.AddScoped(sp => connectionConfiguration.Invoke(new(sp, config.Address, url)).Build()))
+            .WithServices(services =>
+            {
+                return services.AddScoped(sp =>
+                {
+                    var builder = new HubConnectionBuilder()
+                        .WithUrl(url,  HttpConnectionOptions)
+                        .WithAutomaticReconnect();
+                    
+                    if(configuration is not null)
+                        builder = configuration(builder);
+
+                    builder.Services.AddSingleton<IHubProtocol>(_ =>
+                            new JsonHubProtocol(new OptionsWrapper<JsonHubProtocolOptions>(new()
+                            {
+                                PayloadSerializerOptions =
+                                    sp.GetRequiredService<IMessageHub>().JsonSerializerOptions
+                            })));
+                        return builder.Build();
+                    });
+
+            })
             .WithInitialization(async (hub, ct) =>
                 {
                     var logger = hub.ServiceProvider.GetRequiredService<ILoggerFactory>()
                         .CreateLogger(typeof(SignalRClientExtensions));
+                    var address = hub.Address;
+                    var (addressType, id) = MessageHubExtensions.GetAddressTypeAndId(address);
 
                     try
                     {
-
-                        logger.LogInformation("Creating SignalR connection for {Address}", hub.Address);
                         var hubConnection = hub.ServiceProvider.GetRequiredService<HubConnection>();
-                        await hubConnection.StartAsync(ct);
                         hub.RegisterForDisposal(async _ =>
                         {
                             await hubConnection.StopAsync(ct);
                             await hubConnection.DisposeAsync();
                         });
 
+                        hubConnection.Reconnecting += async (exception) =>
+                        {
+                            if (exception is not null)
+                                logger.LogWarning("Disconnected from SignalR connection for {Address}:\n{Exception}", hub.Address, exception);
 
+                            try
+                            {
+                                var connected =
+                                    await hubConnection.InvokeAsync<MeshConnection>(
+                                        "Connect",
+                                        hub.Address.ToString());
+
+                                if (connected.Status != ConnectionStatus.Connected)
+                                    throw new MeshException("Couldn't connect.");
+
+                            }
+                            catch (Exception ex)
+                            {
+                                logger.LogError("Unable connecting SignalR connection for {Address} :\n{Exception}", hub.Address, ex);
+                                throw;
+                            }
+                            // Your callback logic here
+                            Console.WriteLine("Reconnecting...");
+                        };
+
+                        logger.LogInformation("Creating SignalR connection for {AddressType} {Id}", addressType, id);
+                        await hubConnection.StartAsync(ct);
+
+                        var connection =
+                            await hubConnection.InvokeAsync<MeshConnection>("Connect", addressType, id, ct);
+                        if (connection.Status != ConnectionStatus.Connected)
+                            throw new MeshException("Couldn't connect.");
                         hubConnection.On<IMessageDelivery>("ReceiveMessage", message =>
                         {
                             // Handle the received message
-                            logger.LogDebug("Received message for address {address}: {message}", hub.Address, 
-                                message);
+                            logger.LogDebug($"Received message for address {address}: {message}");
                             hub.DeliverMessage(message);
                         });
 
                     }
                     catch (Exception ex)
                     {
-                        logger.LogError("Unable connecting SignalR connection for {Address} :\n{Exception}",
-                            hub.Address, ex);
+                        logger.LogError("Unable connecting SignalR connection for {AddressType} {Id}:\n{Exception}",
+                            addressType, id, ex);
                         throw;
                     }
                 })
