ca5cb662e75577f50814d1fc3e470f89b13b0179
Roland Bürgi <rbuergi@systemorph.com>
2025-01-07 09:12:26 +0100

    Iterating on Chart samples

--- src/MeshWeaver.Charting/Models/IDatasetWithTension.cs
+++ src/MeshWeaver.Charting/Models/IDatasetWithTension.cs
@@ -10,5 +10,5 @@ public interface IDataSetWithTension<T>
     /// <summary>
     /// Bezier curve tension of the line. Set to 0 to draw straight lines. This option is ignored if monotone cubic interpolation is used. Note This was renamed from 'tension' but the old name still works.
     /// </summary>
-    public T Smoothed(double? tension);
+    public T WithTension(double? tension);
 }
