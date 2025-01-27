338faa96ae0ad924f140f0e30c3edf1bbad1dfda
Roland Bürgi <rbuergi@systemorph.com>
2025-01-27 10:48:42 +0100

    Deleting samples folder

--- samples/MeshWeaver.Catalog.Layout/CatalogItemControl.cs
+++
@@ -1,8 +0,0 @@
-﻿using MeshWeaver.Layout;
-
-namespace MeshWeaver.Catalog.Layout;
-
-public record CatalogItemControl(object Data) : 
-    UiControl<CatalogItemControl>(ModuleSetup.ModuleName, ModuleSetup.ApiVersion)
-{
-}
