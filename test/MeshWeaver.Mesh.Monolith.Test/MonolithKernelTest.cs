ebe80bbf35edff0f12d6a64feca51d706471b954
Roland Bürgi <rbuergi@systemorph.com>
2025-02-14 23:14:34 +0100

    calculator test

--- test/MeshWeaver.Mesh.Monolith.Test/MonolithKernelTest.cs
+++ test/MeshWeaver.Mesh.Monolith.Test/MonolithKernelTest.cs
@@ -1,12 +1,15 @@
 ﻿using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Text.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Extensions;
+using MeshWeaver.Data;
 using MeshWeaver.Kernel;
 using MeshWeaver.Kernel.Hub;
 using MeshWeaver.Layout;
+using MeshWeaver.Layout.Client;
 using MeshWeaver.Mesh;
 using MeshWeaver.Messaging;
 using Microsoft.DotNet.Interactive.Commands;
@@ -88,6 +91,45 @@ public class MonolithKernelTest(ITestOutputHelper output) : MonolithMeshTestBase
         var standardOutput = kernelEvents.OfType<ReturnValueProduced>().Single();
         var value = standardOutput.FormattedValues.Single();
         value.Value.Should().Contain("iframe").And.Subject.Should().Contain(url);
+    }
+
+    [Fact]
+    public async Task CalculatorTest()
+    {
+        const string Code = @"using MeshWeaver.Layout;
+using MeshWeaver.Mesh;
+using static MeshWeaver.Layout.Controls;
+record Calculator(double Summand1, double Summand2);
+static object CalculatorSum(Calculator c) => Markdown($""**Sum**: {c.Summand1 + c.Summand2}"");
+Mesh.Edit(new Calculator(1,2), CalculatorSum)
+";
+        var kernel = new KernelAddress();
+        const string Area = nameof(Area);
+        var client = GetClient();
+        client.Post(
+            new SubmitCodeRequest(Code) { Id = Area},
+            o => o.WithTarget(kernel));
+
+        var stream = client.GetWorkspace().GetRemoteStream< JsonElement, LayoutAreaReference>(kernel, new LayoutAreaReference(Area));
+        var control = await stream.GetControlStream(Area)
+            .Timeout(10.Seconds())
+            .FirstAsync(x => x is not null);
+
+        var stack = control.Should().BeOfType<StackControl>().Which;
+        control = await stream.GetControlStream(stack.Areas.First().Area.ToString())
+            .Timeout(10.Seconds())
+            .FirstAsync(x => x is not null);
+        var editor = control.Should().BeOfType<EditorControl>().Which;
+        editor.DataContext.Should().NotBeNull();
+        var data = await stream.GetDataStream<object>(new(editor.DataContext))
+            .Timeout(10.Seconds())
+            .FirstAsync(x => x is not null);
+        stream.UpdatePointer(3, editor.DataContext, new("summand1"));
+        var md = await stream.GetControlStream(stack.Areas.Last().Area.ToString())
+            .Timeout(3.Seconds())
+            .FirstAsync(x => !(x as MarkdownControl)?.Markdown?.ToString()?.Contains("3") == true);
+
+        md.Should().BeOfType<MarkdownControl>().Which.Markdown.ToString().Should().Contain("5");
     }
 
     private readonly ReplaySubject<KernelEventEnvelope> kernelEventsStream = new();
