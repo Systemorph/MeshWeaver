629d873b6f68793a1c4185ee68d6e6eb22282456
Roland Bürgi <rbuergi@systemorph.com>
2025-02-24 15:23:03 +0100

    adding collapsability to NavMenuSkin

--- src/MeshWeaver.Layout/NavMenuControl.cs
+++ src/MeshWeaver.Layout/NavMenuControl.cs
@@ -28,6 +28,19 @@ public record NavMenuSkin : Skin<NavMenuSkin>
     /// <param name="width">The width to set.</param>
     /// <returns>A new <see cref="NavMenuSkin"/> instance with the specified width.</returns>
     public NavMenuSkin WithWidth(int width) => this with { Width = width };
+
+    /// <summary>
+    /// Sets whether the menu is expanded.
+    /// </summary>
+    public object Expanded { get; init; } = true;
+
+    /// <summary>
+    /// Collapse the menu
+    /// </summary>
+    /// <param name="expanded"></param>
+    /// <returns></returns>
+    public NavMenuSkin Collapse(bool expanded = false) =>
+        this with { Expanded = expanded };
 }
 
 /// <summary>
