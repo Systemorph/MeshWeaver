b3c52060f28191012591cd6275f04d60c7ce3aeb
Roland Bürgi <rbuergi@systemorph.com>
2025-02-28 09:34:34 +0100

    Refactoring menus

---
+++ portal/MeshWeaver.Portal.Shared.Web/Layout/MainLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿using MeshWeaver.Portal.Shared.Web.Resize;
+using Microsoft.AspNetCore.Components;
+
+namespace MeshWeaver.Portal.Shared.Web.Layout;
+
+public partial class MainLayout
+{
+    private bool isNavMenuOpen;
+    protected override void OnParametersSet()
+    {
+        if (ViewportInformation.IsDesktop && isNavMenuOpen)
+        {
+            isNavMenuOpen = false;
+            CloseMobileNavMenu();
+        }
+    }
+    [CascadingParameter]
+    public required ViewportInformation ViewportInformation { get; set; }
+    private void CloseMobileNavMenu()
+    {
+        isNavMenuOpen = false;
+        StateHasChanged();
+    }
+
+}
