fa2d64b07133d5c2baa01445b958066c6956793c
Roland Bürgi <rbuergi@systemorph.com>
2025-03-11 17:08:22 +0100

    introducing cohosted setup

---
+++ portal/aspire-cohosted/MeshWeaver.Portal.Cohosted.AppHost/OpenTelemetryCollector/OpenTelemetryCollectorResourceBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace MeshWeaver.Portal.AppHost.OpenTelemetryCollector;
+
+public static class OpenTelemetryCollectorResourceBuilderExtensions
+{
+    private const string DashboardOtlpUrlVariableName = "DOTNET_DASHBOARD_OTLP_ENDPOINT_URL";
+    private const string DashboardOtlpApiKeyVariableName = "AppHost:OtlpApiKey";
+    private const string DashboardOtlpUrlDefaultValue = "http://localhost:18889";
+
+    public static IResourceBuilder<OpenTelemetryCollectorResource> AddOpenTelemetryCollector(this IDistributedApplicationBuilder builder, string name, string configFileLocation)
+    {
+        builder.AddOpenTelemetryCollectorInfrastructure();
+
+        var url = builder.Configuration[DashboardOtlpUrlVariableName] ?? DashboardOtlpUrlDefaultValue;
+        var isHttpsEnabled = url.StartsWith("https", StringComparison.OrdinalIgnoreCase);
+
+        var dashboardOtlpEndpoint = new HostUrl(url);
+
+        var resource = new OpenTelemetryCollectorResource(name);
+        var resourceBuilder = builder.AddResource(resource)
+            .WithImage("ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-contrib", "latest")
+            .WithEndpoint(targetPort: 4317, name: OpenTelemetryCollectorResource.OtlpGrpcEndpointName, scheme: isHttpsEnabled ? "https" : "http")
+            .WithEndpoint(targetPort: 4318, name: OpenTelemetryCollectorResource.OtlpHttpEndpointName, scheme: isHttpsEnabled ? "https" : "http")
+            .WithBindMount(configFileLocation, "/etc/otelcol-contrib/config.yaml")
+            .WithEnvironment("ASPIRE_ENDPOINT", $"{dashboardOtlpEndpoint}")
+            .WithEnvironment("ASPIRE_API_KEY", builder.Configuration[DashboardOtlpApiKeyVariableName])
+            .WithEnvironment("ASPIRE_INSECURE", isHttpsEnabled ? "false" : "true");
+
+
+        return resourceBuilder;
+    }
+}
