8d0c89802b1c3b6a87a0c0d3f343beadf59b7887
Roland Bürgi <rbuergi@systemorph.com>
2025-03-26 21:47:39 +0100

    iterating on article catalog and file browser

---
+++ src/MeshWeaver.Blazor/Components/CollectionPicker.razor.cs
@@ -0,0 +1,39 @@
+﻿using MeshWeaver.Articles;
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace MeshWeaver.Blazor.Components;
+
+public partial class CollectionPicker
+{
+    private IReadOnlyCollection<Option<string>> collections;
+    [Inject] private IArticleService ArticleService { get; set; }
+    [Parameter] public string NullLabel { get; set; } 
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        var definedCollections = await ArticleService.GetCollectionsAsync();
+
+        var options = definedCollections
+            .Select(a => new Option<string>() { Text = a.DisplayName, Value = a.Collection });
+
+        if (NullLabel is not null)
+            options = options
+                .Prepend(new Option<string>() { Text = NullLabel });
+
+
+
+        collections = options.ToArray();
+    }
+    private Task OnValueChanged(string collection)
+    {
+        if (Collection == collection)
+            return Task.CompletedTask;
+        if(collection == NullLabel)
+            collection = null;
+
+        Collection = collection;
+        return CollectionChanged.InvokeAsync(collection);
+    }
+
+}
---
+++ src/MeshWeaver.Blazor/Pages/FileBrowserPage.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeshWeaver.Blazor.Pages;
+
+public partial class FileBrowserPage
+{
+    private void CollectionChanged(string collection)
+    {
+        Collection = collection;
+        InvokeAsync(StateHasChanged);
+    }
+}
