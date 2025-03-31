4cc98cb5f418cbe98f51816bf961a64bba819fe5
Roland Bürgi <rbuergi@systemorph.com>
2025-03-31 14:33:15 +0200

    Factoring out ArticleHeaderDisplay

--- src/MeshWeaver.Blazor/Articles/ArticleView.razor.cs
+++ src/MeshWeaver.Blazor/Articles/ArticleView.razor.cs
@@ -82,75 +82,11 @@ public partial class ArticleView
     }
     
     private readonly KernelAddress KernelAddress = new();
-    private string Name
-    {
-        get => ArticleModel?.Element.Name;
-        set => ArticleModel?.Update(a => a with{Name = value});
-    }
-
-    private string Collection
-    {
-        get => ArticleModel?.Element.Collection;
-        set => ArticleModel?.Update(a => a with { Collection = value });
-    }
-
-    private string Title
-    {
-        get => ArticleModel?.Element.Title;
-        set => ArticleModel?.Update(a => a with { Title = value });
-    }
-
-    private List<string> Tags
-    {
-        get => ArticleModel?.Element.Tags;
-        set => ArticleModel?.Update(a => a with { Tags = value });
-    }
-
-    private string Thumbnail
-    {
-        get => ArticleModel?.Element.Thumbnail;
-        set => ArticleModel?.Update(a => a with { Thumbnail = value });
-    }
-
-    private string Abstract
-    {
-        get => ArticleModel?.Element.Abstract;
-        set => ArticleModel?.Update(a => a with { Abstract = value });
-    }
-
-    private IReadOnlyCollection<Author> Authors
-    {
-        get => ArticleModel?.Element.AuthorDetails;
-    }
-
-    private DateTime? Published
-    {
-        get => ArticleModel?.Element.Published;
-        set => ArticleModel?.Update(a => a with { Published = value });
-    }
-
-    private DateTime? LastUpdated
-    {
-        get => ArticleModel?.Element.LastUpdated;
-    }
-
-    private string Html
-    {
-        get => ArticleModel?.Element.PrerenderedHtml;
-    }
-
-    private string VideoUrl
-    {
-        get => ArticleModel?.Element.VideoUrl;
-        set => ArticleModel?.Update(a => a with { VideoUrl = value });
-    }
 
     private MarkdownControl MarkdownControl
     {
         get => new MarkdownControl(ArticleModel?.Element.Content){Html = ConvertHtml(ArticleModel?.Element.PrerenderedHtml)};
     }
-    private string EditLink => $"/article/edit/{Collection}/{Name}";
-    private string ThumbnailPath => $"/static/{Collection}/{Thumbnail}";
 
 
     private string ConvertHtml(object arg)
@@ -158,20 +94,6 @@ public partial class ArticleView
         return arg?.ToString()!.Replace(ExecutableCodeBlockRenderer.KernelAddressPlaceholder, KernelAddress.ToString());
     }
 
-    private string GetEmbedUrl(string videoUrl)
-    {
-        if (videoUrl.Contains("youtube.com/watch"))
-        {
-            var videoId = videoUrl.Split("v=")[1];
-            var ampersandPosition = videoId.IndexOf('&');
-            if (ampersandPosition != -1)
-            {
-                videoId = videoId.Substring(0, ampersandPosition);
-            }
-            return $"https://www.youtube.com/embed/{videoId}";
-        }
-        return videoUrl;
-    }
 
     protected override void OnAfterRender(bool firstRender)
     {
