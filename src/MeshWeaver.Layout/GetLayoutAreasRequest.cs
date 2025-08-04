bc2c7fd93e5b5658b5c244b6dfb2249edbf0aa60
Roland Bürgi <rbuergi@systemorph.com>
2025-08-04 13:27:38 +0200

    adding LayoutAreaPlugin

---
+++ src/MeshWeaver.Layout/GetLayoutAreasRequest.cs
@@ -0,0 +1,15 @@
+﻿using MeshWeaver.Messaging;
+
+namespace MeshWeaver.Layout;
+
+/// <summary>
+/// Gets the list of layout areas available in the layout context.
+/// </summary>
+public record GetLayoutAreasRequest : IRequest<LayoutAreasResponse>;
+
+/// <summary>
+/// Returns the list of layout areas with their definitions.
+/// </summary>
+/// <param name="Areas">List of layout area definitions available</param>
+public record LayoutAreasResponse(IEnumerable<LayoutAreaDefinition> Areas);
+
