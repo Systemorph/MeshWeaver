a8b2eace468e3c88fe1b53fe3e6cffd26884f302
Roland Bürgi <rbuergi@systemorph.com>
2025-10-16 19:40:45 +0200

    adding content service by default in blazor hosting

--- src/MeshWeaver.Blazor/Infrastructure/PortalApplication.cs
+++ src/MeshWeaver.Blazor/Infrastructure/PortalApplication.cs
@@ -1,4 +1,5 @@
-﻿using MeshWeaver.Layout.Client;
+﻿using MeshWeaver.ContentCollections;
+using MeshWeaver.Layout.Client;
 using MeshWeaver.Mesh;
 using MeshWeaver.Mesh.Services;
 using MeshWeaver.Messaging;
@@ -8,20 +9,22 @@ namespace MeshWeaver.Blazor.Infrastructure;
 
 public class PortalApplication(IMessageHub hub, IRoutingService routingService) : IDisposable
 {
-    public IMessageHub Hub { get; } = hub.GetHostedHub(new PortalAddress(), 
-        c => 
+    public IMessageHub Hub { get; } = hub.GetHostedHub(new PortalAddress(),
+        c =>
             hub.ServiceProvider.GetRequiredService<ILayoutClient>()
                 .Configuration
                 .PortalConfiguration
-                .Aggregate(DefaultPortalConfig(c, routingService), 
+                .Aggregate(DefaultPortalConfig(c, routingService),
                     (cc, ccc) => ccc.Invoke(cc)))!;
 
-    public static MessageHubConfiguration DefaultPortalConfig(MessageHubConfiguration config, IRoutingService routingService)
+    public static MessageHubConfiguration DefaultPortalConfig(MessageHubConfiguration config,
+        IRoutingService routingService)
         => config.WithInitialization(async (hub, _) =>
-        {
-            var meshRegistry = await routingService.RegisterStreamAsync(hub);
-            hub.RegisterForDisposal(meshRegistry);
-        });
+            {
+                var meshRegistry = await routingService.RegisterStreamAsync(hub);
+                hub.RegisterForDisposal(meshRegistry);
+            })
+            .AddContentCollections();
 
 
     public void Dispose()
--- src/MeshWeaver.Hosting.Blazor/BlazorHostingExtensions.cs
+++ src/MeshWeaver.Hosting.Blazor/BlazorHostingExtensions.cs
@@ -17,6 +17,7 @@ public static class BlazorHostingExtensions
     public static MeshBuilder AddBlazor(this MeshBuilder builder, Func<LayoutClientConfiguration, LayoutClientConfiguration>? clientConfig = null) =>
         builder
             .ConfigureServices(services => services
+                .AddContentService()
                 .AddFluentUIComponents()
                 .AddScoped<PortalApplication>()
                 .Configure<RouteOptions>(options =>
