7457afe884278f2a11596b696601372b55169027
Roland Bürgi <rbuergi@systemorph.com>
2025-10-16 13:53:40 +0200

    extending MeshNodeAttribute to include factories

--- src/MeshWeaver.Mesh.Contract/MeshNodeAttribute.cs
+++ src/MeshWeaver.Mesh.Contract/MeshNodeAttribute.cs
@@ -7,12 +7,18 @@ namespace MeshWeaver.Mesh;
 public abstract class MeshNodeAttribute : Attribute
 {
     public virtual IEnumerable<MeshNode> Nodes => [];
-    
+
+    /// <summary>
+    /// Gets the mesh node factories for dynamic node creation.
+    /// Each factory receives an Address and can return a MeshNode or null if it doesn't handle that address.
+    /// </summary>
+    public virtual IEnumerable<Func<Address, MeshNode?>> NodeFactories => [];
+
     protected MeshNode CreateFromHubConfiguration(Address address, string name,
         Func<MessageHubConfiguration, MessageHubConfiguration> hubConfiguration)
         => new(address.Type, address.Id, name)
         {
-            AssemblyLocation = GetType().Assembly.Location, 
+            AssemblyLocation = GetType().Assembly.Location,
             HubConfiguration = hubConfiguration
         };
 
