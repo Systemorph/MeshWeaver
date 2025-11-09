c6f6c68dfa45b3ace493bdbff7cf97c4503c055d
Roland Bürgi <rbuergi@systemorph.com>
2025-11-09 13:47:05 +0100

    introducing presentation mode

--- src/MeshWeaver.ContentCollections/ArticlesLayoutArea.cs
+++ src/MeshWeaver.ContentCollections/ArticlesLayoutArea.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel;
-using System.Reactive.Linq;
 using MeshWeaver.Layout;
 using MeshWeaver.Layout.Composition;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,7 +17,7 @@ public static class ArticlesLayoutArea
     /// <param name="_">The rendering context</param>
     /// <returns>An Article Catalog control</returns>
     [Browsable(false)]
-    public static UiControl? Articles(LayoutAreaHost host, RenderingContext _)
+    public static UiControl Articles(LayoutAreaHost host, RenderingContext _)
     {
         var selectedCollection = host.Reference.Id?.ToString();
         if (selectedCollection is not null)
@@ -32,7 +31,7 @@ public static class ArticlesLayoutArea
         };
     }
 
-    private static UiControl? GetCollectionFromId(LayoutAreaHost host, string selectedCollection)
+    private static UiControl GetCollectionFromId(LayoutAreaHost host, string selectedCollection)
     {
         var contentService = host.Hub.GetContentService();
         var collectionConfig = contentService.GetCollectionConfig(selectedCollection);
