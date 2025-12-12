54d503df0b7ecf4fd9812b0390bc3c6fc0667c38
Roland Bürgi <rbuergi@systemorph.com>
2025-12-12 18:27:47 +0100

    starting to factor out classes in a portal project

--- portal/MeshWeaver.Portal.Shared.Web/Components/NavItem.cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.FluentUI.AspNetCore.Components;
-
-namespace MeshWeaver.Portal.Shared.Web.Components
-{
-    public record NavItem(string Title, string Href, Icon Icon);
-}
--- portal/MeshWeaver.Portal.Shared.Web/Infrastructure/StylesConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MeshWeaver.Portal.Shared.Web.Infrastructure
-{
-    public class StylesConfiguration
-    {
-        public string StylesheetName { get; set; } = string.Empty;
-    }
-}
